af740988bbcf55d655bbfed95252fdf0cb10140f
cvs2git <cvs2git>
1990-03-05 17:04:44 +0000

    This commit was manufactured by cvs2svn to create branch 'non-descriptor'.

--- ldb/parse.h
+++
@@ -1,9 +0,0 @@
-/* $Header: /Volumes/share2/src/cmucl/cvs2git/cvsroot/src/ldb/Attic/parse.h,v 1.1 1990/02/24 19:37:27 wlott Exp $ */
-
-/* All parse routines take a char ** as their only argument */
-
-boolean more_p();
-char *parse_token();
-lispobj parse_lispobj();
-char *parse_addr();
-long parse_number();
