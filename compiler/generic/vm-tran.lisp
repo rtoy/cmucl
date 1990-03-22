26bded7d798fa496cda86c2fc1b40478d3c91273
wlott <wlott>
1990-03-22 17:49:22 +0000

    Fixed header-ref hacks now that fast-svref is gone.  Now we use data-vector-ref/simple-string.
    Moved sequence simple-string transforms into here, 'cause they are vm
    dependent.

--- compiler/generic/vm-tran.lisp
+++ compiler/generic/vm-tran.lisp
@@ -7,7 +7,7 @@
 ;;; Scott Fahlman (FAHLMAN@CMUC). 
 ;;; **********************************************************************
 ;;;
-;;; $Header: /Volumes/share2/src/cmucl/cvs2git/cvsroot/src/compiler/generic/vm-tran.lisp,v 1.4 1990/03/21 23:36:16 wlott Exp $
+;;; $Header: /Volumes/share2/src/cmucl/cvs2git/cvsroot/src/compiler/generic/vm-tran.lisp,v 1.5 1990/03/22 17:49:22 wlott Exp $
 ;;;
 ;;;    This file contains impelemtentation-dependent transforms.
 ;;;
@@ -62,20 +62,19 @@
 
 (def-primitive-translator header-ref (obj slot)
   (warn "Someone used HEADER-REF.")
-  `(%primitive fast-svref ,obj ,slot))
+  `(%primitive data-vector-ref/simple-vector ,obj ,slot))
 
 (def-primitive-translator header-set (obj slot value)
   (warn "Someone used HEADER-SET.")
-  `(%primitive fast-svset ,obj ,slot ,value))
+  `(%primitive data-vector-set/simple-vector ,obj ,slot ,value))
 
 (def-primitive-translator header-length (obj)
   (warn "Someone used HEADER-LENGTH.")
   `(%primitive vector-length ,obj))
 
 
-
-;;; Transforms for data-vector-ref for strange array types.
-
+
+;;;; Transforms for data-vector-ref for strange array types.
 
 (deftransform data-vector-ref ((array index)
 			       (simple-array t))
@@ -143,3 +142,78 @@
   (frob simple-bit-vector 1)
   (frob (simple-array (unsigned-byte 2) (*)) 2)
   (frob (simple-array (unsigned-byte 4) (*)) 4))
+
+
+
+
+;;;; Simple string transforms:
+
+(defconstant vector-data-bit-offset (* vm:vector-data-offset vm:word-bits))
+
+(deftransform subseq ((string start &optional (end nil))
+		      (simple-string t &optional t))
+  '(let* ((length (- (or end (length string))
+		     start))
+	  (result (make-string length)))
+     (bit-bash-copy string
+		    (+ (* start vm:byte-bits) vector-data-bit-offset)
+		    result
+		    vector-data-bit-offset
+		    (* (- end start) vm:byte-bits))
+     result))
+
+
+(deftransform copy-seq ((seq) (simple-string))
+  '(let* ((length (length seq))
+	  (res (make-string length)))
+     (bit-bash-copy string
+		    vector-data-bit-offset
+		    result
+		    vector-data-bit-offset
+		    (* length vm:byte-bits))
+     res))
+
+
+(deftransform replace ((string1 string2 &key (start1 0) (start2 0)
+				end1 end2)
+		       (simple-string simple-string &rest t))
+  '(progn
+     (bit-bash-copy string1
+		    (+ (* start1 vm:byte-bits) vector-data-bit-offset)
+		    string2
+		    (+ (* start2 vm:byte-bits) vector-data-bit-offset)
+		    (* (min (- (or end1 (length string1))
+			       start1)
+			    (- (or end2 (length string2))
+			       start2))
+		       vm:byte-bits))
+     string1))
+
+
+(deftransform concatenate ((rtype &rest sequences)
+			   (t &rest simple-string)
+			   simple-string)
+  (collect ((lets)
+	    (forms)
+	    (all-lengths)
+	    (args))
+    (dolist (seq sequences)
+      (declare (ignore seq))
+      (let ((n-seq (gensym))
+	    (n-length (gensym)))
+	(args n-seq)
+	(lets `(,n-length (* (length ,n-seq) vm:byte-bits)))
+	(all-lengths n-length)
+	(forms `(bit-bash-copy ,n-seq vector-data-bit-offset
+			       res start
+			       ,n-length))
+	(forms `(setq start (+ start ,n-length)))))
+    `(lambda (rtype ,@(args))
+       (declare (ignore rtype))
+       (let* (,@(lets)
+	      (res (make-string (truncate (+ ,@(all-lengths)) vm:byte-bits)))
+	      (start vector-data-bit-offset))
+	 (declare (type index start ,@(all-lengths)))
+	 ,@(forms)
+	 res))))
+
