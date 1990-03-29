7b4a5c7cf3a5615f4021bce61c6061f1bf642840
ch <ch>
1990-03-29 21:13:23 +0000

    Previous version incremented the BSP by the wrong size (was 4 not 8).

--- ldb/dynbind.c
+++ ldb/dynbind.c
@@ -1,5 +1,5 @@
 /*
- * $Header: /Volumes/share2/src/cmucl/cvs2git/cvsroot/src/ldb/Attic/dynbind.c,v 1.1 1990/03/29 02:55:05 ch Exp $
+ * $Header: /Volumes/share2/src/cmucl/cvs2git/cvsroot/src/ldb/Attic/dynbind.c,v 1.2 1990/03/29 21:13:23 ch Exp $
  * 
  * Support for dynamic binding from C.
  */
@@ -16,7 +16,7 @@ lispobj symbol, value;
 
 	old_value = SymbolValue(symbol);
 	binding = (struct binding *) current_binding_stack_pointer;
-	current_binding_stack_pointer += (sizeof(binding) / sizeof(lispobj));
+	current_binding_stack_pointer += (sizeof(struct binding) / sizeof(lispobj));
 	binding->value = old_value;
 	binding->symbol = symbol;
 	SetSymbolValue(symbol, value);
@@ -35,7 +35,7 @@ unbind()
 
 	binding->symbol = 0;
 
-	current_binding_stack_pointer -= (sizeof(binding) / sizeof(lispobj));
+	current_binding_stack_pointer -= (sizeof(struct binding) / sizeof(lispobj));
 }
 
 unbind_to_here(bsp)
@@ -54,6 +54,7 @@ lispobj *bsp;
 			binding->symbol = 0;
 		}
 
-		current_binding_stack_pointer -= (sizeof(binding) / sizeof(lispobj));
+		current_binding_stack_pointer -=
+			(sizeof(struct binding) / sizeof(lispobj));
 	}
 }
