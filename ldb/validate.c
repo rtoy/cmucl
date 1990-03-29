fc8e945cb9c5731af3c54c0057a95b34689d574c
ch <ch>
1990-03-29 21:15:22 +0000

    Updated to use parameters from ./validate.h

--- ldb/validate.c
+++ ldb/validate.c
@@ -1,8 +1,9 @@
-/* $Header: /Volumes/share2/src/cmucl/cvs2git/cvsroot/src/ldb/Attic/validate.c,v 1.1 1990/03/28 22:51:19 ch Exp $ */
+/* $Header: /Volumes/share2/src/cmucl/cvs2git/cvsroot/src/ldb/Attic/validate.c,v 1.2 1990/03/29 21:15:22 ch Exp $ */
 
 #include <stdio.h>
 #include "lisp.h"
 #include "globals.h"
+#include "validate.h"
 
 validate()
 {
@@ -10,28 +11,27 @@ validate()
 	fflush(stdout);
 
 	/* Read only space */
-	read_only_space = (lispobj *) 0x20000000;
-	os_validate(read_only_space, 0x1000000);
+	read_only_space = (lispobj *) READ_ONLY_SPACE_START;
+	os_validate(read_only_space, READ_ONLY_SPACE_SIZE);
 
 	/* Static Space */
-	static_space = (lispobj *) 0x30000000;
-	os_validate(static_space, 0x1000000);
+	static_space = (lispobj *) STATIC_SPACE_START;
+	os_validate(static_space, STATIC_SPACE_SIZE);
 
-	/* Dynamic Spaces */
-	dynamic_0_space = (lispobj *) 0x40000000;
-	os_validate(dynamic_0_space, 0x2000000);
+	/* Dynamic 0 Space */
+	dynamic_0_space = (lispobj *) DYNAMIC_0_SPACE_START;
+	os_validate(dynamic_0_space, DYNAMIC_SPACE_SIZE);
 	current_dynamic_space = dynamic_0_space;
 
-	dynamic_1_space = (lispobj *) 0x48000000;
-	os_validate(dynamic_1_space, 0x08000000);
+	dynamic_1_space = (lispobj *) DYNAMIC_1_SPACE_START;
 
 	/* Control Stack */
-	control_stack = (lispobj *) 0x50000000;
-	os_validate(control_stack, 0x08000000);
+	control_stack = (lispobj *) CONTROL_STACK_START;
+	os_validate(control_stack, CONTROL_STACK_SIZE);
 
 	/* Binding Stack */
-	binding_stack = (lispobj *) 0x60000000;
-	os_validate(binding_stack, 0x2000000);
+	binding_stack = (lispobj *) BINDING_STACK_START;
+	os_validate(binding_stack, BINDING_STACK_SIZE);
 
 	printf(" done.\n");
 }
