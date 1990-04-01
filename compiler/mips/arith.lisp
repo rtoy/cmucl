870677f16481cedfff2050c37eda01e959129e00
wlott <wlott>
1990-04-01 17:37:49 +0000

    Added static function definitions for two-arg comparisons.

--- compiler/mips/arith.lisp
+++ compiler/mips/arith.lisp
@@ -304,9 +304,20 @@
     (nop)))
 
 
-(define-static-function two-arg-plus (x y) :translate +)
-(define-static-function two-arg-minus (x y) :translate -)
-(define-static-function two-arg-times (x y) :translate *)
-(define-static-function two-arg-divide (x y) :translate /)
+
+
+;;;; Static functions.
+
+(define-static-function two-arg-+ (x y) :translate +)
+(define-static-function two-arg-- (x y) :translate -)
+(define-static-function two-arg-* (x y) :translate *)
+(define-static-function two-arg-/ (x y) :translate /)
+
+(define-static-function two-arg-< (x y) :translate <)
+(define-static-function two-arg-<= (x y) :translate <=)
+(define-static-function two-arg-> (x y) :translate >)
+(define-static-function two-arg->= (x y) :translate >=)
+(define-static-function two-arg-= (x y) :translate =)
+(define-static-function two-arg-/= (x y) :translate /=)
 
 (define-static-function negate (x) :translate -)
