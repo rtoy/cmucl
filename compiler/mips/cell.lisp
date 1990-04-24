07031746d83456926dfae334a46cf96b770c9a2b
wlott <wlott>
1990-04-24 19:07:35 +0000

    Removed spurious node references.

--- compiler/mips/cell.lisp
+++ compiler/mips/cell.lisp
@@ -7,7 +7,7 @@
 ;;; Scott Fahlman (FAHLMAN@CMUC). 
 ;;; **********************************************************************
 ;;;
-;;; $Header: /Volumes/share2/src/cmucl/cvs2git/cvsroot/src/compiler/mips/cell.lisp,v 1.31 1990/04/24 02:55:58 wlott Exp $
+;;; $Header: /Volumes/share2/src/cmucl/cvs2git/cvsroot/src/compiler/mips/cell.lisp,v 1.32 1990/04/24 19:07:35 wlott Exp $
 ;;;
 ;;;    This file contains the VM definition of various primitive memory access
 ;;; VOPs for the MIPS.
@@ -167,7 +167,6 @@
   (:args (object :scs (descriptor-reg) :target obj-temp))
   (:results (value :scs (descriptor-reg any-reg)))
   (:policy :fast-safe)
-  (:node-var node)
   (:temporary (:type random  :scs (non-descriptor-reg)) temp)
   (:temporary (:scs (descriptor-reg) :from (:argument 0)) obj-temp))
 
@@ -179,7 +178,7 @@
   (:generator 9
     (move obj-temp object)
     (loadw value obj-temp vm:symbol-value-slot vm:other-pointer-type)
-    (let ((err-lab (generate-error-code node di:unbound-symbol-error obj-temp)))
+    (let ((err-lab (generate-error-code di:unbound-symbol-error obj-temp)))
       (inst xor temp value vm:unbound-marker-type)
       (inst beq temp zero-tn err-lab)
       (inst nop))))
@@ -192,8 +191,7 @@
   (:generator 10
     (move obj-temp object)
     (loadw value obj-temp vm:symbol-function-slot vm:other-pointer-type)
-    (let ((err-lab (generate-error-code node di:undefined-symbol-error
-					obj-temp)))
+    (let ((err-lab (generate-error-code di:undefined-symbol-error obj-temp)))
       (test-simple-type value temp err-lab t vm:function-pointer-type))))
 
 
