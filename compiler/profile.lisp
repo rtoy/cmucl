113c001a5444a18471923b77fac19ee055b35458
ram <ram>
1990-04-27 11:51:47 +0000

    Added new and missing functions and flushed old ones.

--- compiler/profile.lisp
+++ compiler/profile.lisp
@@ -13,20 +13,20 @@
 	 flush-dead-code
 	 generate-type-checks
 	 constraint-propagate
+	 pre-environment-analyze-top-level
 	 environment-analyze
 	 gtn-analyze
 	 control-analyze
 	 ltn-analyze
 	 stack-analyze
 	 ir2-convert
+	 select-representations
 	 lifetime-pre-pass
 	 lifetime-flow-analysis
 	 reset-current-conflict
 	 lifetime-post-pass
 	 delete-unreferenced-tns
 
-;	 pack
-	 compute-costs-and-target
 	 pack-wired-tn
 	 pack-tn
 	 pack-targeting-tns
@@ -35,6 +35,14 @@
 
 	 generate-code
 	 fasl-dump-component
+	 clear-ir2-info
+	 macerate-ir1-component
+	 merge-top-level-lambdas
+	 ir1-finalize
+	 clear-stuff
+	 read-source-form
+	 fasl-dump-source-info
+	 fasl-dump-top-level-lambda-call
 ;	 check-life-consistency
 ;	 check-ir1-consistency
 ;	 check-ir2-consistency
