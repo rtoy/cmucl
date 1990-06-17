e0136a8febf50a8ac4e1ec62647a046b2f445ee3
ram <ram>
1990-06-17 11:44:43 +0000

    New compilation hacks.

--- clx/build-clx.lisp
+++ clx/build-clx.lisp
@@ -1,7 +1,7 @@
 ;;; -*- Mode: Lisp; Package: Xlib; Log: clx.log -*-
 
 ;;; Load this file if you want to compile CLX in its entirety.
-
+#+nil
 (proclaim '(optimize (speed 3) (safety 0) (space 1)
 		     (compilation-speed 0)))
 
@@ -19,7 +19,7 @@
 
 ;(make-package "XLIB" :use '("LISP"))
 
+(compile-file "clx:defsystem.lisp" :error-file nil :load t)
 
-(compile-file "clx:defsystem.lisp" :error-file nil)
-(load "clx:defsystem.fasl")
-(xlib:compile-clx (pathname "clx:"))
+(with-compilation-unit ()
+  (xlib:compile-clx (pathname "clx:")))
