57dc4407b0e1a305da36bdbc70b8cf2e2d6c692c
wlott <wlott>
1990-06-23 06:20:37 +0000

    Can't use #+new-compiler in FIXNUM, 'cause the wrong one will be used in the bootstrap env because of the eval-when that wraps everything.

--- compiler/mips/parms.lisp
+++ compiler/mips/parms.lisp
@@ -7,7 +7,7 @@
 ;;; Scott Fahlman (FAHLMAN@CMUC). 
 ;;; **********************************************************************
 ;;;
-;;; $Header: /Volumes/share2/src/cmucl/cvs2git/cvsroot/src/compiler/mips/parms.lisp,v 1.56 1990/06/23 05:39:59 wlott Exp $
+;;; $Header: /Volumes/share2/src/cmucl/cvs2git/cvsroot/src/compiler/mips/parms.lisp,v 1.57 1990/06/23 06:20:37 wlott Exp $
 ;;;
 ;;;    This file contains some parameterizations of various VM
 ;;; attributes for the MIPS.  This file is separate from other stuff so 
@@ -540,8 +540,10 @@
 (defun fixnum (num)
   "Make a fixnum out of NUM.  (i.e. shift by two bits if it will fit.)"
   (if (<= #x-20000000 num #x1fffffff)
-      #+new-compiler (ash num 2)
-      #-new-compiler (if (minusp num) (- (ash (- num) 2)) (ash num 2))
+      ;; ### ASH doesn't work on negative bignums in the old compiler, but
+      ;; it we #-new-compiler this, the wrong defn will be used when we try
+      ;; to ncompile-file it in the bootstrap env.
+      (if (minusp num) (- (ash (- num) 2)) (ash num 2))
       (error "~D is too big for a fixnum." num)))
 
 
