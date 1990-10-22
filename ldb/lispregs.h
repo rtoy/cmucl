77f6cd795423e16e31b281d57cdaf846582a3d9e
wlott <wlott>
1990-10-22 12:30:03 +0000

    Moved some registers around so that we could use jal and have the return address in the LIP.  Also changed the name of CONT to CSP and OLDCONT to OCFP.

--- ldb/lispregs.h
+++ ldb/lispregs.h
@@ -1,7 +1,9 @@
-/* $Header: /Volumes/share2/src/cmucl/cvs2git/cvsroot/src/ldb/Attic/lispregs.h,v 1.3 1990/05/12 16:41:33 ch Exp $ */
+/* $Header: /Volumes/share2/src/cmucl/cvs2git/cvsroot/src/ldb/Attic/lispregs.h,v 1.4 1990/10/22 12:30:03 wlott Exp $ */
 
 #ifdef LANGUAGE_ASSEMBLY
+#ifdef mips
 #define REG(num) $num
+#endif
 #else
 #define REG(num) num
 
@@ -10,13 +12,14 @@ extern char *lisp_register_names[];
 
 #define NREGS	(32)
 
+#ifdef mips
 #define ZERO    REG(0)
-#define LIP     REG(1)
-#define NL0     REG(2)
-#define NL1     REG(3)
-#define NL2     REG(4)
-#define NL3     REG(5)
-#define NL4     REG(6)
+#define NL3     REG(1)
+#define NL4     REG(2)
+#define FLAGS   REG(3)
+#define NL0     REG(4)
+#define NL1     REG(5)
+#define NL2     REG(6)
 #define NARGS   REG(7)
 #define A0      REG(8)
 #define A1      REG(9)
@@ -26,17 +29,18 @@ extern char *lisp_register_names[];
 #define A5      REG(13)
 #define CNAME   REG(14)
 #define LEXENV  REG(15)
-#define ARGS    REG(16)
-#define OLDCONT REG(17)
+#define NFP     REG(16)
+#define OCFP    REG(17)
 #define LRA     REG(18)
 #define L0      REG(19)
 #define NULLREG REG(20)
 #define BSP     REG(21)
-#define CONT    REG(22)
+#define CFP     REG(22)
 #define CSP     REG(23)
-#define FLAGS   REG(24)
+#define L1      REG(24)
 #define ALLOC   REG(25)
-#define L1      REG(28)
+#define L2      REG(28)
 #define NSP     REG(29)
 #define CODE    REG(30)
-#define L2      REG(31)
+#define LIP     REG(31)
+#endif
