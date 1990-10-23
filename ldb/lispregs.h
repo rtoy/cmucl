5915abd8304331e46f4383926f52aa185b6575e0
wlott <wlott>
1990-10-23 00:02:53 +0000

    Added SPARC registers.

--- ldb/lispregs.h
+++ ldb/lispregs.h
@@ -1,8 +1,11 @@
-/* $Header: /Volumes/share2/src/cmucl/cvs2git/cvsroot/src/ldb/Attic/lispregs.h,v 1.4 1990/10/22 12:30:03 wlott Exp $ */
+/* $Header: /Volumes/share2/src/cmucl/cvs2git/cvsroot/src/ldb/Attic/lispregs.h,v 1.5 1990/10/23 00:02:45 wlott Exp $ */
 
 #ifdef LANGUAGE_ASSEMBLY
 #ifdef mips
 #define REG(num) $num
+#endif
+#ifdef sparc
+#define REG(num) %num
 #endif
 #else
 #define REG(num) num
@@ -43,4 +46,39 @@ extern char *lisp_register_names[];
 #define NSP     REG(29)
 #define CODE    REG(30)
 #define LIP     REG(31)
+#endif
+
+
+#ifdef sparc
+#define ZERO REG(0)
+#define ALLOC REG(1)
+#define NULLREG REG(2)
+#define CSP REG(3)
+#define CFP REG(4)
+#define BSP REG(5)
+#define NFP REG(6)
+#define CFUNC REG(7)
+#define NL0 REG(8)
+#define NL1 REG(9)
+#define NL2 REG(10)
+#define NL3 REG(11)
+#define NL4 REG(12)
+#define NL5 REG(13)
+#define NSP REG(14)
+#define NARGS REG(16)
+#define A0 REG(16)
+#define A1 REG(17)
+#define A2 REG(18)
+#define A3 REG(19)
+#define A4 REG(20)
+#define A5 REG(21)
+#define OCFP REG(22)
+#define LRA REG(23)
+#define CNAME REG(24)
+#define LEXENV REG(25)
+#define L0 REG(26)
+#define L1 REG(27)
+#define L2 REG(28)
+#define CODE REG(29)
+#define LIP REG(31)
 #endif
--- ldb/regnames.c
+++ ldb/regnames.c
@@ -1,8 +1,9 @@
-/* $Header: /Volumes/share2/src/cmucl/cvs2git/cvsroot/src/ldb/Attic/regnames.c,v 1.2 1990/10/22 12:31:21 wlott Exp $ */
+/* $Header: /Volumes/share2/src/cmucl/cvs2git/cvsroot/src/ldb/Attic/regnames.c,v 1.3 1990/10/23 00:02:53 wlott Exp $ */
 
 #include "lispregs.h"
 
 char *lisp_register_names[] = {
+#ifdef mips
 	"ZERO",
 	"NL3",
 	"NL4",
@@ -35,6 +36,41 @@ char *lisp_register_names[] = {
 	"NSP",
 	"CODE",
 	"LIP"
+#endif
+#ifdef sparc
+        "ZERO",
+        "ALLOC",
+        "NULL",
+        "CSP",
+        "CFP",
+        "BSP",
+        "NFP",
+        "CFUNC",
+        "NL0",
+        "NL1",
+        "NL2",
+        "NL3",
+        "NL4",
+        "NL5",
+        "NSP",
+        "NARGS",
+        "A0",
+        "A1",
+        "A2",
+        "A3",
+        "A4",
+        "A5",
+        "OCFP",
+        "LRA",
+        "CNAME",
+        "LEXENV",
+        "L0",
+        "L1",
+        "L2",
+        "CODE",
+        "???",
+        "LIP"
+#endif
 };
 
 		
