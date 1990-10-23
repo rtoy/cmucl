ed31959672b86f47db289156218b4797f01217bc
wlott <wlott>
1990-10-23 00:04:36 +0000

    Generalized with #ifdefs for the sparc.

--- ldb/globals.h
+++ ldb/globals.h
@@ -1,11 +1,11 @@
-/* $Header: /Volumes/share2/src/cmucl/cvs2git/cvsroot/src/ldb/Attic/globals.h,v 1.5 1990/10/13 04:49:59 wlott Exp $ */
+/* $Header: /Volumes/share2/src/cmucl/cvs2git/cvsroot/src/ldb/Attic/globals.h,v 1.6 1990/10/23 00:04:36 wlott Exp $ */
 
 #if !defined(_INCLUDE_GLOBALS_H_)
 #define _INCLUDED_GLOBALS_H_
 
 #include "lisp.h"
 
-#if !defined(LANGUAGE_ASSEMBLY)
+#ifndef LANGUAGE_ASSEMBLY
 
 extern char *number_stack_start;
 
@@ -13,12 +13,12 @@ extern int foreign_function_call_active;
 
 #ifdef mips
 extern unsigned long saved_global_pointer;
+extern unsigned long current_flags_register;
 #endif
 
 extern lispobj *current_control_stack_pointer;
 extern lispobj *current_control_frame_pointer;
 extern lispobj *current_binding_stack_pointer;
-extern unsigned long current_flags_register;
 
 extern lispobj *read_only_space;
 extern lispobj *static_space;
@@ -31,18 +31,28 @@ extern lispobj *current_dynamic_space;
 extern lispobj *current_dynamic_space_free_pointer;
 extern lispobj *current_auto_gc_trigger;
 
-#else
+#else  LANGUAGE_ASSEMBLY
 
 /* These are needed by ./assem.s */
 
-.extern foreign_function_call_active 4
-
-.extern current_dynamic_space_free_pointer 4
-.extern current_control_stack_pointer 4
-.extern current_control_frame_pointer 4
-.extern current_binding_stack_pointer 4
-.extern current_flags_register 4
-
+#ifdef mips
+#define EXTERN(name,bytes) .extern name bytes
+#endif
+#ifdef sparc
+#define EXTERN(name,bytes) .global _/**/name
 #endif
 
+EXTERN(foreign_function_call_active, 4)
+
+EXTERN(current_dynamic_space_free_pointer, 4)
+EXTERN(current_control_stack_pointer, 4)
+EXTERN(current_control_frame_pointer, 4)
+EXTERN(current_binding_stack_pointer, 4)
+
+#ifdef mips
+EXTERN(current_flags_register, 4)
 #endif
+
+#endif LANGUAGE_ASSEMBLY
+
+#endif _INCLUDED_GLOBALS_H_
