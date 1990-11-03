157c36a1ed59002dc4c65e3af98273f945a0d966
wlott <wlott>
1990-11-03 02:53:31 +0000

    Removed some globals that are no longer used.  Removed the target-byte-order/native-byte-order hacks.

--- compiler/globals.lisp
+++ compiler/globals.lisp
@@ -8,16 +8,6 @@
 	    *last-assembler-node-created* *other-code-vector*
 	    *other-next-location* *fixup-offset* *fixup-offset-map*
 	    *fixup-last-shortening* *result-fixups* *source-path-tree*
-	    *sc-numbers* *sb-list* *template-names* *sc-names* *sb-names*
-	    *meta-sc-numbers* *primitive-type-names* *move-costs* *save-scs*
-	    *save-costs* *restore-costs* *parsed-vops*
-	    *compiler-error-context* *word-length* target-byte-order
-	    *undefined-warnings* *meta-sb-names* *meta-sc-names*
+	    *compiler-error-context* *word-length* 
+	    *undefined-warnings*
 	    *code-segment* *elsewhere*))
-
-(defconstant native-byte-order target-byte-order
-  "The byte order we are running under.")
-
-(eval-when (compile)
-  (format t "foo!")
-  (setf (info variable constant-value 'native-byte-order) target-byte-order))
