9728aba2cd711190b0c8ee2a3beef52c36f5dd13
wlott <wlott>
1990-11-12 03:21:53 +0000

    Added exports for backend-instruction-formats, backend-instruction-flavors, and backend-special-arg-types.

--- compiler/backend.lisp
+++ compiler/backend.lisp
@@ -19,6 +19,8 @@
 	   backend-fasl-file-implementation backend-fasl-file-version
 	   backend-register-save-penalty backend-byte-order
 	   backend-any-primitive-type backend-info-environment
+	   backend-instruction-formats backend-instruction-flavors
+	   backend-special-arg-types
 
 	   ;; The various backends need to call these support routines
 	   make-stack-pointer-tn primitive-type primitive-type-of))
