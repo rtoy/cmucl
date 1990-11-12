5ecc8049fbd4e22ebc488cb9d4bfbf8e88c365a6
wlott <wlott>
1990-11-12 02:36:28 +0000

    Turned off gc_asserts, 'cause we trust GC now.

--- ldb/gc.h
+++ ldb/gc.h
@@ -1,7 +1,7 @@
 /*
  * Header file for GC
  *
- * $Header: /Volumes/share2/src/cmucl/cvs2git/cvsroot/src/ldb/Attic/gc.h,v 1.3 1990/05/13 15:30:58 ch Exp $
+ * $Header: /Volumes/share2/src/cmucl/cvs2git/cvsroot/src/ldb/Attic/gc.h,v 1.4 1990/11/12 02:36:28 wlott Exp $
  */
 
 #if !defined(_INCLUDED_GC_H_)
@@ -11,13 +11,20 @@
 
 extern void gc_lose();
 
-#define gc_assert(ex) { \
-	if (!(ex)) { \
-		fprintf(stderr, "GC invariant lost!  File \"%s\", line %d\n", \
-		__FILE__, __LINE__); \
-		gc_lose(); \
-	} \
-}
+#define gc_abort() do { \
+	fprintf(stderr, "GC invariant lost!  File \"%s\", line %d\n", \
+	__FILE__, __LINE__); \
+	gc_lose(); \
+} while (0)
+
+#if 0
+#define gc_assert(ex) do { \
+	if (!(ex)) gc_abort(); \
+} while (0)
+#else
+#define gc_assert(ex)
+#endif
+
 
 #define CEILING(x,y) (((x) + ((y) - 1)) & (~((y) - 1)))
 #define FIXNUM_TO_INT(x) ((x)>>2)
