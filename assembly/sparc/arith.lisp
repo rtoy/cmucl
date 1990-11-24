8394f569f39d4dd4ce10d19b6abb95fcceabe1c9
wlott <wlott>
1990-11-24 19:20:44 +0000

    Fixed the code to tail-call into static-functions.  Fixed the divide noise.  Added generic comparisons.

--- assembly/sparc/arith.lisp
+++ assembly/sparc/arith.lisp
@@ -7,7 +7,7 @@
 ;;; Scott Fahlman (FAHLMAN@CMUC). 
 ;;; **********************************************************************
 ;;;
-;;; $Header: /Volumes/share2/src/cmucl/cvs2git/cvsroot/src/assembly/sparc/arith.lisp,v 1.1 1990/11/22 11:49:50 wlott Exp $
+;;; $Header: /Volumes/share2/src/cmucl/cvs2git/cvsroot/src/assembly/sparc/arith.lisp,v 1.2 1990/11/24 19:20:44 wlott Exp $
 ;;;
 ;;; Stuff to handle simple cases for generic arithmetic.
 ;;;
@@ -58,7 +58,9 @@
   (inst li nargs (fixnum 2))
   (loadw code-tn cname vm:symbol-raw-function-addr-slot vm:other-pointer-type)
   (inst move ocfp cfp-tn)
-  (inst j code-tn)
+  (inst j code-tn
+	(- (* vm:function-header-code-offset vm:word-bytes)
+	   vm:function-pointer-type))
   (inst move cfp-tn csp-tn)
 
   DONE
@@ -103,7 +105,9 @@
   (inst li nargs (fixnum 2))
   (loadw code-tn cname vm:symbol-raw-function-addr-slot vm:other-pointer-type)
   (inst move ocfp cfp-tn)
-  (inst j code-tn)
+  (inst j code-tn
+	(- (* vm:function-header-code-offset vm:word-bytes)
+	   vm:function-pointer-type))
   (inst move cfp-tn csp-tn)
 
   DONE
@@ -180,7 +184,9 @@
   (inst li nargs (fixnum 2))
   (loadw code-tn cname symbol-raw-function-addr-slot other-pointer-type)
   (inst move ocfp cfp-tn)
-  (inst j code-tn)
+  (inst j code-tn
+	(- (* vm:function-header-code-offset vm:word-bytes)
+	   vm:function-pointer-type))
   (inst move cfp-tn csp-tn)
 
   DONE)
@@ -210,77 +216,296 @@
 ;;;; Division.
 
 #+assembler
-(defun 32/32-restoring-divide (dividend divisor rem quo iter)
-  (let ((done-shifting (gen-label))
-	(not-big-dividend (gen-label)))
+(defun emit-divide-loop (divisor rem quo tagged)
+  (labels
+      ((do-loop (depth)
+	 (cond
+	  ((zerop depth)
+	   (inst unimp 0))
+	  (t
+	   (let ((label-1 (gen-label))
+		 (label-2 (gen-label)))
+	     (inst cmp divisor rem)
+	     (inst b :geu label-1)
+	     (inst nop)
+	     (inst sll divisor 1)
+	     (do-loop (1- depth))
+	     (inst srl divisor 1)
+	     (inst cmp divisor rem)
+	     (emit-label label-1)
+	     (inst b :gtu label-2)
+	     (inst sll quo 1)
+	     (inst add quo (if tagged (fixnum 1) 1))
+	     (inst sub rem divisor)
+	     (emit-label label-2))))))
+    (do-loop (if tagged 30 32))))
 
-    (inst addcc rem dividend 0)
-    (inst b :ge not-big-dividend)
-    (inst li iter (fixnum -1))
+(define-assembly-routine (positive-fixnum-truncate
+			  (:note "unsigned fixnum truncate")
+			  (:cost 45)
+			  (:translate truncate)
+			  (:policy :fast-safe)
+			  (:arg-types positive-fixnum positive-fixnum)
+			  (:result-types positive-fixnum positive-fixnum))
+			 ((:arg dividend any-reg nl0-offset)
+			  (:arg divisor any-reg nl1-offset)
 
-    ;; The dividend is huge.  If we shift the divisor until it's negative.
-    (let ((loop (gen-label)))
-      (inst cmp divisor)
-      (inst b :lt done-shifting)
-      (inst li quo -1)
-      (emit-label loop)
-      (inst addcc divisor divisor)
-      (inst b :ge loop)
-      (inst add iter (fixnum 1))
-      (inst b done-shifting)
-      (inst nop))
+			  (:res quo any-reg nl2-offset)
+			  (:res rem any-reg nl0-offset))
 
-    ;; The dividend is not huge.  Keep shifting the divisor up until it's
-    ;; larger than the dividend.
-    (emit-label not-big-dividend)
-    (let ((loop (gen-lable)))
-      (inst cmp divisor rem)
-      (inst b :ge done-shifting)
-      (inst li quo -1)
-      (emit-label loop)
-      (inst sll divisor 1)
-      (inst cmp quo rem)
-      (inst b :nc loop)
-      (inst add iter (fixnum 1)))
+  (move rem dividend)
+  (inst li quo 0)
+  (emit-divide-loop divisor rem quo t))
 
-    (emit-label done-shifting)
-    (let ((loop-1 (gen-label))
-	  (loop-2 (gen-label))
-	  (finished (gen-label)))
-      (emit-label loop-1)
-      (inst subcc iter (fixnum 1))
-      (inst b :le finished)
-      (emit-label loop-2)
-      (inst sub temp rem divisor)
-      (inst slr divisor 1)
-      (inst b :cs loop)
-      (inst addcc quo quo)
-      (inst subcc iter (fixnum 1))
-      (inst b :gt loop-2)
-      (inst move rem temp)
-      (emit-label finished))
 
-    (inst not quo)))
+(define-assembly-routine (fixnum-truncate
+			  (:note "fixnum truncate")
+			  (:cost 50)
+			  (:policy :fast-safe)
+			  (:translate truncate)
+			  (:arg-types tagged-num tagged-num)
+			  (:result-types tagged-num tagged-num))
+			 ((:arg dividend any-reg nl0-offset)
+			  (:arg divisor any-reg nl1-offset)
 
+			  (:res quo any-reg nl2-offset)
+			  (:res rem any-reg nl0-offset)
+
+			  (:temp quo-sign any-reg nl5-offset)
+			  (:temp rem-sign any-reg nargs-offset))
+  
+  (inst xor quo-sign dividend divisor)
+  (inst move rem-sign dividend)
+  (let ((label (gen-label)))
+    (inst cmp dividend)
+    (inst ba :lt label)
+    (inst neg dividend)
+    (emit-label label))
+  (let ((label (gen-label)))
+    (inst cmp divisor)
+    (inst ba :lt label)
+    (inst neg divisor)
+    (emit-label label))
+  (emit-divide-loop divisor rem quo t)
+  (let ((label (gen-label)))
+    ;; If the quo-sign is negative, we need to negate quo.
+    (inst cmp quo-sign)
+    (inst ba :lt label)
+    (inst neg quo)
+    (emit-label label))
+  (let ((label (gen-label)))
+    ;; If the rem-sign is negative, we need to negate rem.
+    (inst cmp rem-sign)
+    (inst ba :lt label)
+    (inst neg rem)
+    (emit-label label)))
+
+
+(define-assembly-routine (signed-truncate
+			  (:note "(signed-byte 32) truncate")
+			  (:cost 60)
+			  (:policy :fast-safe)
+			  (:translate truncate)
+			  (:arg-types signed-num signed-num)
+			  (:result-types signed-num signed-num))
+
+			 ((:arg dividend signed-reg nl0-offset)
+			  (:arg divisor signed-reg nl1-offset)
+
+			  (:res quo signed-reg nl2-offset)
+			  (:res rem signed-reg nl0-offset)
+
+			  (:temp quo-sign signed-reg nl5-offset)
+			  (:temp rem-sign signed-reg nargs-offset))
+  
+  (inst xor quo-sign dividend divisor)
+  (inst move rem-sign dividend)
+  (let ((label (gen-label)))
+    (inst cmp dividend)
+    (inst ba :lt label)
+    (inst neg dividend)
+    (emit-label label))
+  (let ((label (gen-label)))
+    (inst cmp divisor)
+    (inst ba :lt label)
+    (inst neg divisor)
+    (emit-label label))
+  (emit-divide-loop divisor rem quo nil)
+  (let ((label (gen-label)))
+    ;; If the quo-sign is negative, we need to negate quo.
+    (inst cmp quo-sign)
+    (inst ba :lt label)
+    (inst neg quo)
+    (emit-label label))
+  (let ((label (gen-label)))
+    ;; If the rem-sign is negative, we need to negate rem.
+    (inst cmp rem-sign)
+    (inst ba :lt label)
+    (inst neg rem)
+    (emit-label label)))
+
+
+;;;; Comparison
 
 (macrolet
-    ((frob (name note cost prim-type sc)
+    ((define-cond-assem-rtn (name translate static-fn cmp)
        `(define-assembly-routine (,name
-				  (:note ,note)
-				  (:cost ,cost)
-				  (:translate truncate)
-				  (:policy :fast-safe)
-				  (:arg-types ,prim-type ,prim-type)
-				  (:result-types ,prim-type ,prim-type))
-				 ((:arg dividend ,sc nl0-offset)
-				  (:arg divisor ,sc nl1-offset)
+				  (:cost 10)
+				  (:return-style :full-call)
+				  (:policy :safe)
+				  (:translate ,translate)
+				  (:save-p t))
+				 ((:arg x (descriptor-reg any-reg) a0-offset)
+				  (:arg y (descriptor-reg any-reg) a1-offset)
+				  
+				  (:res res descriptor-reg a0-offset)
+				  
+				  (:temp nargs any-reg nargs-offset)
+				  (:temp cname descriptor-reg cname-offset)
+				  (:temp ocfp any-reg ocfp-offset))
+	  (inst andcc zero-tn x 3)
+	  (inst b :ne DO-STATIC-FN)
+	  (inst andcc zero-tn y 3)
+	  (inst b :eq DO-COMPARE)
+	  (inst cmp x y)
+	  
+	  DO-STATIC-FN
+	  (load-symbol cname ',static-fn)
+	  (inst li nargs (fixnum 2))
+	  (loadw code-tn cname vm:symbol-raw-function-addr-slot
+		 vm:other-pointer-type)
+	  (inst move ocfp cfp-tn)
+	  (inst j code-tn
+		(- (* vm:function-header-code-offset vm:word-bytes)
+		   vm:function-pointer-type))
+	  (inst move cfp-tn csp-tn)
+	  
+	  DO-COMPARE
+	  (inst b ,cmp done)
+	  (load-symbol res t)
+	  (inst move res null-tn)
+	  DONE)))
 
-				  (:res quo ,sc nl2-offset)
-				  (:res rem ,sc nl0-offset)
+  (define-cond-assem-rtn generic-< < two-arg-< :lt)
+  (define-cond-assem-rtn generic-<= <= two-arg-<= :le)
+  (define-cond-assem-rtn generic-> > two-arg-> :gt)
+  (define-cond-assem-rtn generic->= >= two-arg->= :ge))
 
-				  (:temp iter any-reg nl3-offset)
-				  (:temp temp non-descriptor-reg nl4-offset))
-	  )))
-  (frob fixnum-truncate "fixnum truncate" 50 tagged-num any-reg)
-  (frob signed-truncate "fixnum truncate" 60 tagged-num any-reg)
-  (frob unsigned-truncate "fixnum truncate" 55 tagged-num any-reg))
+
+(define-assembly-routine (generic-eql
+			  (:cost 10)
+			  (:return-style :full-call)
+			  (:policy :safe)
+			  (:translate eql)
+			  (:save-p t))
+			 ((:arg x (descriptor-reg any-reg) a0-offset)
+			  (:arg y (descriptor-reg any-reg) a1-offset)
+			  
+			  (:res res descriptor-reg a0-offset)
+
+			  (:temp lra descriptor-reg lra-offset)
+			  (:temp nargs any-reg nargs-offset)
+			  (:temp cname descriptor-reg cname-offset)
+			  (:temp ocfp any-reg ocfp-offset))
+  (inst cmp x y)
+  (inst b :eq RETURN-T)
+  (inst andcc zero-tn x 3)
+  (inst b :eq RETURN-NIL)
+  (inst andcc zero-tn y 3)
+  (inst b :ne DO-STATIC-FN)
+  (inst nop)
+
+  RETURN-NIL
+  (inst move res null-tn)
+  (lisp-return lra :offset 2)
+
+  DO-STATIC-FN
+  (load-symbol cname 'eql)
+  (inst li nargs (fixnum 2))
+  (loadw code-tn cname vm:symbol-raw-function-addr-slot vm:other-pointer-type)
+  (inst move ocfp cfp-tn)
+  (inst j code-tn
+	(- (* vm:function-header-code-offset vm:word-bytes)
+	   vm:function-pointer-type))
+  (inst move cfp-tn csp-tn)
+
+  RETURN-T
+  (load-symbol res t))
+
+(define-assembly-routine (generic-=
+			  (:cost 10)
+			  (:return-style :full-call)
+			  (:policy :safe)
+			  (:translate =)
+			  (:save-p t))
+			 ((:arg x (descriptor-reg any-reg) a0-offset)
+			  (:arg y (descriptor-reg any-reg) a1-offset)
+
+			  (:res res descriptor-reg a0-offset)
+
+			  (:temp lra descriptor-reg lra-offset)
+			  (:temp nargs any-reg nargs-offset)
+			  (:temp cname descriptor-reg cname-offset)
+			  (:temp ocfp any-reg ocfp-offset))
+  (inst cmp x y)
+  (inst b :eq RETURN-T)
+  (inst andcc zero-tn x 3)
+  (inst b :ne DO-STATIC-FN)
+  (inst andcc zero-tn y 3)
+  (inst b :ne DO-STATIC-FN)
+  (inst nop)
+
+  (inst move res null-tn)
+  (lisp-return lra :offset 2)
+
+  DO-STATIC-FN
+  (load-symbol cname 'two-arg-=)
+  (inst li nargs (fixnum 2))
+  (loadw code-tn cname vm:symbol-raw-function-addr-slot vm:other-pointer-type)
+  (inst move ocfp cfp-tn)
+  (inst j code-tn
+	(- (* vm:function-header-code-offset vm:word-bytes)
+	   vm:function-pointer-type))
+  (inst move cfp-tn csp-tn)
+
+  RETURN-T
+  (load-symbol res t))
+
+(define-assembly-routine (generic-/=
+			  (:cost 10)
+			  (:return-style :full-call)
+			  (:policy :safe)
+			  (:translate /=)
+			  (:save-p t))
+			 ((:arg x (descriptor-reg any-reg) a0-offset)
+			  (:arg y (descriptor-reg any-reg) a1-offset)
+
+			  (:res res descriptor-reg a0-offset)
+
+			  (:temp lra descriptor-reg lra-offset)
+			  (:temp nargs any-reg nargs-offset)
+			  (:temp cname descriptor-reg cname-offset)
+			  (:temp ocfp any-reg ocfp-offset))
+  (inst cmp x y)
+  (inst b :eq RETURN-NIL)
+  (inst andcc zero-tn x 3)
+  (inst b :ne DO-STATIC-FN)
+  (inst andcc zero-tn y 3)
+  (inst b :ne DO-STATIC-FN)
+  (inst nop)
+
+  (load-symbol res t)
+  (lisp-return lra :offset 2)
+
+  DO-STATIC-FN
+  (load-symbol cname 'two-arg-=)
+  (inst li nargs (fixnum 2))
+  (loadw code-tn cname vm:symbol-raw-function-addr-slot vm:other-pointer-type)
+  (inst move ocfp cfp-tn)
+  (inst j code-tn
+	(- (* vm:function-header-code-offset vm:word-bytes)
+	   vm:function-pointer-type))
+  (inst move cfp-tn csp-tn)
+
+  RETURN-NIL
+  (inst move res null-tn))
