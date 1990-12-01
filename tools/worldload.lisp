26736c0381a9929636d1fe61ff7ccc0270e1baa9
wlott <wlott>
1990-12-01 16:53:40 +0000

    Set the search lists vm: and assem: conditionally.

--- tools/worldload.lisp
+++ tools/worldload.lisp
@@ -16,8 +16,14 @@
 ;;;
 (setf (ext:search-list "code:") '("lisp:code/"))
 (setf (ext:search-list "c:") '("lisp:compiler/"))
-(setf (ext:search-list "vm:") '("c:mips/" "c:generic/"))
-(setf (ext:search-list "assem:") '("lisp:assembly/mips/" "lisp:assembly/"))
+(setf (ext:search-list "vm:")
+      '(#+pmax "c:mips/"
+        #+sparc "c:sparc/"
+	"c:generic/"))
+(setf (ext:search-list "assem:")
+      '(#+pmax "lisp:assembly/mips/"
+	#+sparc "lisp:assembly/sparc/"
+	"lisp:assembly/"))
 (setf (ext:search-list "hem:") '("lisp:hemlock/"))
 (setf (ext:search-list "clx:") '("lisp:clx/"))
 (setf (ext:search-list "pcl:") '("lisp:pcl/"))
