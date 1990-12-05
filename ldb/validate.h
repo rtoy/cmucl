3d8da0d5f771f7f4aba36cf16a7c3b8ae6eb884d
wlott <wlott>
1990-12-05 11:04:49 +0000

    Sun's mmu is braindamaged and can't deal with addresses in the range 0x20000000 - 0xe0000000.  So we have to put the control and binding stack elsewhere.

--- ldb/validate.h
+++ ldb/validate.h
@@ -1,4 +1,4 @@
-/* $Header: /Volumes/share2/src/cmucl/cvs2git/cvsroot/src/ldb/Attic/validate.h,v 1.4 1990/10/13 16:28:53 wlott Exp $ */
+/* $Header: /Volumes/share2/src/cmucl/cvs2git/cvsroot/src/ldb/Attic/validate.h,v 1.5 1990/12/05 11:04:49 wlott Exp $ */
 
 #if !defined(_INCLUDE_VALIDATE_H_)
 #define _INCLUDE_VALIDATE_H_
@@ -13,10 +13,20 @@
 #define DYNAMIC_1_SPACE_START	(0x0b000000)
 #define DYNAMIC_SPACE_SIZE	(0x05000000)
 
+#ifdef sparc
+#define CONTROL_STACK_START	(0x00e00000)
+#define CONTROL_STACK_SIZE	(0x00080000)
+#else
 #define CONTROL_STACK_START	(0x50000000)
 #define CONTROL_STACK_SIZE	(0x00100000)
+#endif
 
+#ifdef sparc
+#define BINDING_STACK_START	(0x00f00000)
+#define BINDING_STACK_SIZE	(0x00080000)
+#else
 #define BINDING_STACK_START	(0x60000000)
 #define BINDING_STACK_SIZE	(0x00100000)
+#endif
 
 #endif
