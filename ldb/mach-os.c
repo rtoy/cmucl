6d70976ee13fe477040605ed10822a15186599cc
wlott <wlott>
1991-05-24 19:06:00 +0000

    Initial revision

---
+++ ldb/mach-os.c
@@ -0,0 +1,167 @@
+/*
+ * $Header: /Volumes/share2/src/cmucl/cvs2git/cvsroot/src/ldb/Attic/mach-os.c,v 1.1 1991/05/24 19:04:38 wlott Exp $
+ *
+ * OS-dependent routines.  This file (along with os.h) exports an
+ * OS-independent interface to the operating system VM facilities.
+ * Suprisingly, this interface looks a lot like the Mach interface
+ * (but simpler in some places).  For some operating systems, a subset
+ * of these functions will have to be emulated.
+ *
+ * This is the Mach version.
+ *
+ */
+
+#include <stdio.h>
+#include <mach.h>
+#include "ldb.h"
+#include "os.h"
+
+#define MAX_SEGS 32
+
+static struct segment {
+    vm_address_t start;
+    vm_size_t length;
+} addr_map[MAX_SEGS];
+static int segments = -1;
+
+vm_size_t os_vm_page_size;
+
+void os_init()
+{
+	os_vm_page_size = vm_page_size;
+}
+
+void os_validate(addr, len)
+vm_address_t addr;
+vm_size_t len;
+{
+    kern_return_t res;
+
+#if defined(EXT_PAGER)
+    res = pager_vm_allocate(task_self(), &addr, len, addr==NULL);
+#else
+    res = vm_allocate(task_self(), &addr, len, addr==NULL);
+#endif
+
+    if (res != KERN_SUCCESS)
+        mach_error("Could not vm_allocate memory: ", res);
+
+    segments = -1;
+}
+
+void os_invalidate(addr, len)
+vm_address_t addr;
+vm_size_t len;
+{
+    kern_return_t res;
+
+#if defined(EXT_PAGER)
+    res = pager_vm_deallocate(task_self(), addr, len);
+#else
+    res = vm_deallocate(task_self(), addr, len);
+#endif
+
+    if (res != KERN_SUCCESS)
+        mach_error("Could not vm_allocate memory: ", res);
+
+    segments = -1;
+}
+
+vm_address_t os_map(fd, offset, addr, len)
+int fd, offset;
+vm_address_t addr;
+vm_size_t len;
+{
+    kern_return_t res;
+
+#if defined(EXT_PAGER)
+    res = pager_map_fd(fd, offset, &addr, 0, len);
+#else
+    res = map_fd(fd, offset, &addr, 0, len);
+#endif
+
+    if (res != KERN_SUCCESS)
+        mach_error("Could not map_fd memory: ", res);
+
+    segments = -1;
+
+    return addr;
+}
+
+void os_flush_icache(address, length)
+vm_address_t address;
+vm_size_t length;
+{
+#ifdef mips
+	vm_machine_attribute_val_t flush;
+	kern_return_t kr;
+
+	flush = MATTR_VAL_ICACHE_FLUSH;
+
+	kr = vm_machine_attribute(task_self(), address, length,
+				  MATTR_CACHE, &flush);
+	if (kr != KERN_SUCCESS)
+		mach_error("Could not flush the instruction cache", kr);
+#endif
+}
+
+void os_protect(address, length, protection)
+vm_address_t address;
+vm_size_t length;
+vm_prot_t protection;
+{
+	vm_protect(task_self(), address, length, FALSE, protection);
+}
+
+
+boolean valid_addr(test)
+vm_address_t test;
+{
+    vm_address_t addr;
+    vm_size_t size;
+    long bullshit;
+    int curseg;
+
+    if (segments == -1) {
+        addr = 0;
+        curseg = 0;
+
+        while (1) {
+            if (vm_region(task_self(), &addr, &size, &bullshit, &bullshit, &bullshit, &bullshit, &bullshit, &bullshit) != KERN_SUCCESS)
+                break;
+
+            if (curseg > 0 && addr_map[curseg-1].start + addr_map[curseg-1].length == addr)
+                addr_map[curseg-1].length += size;
+            else {
+                addr_map[curseg].start = addr;
+                addr_map[curseg].length = size;
+                curseg++;
+            }
+
+            addr += size;
+        }
+
+        segments = curseg;
+    }
+    
+    for (curseg = 0; curseg < segments; curseg++)
+        if (addr_map[curseg].start <= test && test < addr_map[curseg].start + addr_map[curseg].length)
+            return TRUE;
+    return FALSE;
+}
+
+static void sigbus_handler(signal, code, context)
+int signal, code;
+struct sigcontext *context;
+{
+    if(!interrupt_maybe_gc(context))
+	interrupt_handle_now(signal, code, context);
+}
+
+void os_install_interrupt_handlers()
+{
+    interrupt_install_low_level_handler(SIGBUS,sigbus_handler);
+#ifdef mips
+    interrupt_install_low_level_handler(SIGSEGV,sigbus_handler);
+#endif
+}
---
+++ ldb/rt-arch.c
@@ -0,0 +1,78 @@
+#include "ldb.h"
+#include "lisp.h"
+#include "globals.h"
+#include "validate.h"
+#include "os.h"
+#include "arch.h"
+#include "lispregs.h"
+#include "signal.h"
+
+void arch_init()
+{
+}
+
+os_vm_address_t arch_get_bad_addr(context)
+struct sigcontext *context;
+{
+    if (!SymbolValue(PSEUDO_ATOMIC_ATOMIC))
+	/* If we are not pseudo-atomic, then we could only have hit the */
+	/* trigger if the allocation pointer points beyond the trigger. */
+	return (os_vm_address_t)SymbolValue(ALLOCATION_POINTER);
+    else
+	/* Otherwise, we might be inside an allocator.  The best we can do */
+	/* is to try turning off the trigger and seeing if the problem goes */
+	/* away.  If it does, then we guessed right.  If it doesn't, we will */
+	/* end up back here, except current_auto_gc_trigger will be NULL, */
+	/* so we will take the sigbus at that time. */
+	return (os_vm_address_t)current_auto_gc_trigger;
+}
+
+void arch_skip_instruction(context)
+struct sigcontext *context;
+{
+    unsigned long pc = context->sc_iar;
+    unsigned long prev_inst = ((unsigned long *)pc)[-1];
+
+    if ((prev_inst >> 24) == 0x89) {
+	short offset = prev_inst & 0xffff;
+	context->sc_pc = pc - 4 + offset;
+    }
+    else
+	context->sc_pc = pc + 4;
+}
+
+static sigtrap_handler(signal, code, context)
+     int signal, code;
+     struct sigcontext *context;
+{
+    switch (*((unsigned short *)context->sc_iar+1)) {
+      case trap_PendingInterrupt:
+	interrput_handle_pending(context);
+	break;
+
+      case trap_Halt:
+	crap_out("%primitive halt called; the party is over.\n");
+
+      case trap_Error:
+      case trap_Cerror:
+	interrupt_internal_error(signal, code, context, code==trap_Cerror);
+	break;
+
+      case trap_Breakpoint:
+	sigsetmask(context->sc_mask);
+	fake_foreign_function_call(context);
+	handle_breakpoint(signal, code, context);
+	undo_fake_foreign_function_call(context);
+	break;
+
+      default:
+	interrupt_handle_now(signal, code, context);
+	break;
+    }
+}
+
+
+void arch_install_interrupt_handlers()
+{
+    interrupt_install_low_level_handler(SIGTRAP,sigtrap_handler);
+}
