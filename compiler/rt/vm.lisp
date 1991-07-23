6ccbeb7dd7d2bd87d557d8304c6716fd8e8b942b
ram <ram>
1991-07-23 12:06:28 +0000

    Removed initial value for *target-float-hardware*. Removed register 0 from the valid AFPA registers so that we can use it as a super-temporary for loading "immediate" operands.

--- compiler/rt/vm.lisp
+++ compiler/rt/vm.lisp
@@ -165,17 +165,19 @@
    :save-p t
    :alternate-scs (double-stack))
 
-  ;; Non-Descriptor AFPA-single-floats.
+  ;; Non-Descriptor AFPA-single-floats.  0,1 reserved for loading "immediate"
+  ;; operands.
   (AFPA-single-reg AFPA-float-registers
-   :locations (0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23
+   :locations (2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23
 	       24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40 41 42 43 44
 	       45 46 47 48 49 50 51 52 53 54 55 56 57 58 59 60 61 62 63)
    :constant-scs ()
    :save-p t
    :alternate-scs (single-stack))
-  ;; Non-Descriptor AFPA-double-floats.
+  ;; Non-Descriptor AFPA-double-floats.  0 reserved for loading "immediate"
+  ;; operands.
   (AFPA-double-reg AFPA-float-registers
-   :locations (0 2 4 6 8 10 12 14 16 18 20 22 24 26 28
+   :locations (2 4 6 8 10 12 14 16 18 20 22 24 26 28
 	       30 32 34 36 38 40 42 44 46 48 50 52 54 56 58 60 62)
    :element-size 2
    :constant-scs ()
@@ -345,9 +347,7 @@
   "An a-list for mapping simple array element types to their
   corresponding primitive types.")
 
-(defvar *target-float-hardware* :MC68881
-  "Describes the target float hardware.  Values are :mc68881, :fpa, :afpa,
-   or :any for the RT.")
+(defvar *target-float-hardware*)
 
 ;;; PRIMITIVE-TYPE -- Internal Interface.
 ;;;
