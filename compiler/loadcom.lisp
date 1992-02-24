e870c794f86ec7d814aeb25f350de9d1eb55f3fd
wlott <wlott>
1992-02-24 05:51:52 +0000

    Moved all the loads of backend specific stuff into a new file so we can use that other file to load additional backends.

--- compiler/loadcom.lisp
+++ compiler/loadcom.lisp
@@ -1,16 +1,21 @@
-;;; -*- Package: C -*-
+;;; -*- Package: C; Log: C.Log -*-
 ;;;
-;;;    Load up the compiler.
+;;; **********************************************************************
+;;; This code was written as part of the CMU Common Lisp project at
+;;; Carnegie Mellon University, and has been placed in the public domain.
+;;; If you want to use this code or any part of CMU Common Lisp, please contact
+;;; Scott Fahlman or slisp-group@cs.cmu.edu.
+;;;
+(ext:file-comment
+  "$Header: /Volumes/share2/src/cmucl/cvs2git/cvsroot/src/compiler/loadcom.lisp,v 1.36 1992/02/24 05:51:52 wlott Exp $")
+;;;
+;;; **********************************************************************
+;;;
+;;; Load up the compiler.
 ;;;
 (in-package "C")
 
-(setf *load-verbose* t)
-
-(load "vm:vm-macs")
 (load "c:backend")
-#-rt (load "vm:parms")
-#+rt (load "vm:params")
-(load "vm:objdef")
 (load "c:macros")
 (load "c:sset")
 (load "c:node")
@@ -18,7 +23,6 @@
 (load "c:ctype")
 (load "c:knownfun")
 (load "c:fndb")
-(load "vm:vm-fndb")
 (load "c:ir1util")
 (load "c:ir1tran")
 (load "c:ir1final")
@@ -26,7 +30,6 @@
 (load "c:array-tran")
 (load "c:seqtran")
 (load "c:typetran")
-(load "vm:vm-typetran")
 (load "c:float-tran")
 (load "c:saptran")
 (load "c:locall")
@@ -56,52 +59,11 @@
 (load "c:assembler")
 (load "c:assem-opt")
 (load "assem:assemfile")
-(load "assem:support")
-(load "vm:macros")
-(load "vm:utils")
 (load "c:aliencomp")
 (load "c:ltv")
 (load "c:debug-dump")
 
 (load "c:dump")
-(load "vm:core")
-
-(load "vm:vm")
-(load "vm:insts")
-#-rt (load "vm:primtype")
-(load "vm:move")
-(load "vm:sap")
-(load "vm:system")
-(load "vm:char")
-#-rt (load "vm:float")
-#+(and rt afpa) (load "vm:afpa")
-#+(and rt (not afpa)) (load "vm:mc68881")
-
-(load "vm:memory")
-(load "vm:static-fn")
-(load "vm:arith")
-(load "vm:cell")
-(load "vm:subprim")
-(load "vm:debug")
-(load "vm:c-call")
-(load "vm:print")
-(load "vm:alloc")
-(load "vm:call")
-(load "vm:nlx")
-(load "vm:values")
-(load "vm:array")
-(load "vm:pred")
-(load "vm:type-vops")
-(load "vm:vm-tran")
-
-(load "assem:assem-rtns")
-#-rt (load "assem:bit-bash")
-(load "assem:array")
-(load "assem:arith")
-(load "assem:alloc")
-
-(load "c:pseudo-vops")
-(load "vm:vm-tran")
 (load "c:debug")
 (load "c:assem-check")
 (load "c:copyprop")
@@ -109,10 +71,3 @@
 
 (load "c:eval-comp")
 (load "c:eval")
-
-#+small
-;;;
-;;; If we want a small core, blow away the meta-compile time VOP info.
-(setf (backend-parsed-vops *backend*) (make-hash-table :test #'eq))
-
-(%proclaim '(optimize (speed 1) (safety 1)))
