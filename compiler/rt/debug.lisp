bc99c95f5afa3b71280292990e30ae2f2afb4943
wlott <wlott>
1992-03-10 09:20:19 +0000

    nother typo

--- compiler/rt/debug.lisp
+++ compiler/rt/debug.lisp
@@ -7,7 +7,7 @@
 ;;; Scott Fahlman (FAHLMAN@CMUC). 
 ;;; **********************************************************************
 ;;;
-;;; $Header: /Volumes/share2/src/cmucl/cvs2git/cvsroot/src/compiler/rt/debug.lisp,v 1.3 1992/03/10 08:58:26 wlott Exp $
+;;; $Header: /Volumes/share2/src/cmucl/cvs2git/cvsroot/src/compiler/rt/debug.lisp,v 1.4 1992/03/10 09:20:19 wlott Exp $
 ;;;
 ;;; Compiler support for the new whizzy debugger.
 ;;;
@@ -62,7 +62,7 @@
 	 (offset :scs (any-reg)))
   (:results (result :scs (descriptor-reg)))
   (:arg-types system-area-pointer positive-fixnum)
-  (:result-types ,result-type)
+  (:result-types *)
   (:temporary (:scs (sap-reg) :from (:argument 0) :to :eval) base)
   (:generator 7
     (move base object)
