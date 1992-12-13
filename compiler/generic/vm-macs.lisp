74e035332af92a47eb218b77057f2f11bdb7f5b2
wlott <wlott>
1992-12-13 14:44:48 +0000

    Moved def-primitive-object into hear and changed it to expand directly into stuff to do the defknowns, allocs, refs, and sets.

--- compiler/generic/vm-macs.lisp
+++ compiler/generic/vm-macs.lisp
@@ -7,11 +7,9 @@
 ;;; Scott Fahlman or slisp-group@cs.cmu.edu.
 ;;;
 (ext:file-comment
-  "$Header: /Volumes/share2/src/cmucl/cvs2git/cvsroot/src/compiler/generic/vm-macs.lisp,v 1.7 1992/05/30 17:39:27 wlott Exp $")
+  "$Header: /Volumes/share2/src/cmucl/cvs2git/cvsroot/src/compiler/generic/vm-macs.lisp,v 1.8 1992/12/13 14:44:48 wlott Exp $")
 ;;;
 ;;; **********************************************************************
-;;;
-;;; $Header: /Volumes/share2/src/cmucl/cvs2git/cvsroot/src/compiler/generic/vm-macs.lisp,v 1.7 1992/05/30 17:39:27 wlott Exp $
 ;;;
 ;;;    This file contains some macros and constants that are object-format
 ;;; specific or are used for defining the object format.
@@ -19,8 +17,6 @@
 ;;; Written by William Lott and Christopher Hoover.
 ;;; 
 (in-package "VM")
-
-(export '(*assembly-unit-length*))
 
 
 
@@ -60,13 +56,162 @@
     `(eval-when (compile load eval)
        ,@(nreverse results))))
 
+
+;;;; Primitive object definition stuff.
+
+
+(defun remove-keywords (options keywords)
+  (cond ((null options) nil)
+	((member (car options) keywords)
+	 (remove-keywords (cddr options) keywords))
+	(t
+	 (list* (car options) (cadr options)
+		(remove-keywords (cddr options) keywords)))))
+
+(defstruct (prim-object-slot
+	    (:conc-name slot-)
+	    (:constructor make-slot (name docs rest-p offset length options))
+	    (:make-load-form-fun :just-dump-it-normally))
+  (name nil :type symbol)
+  (docs nil :type (or null simple-string))
+  (rest-p nil :type (member t nil))
+  (offset 0 :type fixnum)
+  (length 1 :type fixnum)
+  (options nil :type list))
+
+(defstruct (primitive-object
+	    (:make-load-form-fun :just-dump-it-normally))
+  (name nil :type symbol)
+  (header nil :type symbol)
+  (lowtag nil :type symbol)
+  (options nil :type list)
+  (slots nil :type list)
+  (size 0 :type fixnum)
+  (variable-length nil :type (member t nil)))
+
+
+(defvar *primitive-objects* nil)
+
+(defun %define-primitive-object (primobj)
+  (let ((name (primitive-object-name primobj)))
+    (setf *primitive-objects*
+	  (cons primobj
+		(remove name *primitive-objects*
+			:key #'primitive-object-name :test #'eq)))
+    name))
+
+
+(defmacro define-primitive-object
+	  ((name &key header lowtag alloc-trans (type t))
+	   &rest slot-specs)
+  (collect ((slots) (exports) (constants) (forms) (inits))
+    (let ((offset (if header 1 0))
+	  (variable-length nil))
+      (dolist (spec slot-specs)
+	(when variable-length
+	  (error "No more slots can follow a :rest-p slott."))
+	(destructuring-bind
+	    (slot-name &rest options
+		       &key docs rest-p (length 1) ((:type slot-type)) init
+		       (ref-known nil ref-known-p) ref-trans
+		       (set-known nil set-known-p) set-trans
+		       &allow-other-keys)
+	    (if (atom spec) (list spec) spec)
+	  (slots (make-slot slot-name docs rest-p offset length
+			    (remove-keywords options
+					     '(:docs :rest-p :length))))
+	  (let ((offset-sym (symbolicate name "-" slot-name
+					 (if rest-p "-OFFSET" "-SLOT"))))
+	    (constants `(defconstant ,offset-sym ,offset
+			  ,@(when docs (list docs))))
+	    (exports offset-sym))
+	  (when ref-trans
+	    (when ref-known-p
+	      (forms `(defknown ,ref-trans (,type) ,slot-type ,ref-known)))
+	    (forms `(def-reffer ,ref-trans ,offset ,lowtag)))
+	  (when set-trans
+	    (when set-known-p
+	      (forms `(defknown ,set-trans
+				,(if (listp set-trans)
+				     (list slot-type type)
+				     (list type slot-type))
+				,slot-type
+			,set-known)))
+	    (forms `(def-setter ,set-trans ,offset ,lowtag)))
+	  (when init
+	    (inits (cons init offset)))
+	  (when rest-p
+	    (setf variable-length t))
+	  (incf offset length)))
+      (unless variable-length
+	(let ((size (symbolicate name "-SIZE")))
+	  (constants `(defconstant ,size ,offset
+			,(format nil
+				 "Number of slots used by each ~S~
+				  ~@[~* including the header~]."
+				 name header)))
+	  (exports size)))
+      (when alloc-trans
+	(forms `(def-alloc ,alloc-trans ,offset ,variable-length ,header
+			   ,lowtag ',(inits))))
+      `(progn
+	 (export ',(exports))
+	 (eval-when (compile load eval)
+	   (%define-primitive-object
+	    ',(make-primitive-object :name name
+				     :header header
+				     :lowtag lowtag
+				     :slots (slots)
+				     :size offset
+				     :variable-length variable-length))
+	   ,@(constants))
+	 ,@(forms)))))
+
+
+
+;;;; reffer and setter definition stuff.
+
+(in-package :c)
+
+(export '(def-reffer def-setter def-alloc))
+
+(defun %def-reffer (name offset lowtag)
+  (setf (function-info-ir2-convert (function-info-or-lose name))
+	#'(lambda (node block)
+	    (ir2-convert-reffer node block name offset lowtag)))
+  name)
+
+(defmacro def-reffer (name offset lowtag)
+  `(%def-reffer ',name ,offset ,lowtag))
+
+(defun %def-setter (name offset lowtag)
+  (setf (function-info-ir2-convert (function-info-or-lose name))
+	(if (listp name)
+	    #'(lambda (node block)
+		(ir2-convert-setfer node block name offset lowtag))
+	    #'(lambda (node block)
+		(ir2-convert-setter node block name offset lowtag))))
+  name)
+
+(defmacro def-setter (name offset lowtag)
+  `(%def-setter ',name ,offset ,lowtag))
+
+(defun %def-alloc (name words variable-length header lowtag inits)
+  (setf (function-info-ir2-convert (function-info-or-lose name))
+	(if variable-length
+	    #'(lambda (node block)
+		(ir2-convert-variable-allocation node block name words header
+						 lowtag inits))
+	    #'(lambda (node block)
+		(ir2-convert-fixed-allocation node block name words header
+					      lowtag inits))))
+  name)
+
+(defmacro def-alloc (name words variable-length header lowtag inits)
+  `(%def-alloc ',name ,words ,variable-length ,header ,lowtag ,inits))
 
 
 ;;;; Some general constant definitions:
-
-;;; The number of bits per element in the assemblers code vector.
-;;;
-(defparameter *assembly-unit-length* 8)
 
 (in-package "C")
 
