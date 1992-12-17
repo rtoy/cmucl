a66c7b1f83caa27b7b17988dbc5033a88e30324c
wlott <wlott>
1992-12-17 14:35:07 +0000

    Fixed defknown of %verify-argument-count to have * for the result type instead of nil.

--- compiler/fndb.lisp
+++ compiler/fndb.lisp
@@ -7,7 +7,7 @@
 ;;; Scott Fahlman or slisp-group@cs.cmu.edu.
 ;;;
 (ext:file-comment
-  "$Header: /Volumes/share2/src/cmucl/cvs2git/cvsroot/src/compiler/fndb.lisp,v 1.51 1992/12/10 16:38:45 ram Exp $")
+  "$Header: /Volumes/share2/src/cmucl/cvs2git/cvsroot/src/compiler/fndb.lisp,v 1.52 1992/12/17 14:35:07 wlott Exp $")
 ;;;
 ;;; **********************************************************************
 ;;;
@@ -1083,7 +1083,7 @@
 (defknown %listify-rest-args (t t) list (flushable))
 (defknown %more-arg-context (t t) (values t fixnum) (flushable))
 (defknown %more-arg (t t) t)
-(defknown %verify-argument-count (t t) nil)
+(defknown %verify-argument-count (t t) *)
 (defknown %argument-count-error (t) nil)
 (defknown %unknown-values () *)
 (defknown %catch (t t) void)
