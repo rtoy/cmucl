3ef118f9efbd48279de35e10833fd2bfc4bcaa30
wlott <wlott>
1993-01-28 14:08:30 +0000

    Added standard header, and changed with-compilation-unit to with-compiler-log-file.

--- tools/clmcom.lisp
+++ tools/clmcom.lisp
@@ -1,3 +1,16 @@
+;;; -*- Package: USER -*-
+;;;
+;;; **********************************************************************
+;;; This code was written as part of the CMU Common Lisp project at
+;;; Carnegie Mellon University, and has been placed in the public domain.
+;;; If you want to use this code or any part of CMU Common Lisp, please contact
+;;; Scott Fahlman or slisp-group@cs.cmu.edu.
+;;;
+(ext:file-comment
+  "$Header: /Volumes/share2/src/cmucl/cvs2git/cvsroot/src/tools/clmcom.lisp,v 1.2 1993/01/28 14:08:30 wlott Exp $")
+;;;
+;;; **********************************************************************
+;;;
 ;;; File for compiling the Motif toolkit and related interface
 ;;; stuff.
 ;;;
@@ -6,8 +19,9 @@
 
 (pushnew :motif-toolkit *features*)
 
-(with-compilation-unit
-    (:optimize '(optimize (speed 3) (safety 1) (ext:inhibit-warnings 3)))
+(with-compiler-log-file
+    ("target:compile-motif.log"
+     :optimize '(optimize (speed 3) (safety 1) (ext:inhibit-warnings 3)))
 
  (comf "target:motif/lisp/initial" :load t)
  (comf "target:motif/lisp/internals" :load t)
@@ -15,8 +29,9 @@
  (comf "target:motif/lisp/events" :load t)
  (comf "target:motif/lisp/conversion" :load t))
 
-(with-compilation-unit
-    (:optimize '(optimize (speed 2) (ext:inhibit-warnings 2)))
+(with-compiler-log-file
+    ("target:compiler-motif.log"
+     :optimize '(optimize (speed 2) (ext:inhibit-warnings 2)))
 
   (comf "target:motif/lisp/interface-glue" :load t)
   (comf "target:motif/lisp/xt-types" :load t)
@@ -29,8 +44,8 @@
 
 (xt::build-toolkit-interface)
 
-(with-compilation-unit
-    ()
+(with-compiler-log-file
+    ("target:compile-motif.log")
   (comf "target:interface/initial" :load t)
   (comf "target:interface/interface" :load t)
   (comf "target:interface/inspect" :load t)
