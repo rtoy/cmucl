442468e0ae3692a5bb55e793496eb21be73ab1dd
ram <ram>
1993-05-13 21:34:16 +0000

    Byte compile all macros.  Use cat-if-anything-changed.

--- tools/clxcom.lisp
+++ tools/clxcom.lisp
@@ -19,31 +19,32 @@
      '(((:and :external :global)
 	(declare (optimize-interface (safety 2) (debug-info 1))))
        ((:and :external :macro)
-	(declare (optimize (safety 2))))))
-  (let ((c::*suppress-values-declaration* t))
-    (comf "target:clx/package" :load t)
-    (comf "target:clx/defsystem" :load t)
-    (comf "target:clx/depdefs" :load t)
-    (comf "target:clx/clx" :load t)
-    (comf "target:clx/dependent" :load t)
-    (comf "target:clx/macros")	; these are just macros
-    (load "target:clx/macros")
-    (comf "target:clx/bufmac")	; these are just macros
-    (load "target:clx/bufmac")
-    (comf "target:clx/buffer" :load t)
-    (comf "target:clx/display" :load t)
-    (comf "target:clx/gcontext" :load t)
-    (comf "target:clx/input" :load t)
-    (comf "target:clx/requests" :load t)
-    (comf "target:clx/fonts" :load t)
-    (comf "target:clx/graphics" :load t)
-    (comf "target:clx/text" :load t)
-    (comf "target:clx/attributes" :load t)
-    (comf "target:clx/translate" :load t)
-    (comf "target:clx/keysyms" :load t)
-    (comf "target:clx/manager" :load t)
-    (comf "target:clx/image" :load t)
-    (comf "target:clx/resource" :load t))
+	(declare (optimize (safety 2))))
+       (:macro (declare (optimize (speed 0))))))
+    (let ((c::*suppress-values-declaration* t))
+      (comf "target:clx/package" :load t)
+      (comf "target:clx/defsystem" :load t)
+      (comf "target:clx/depdefs" :load t)
+      (comf "target:clx/clx" :load t)
+      (comf "target:clx/dependent" :load t)
+      (comf "target:clx/macros")	; these are just macros
+      (load "target:clx/macros")
+      (comf "target:clx/bufmac")	; these are just macros
+      (load "target:clx/bufmac")
+      (comf "target:clx/buffer" :load t)
+      (comf "target:clx/display" :load t)
+      (comf "target:clx/gcontext" :load t)
+      (comf "target:clx/input" :load t)
+      (comf "target:clx/requests" :load t)
+      (comf "target:clx/fonts" :load t)
+      (comf "target:clx/graphics" :load t)
+      (comf "target:clx/text" :load t)
+      (comf "target:clx/attributes" :load t)
+      (comf "target:clx/translate" :load t)
+      (comf "target:clx/keysyms" :load t)
+      (comf "target:clx/manager" :load t)
+      (comf "target:clx/image" :load t)
+      (comf "target:clx/resource" :load t))
   (comf "target:code/clx-ext")
   (comf "target:hemlock/charmacs" :load t)
   (comf "target:hemlock/key-event" :load t)
@@ -52,39 +53,29 @@
   #+pcl
   (comf "target:code/inspect"))
 
-(ext:run-program
- "cat"
- (mapcar #'(lambda (x)
-	     (namestring
-	      (truename
-	       (make-pathname
-		:defaults x
-		:type (c:backend-fasl-file-type c:*target-backend*)))))
-	 '(
-	   "target:clx/package"
-	   "target:clx/depdefs"
-	   "target:clx/clx"
-	   "target:clx/dependent"
-	   "target:clx/macros"
-	   "target:clx/bufmac"
-	   "target:clx/buffer"
-	   "target:clx/display"
-	   "target:clx/gcontext"
-	   "target:clx/input"
-	   "target:clx/requests"
-	   "target:clx/fonts"
-	   "target:clx/graphics"
-	   "target:clx/text"
-	   "target:clx/attributes"
-	   "target:clx/translate"
-	   "target:clx/keysyms"
-	   "target:clx/manager"
-	   "target:clx/image"
-	   "target:clx/resource"
-	   "target:code/clx-ext"
-	   "target:hemlock/charmacs"
-	   "target:hemlock/key-event"
-	   "target:hemlock/keysym-defs"))
- :if-output-exists :supersede
- :output (make-pathname :defaults "target:clx/clx-library"
-			:type (c:backend-fasl-file-type c:*target-backend*)))
+(cat-if-anything-changed
+ "target:clx/clx-library"
+ "target:clx/package"
+ "target:clx/depdefs"
+ "target:clx/clx"
+ "target:clx/dependent"
+ "target:clx/macros"
+ "target:clx/bufmac"
+ "target:clx/buffer"
+ "target:clx/display"
+ "target:clx/gcontext"
+ "target:clx/input"
+ "target:clx/requests"
+ "target:clx/fonts"
+ "target:clx/graphics"
+ "target:clx/text"
+ "target:clx/attributes"
+ "target:clx/translate"
+ "target:clx/keysyms"
+ "target:clx/manager"
+ "target:clx/image"
+ "target:clx/resource"
+ "target:code/clx-ext"
+ "target:hemlock/charmacs"
+ "target:hemlock/key-event"
+ "target:hemlock/keysym-defs")
