ba6afac03d4a9f7779c40117ed6d5f241d819575
wlott <wlott>
1993-05-25 00:52:44 +0000

    Fixed large-alloc to dink the suspends_disabled_count around the alloc and use call_into_c instead of calling allocate_large_object directly so that foreign_fn_call_active gets updates.

--- assembly/mips/alloc.lisp
+++ assembly/mips/alloc.lisp
@@ -7,7 +7,7 @@
 ;;; Scott Fahlman or slisp-group@cs.cmu.edu.
 ;;;
 (ext:file-comment
-  "$Header: /Volumes/share2/src/cmucl/cvs2git/cvsroot/src/assembly/mips/alloc.lisp,v 1.5 1993/05/21 18:13:17 wlott Exp $")
+  "$Header: /Volumes/share2/src/cmucl/cvs2git/cvsroot/src/assembly/mips/alloc.lisp,v 1.6 1993/05/25 00:52:44 wlott Exp $")
 ;;;
 ;;; **********************************************************************
 ;;;
@@ -134,15 +134,29 @@
   ;; be large.
   (emit-label large-alloc-entry)
 
-  ;; ### start atomic region.
+  ;; Disable suspends.
+  (loadw temp mutator-tn mutator-suspends-disabled-count-slot)
+  (inst addu temp temp 1)
+  (storew temp mutator-tn mutator-suspends-disabled-count-slot)
 
   (with-regs-saved (a1 a2 a3 a4 a5 fdefn lexenv ra)
-    (inst jal (make-fixup "allocate_large_object" :foreign))
+    (inst li temp (make-fixup "allocate_large_object" :foreign))
+    (inst jal (make-fixup "call_into_c" :foreign))
     (inst nop))
 
   (inst move result temp)
 
-  ;; ### end atomic region
-  )
+  ;; Re-enable suspends.
+  (loadw temp mutator-tn mutator-suspends-disabled-count-slot)
+  (inst subu temp temp 1)
+  (inst bne temp zero-tn done)
+  (storew temp mutator-tn mutator-suspends-disabled-count-slot)
+  ;; Check to see if any are pending.
+  (loadw temp mutator-tn mutator-suspend-pending-slot)
+  (inst beq temp zero-tn done)
+  (inst nop)
+
+  (inst break pending-interrupt-trap)
+  DONE)
 
 ); #+gengc progn
