2d500643b5f95e76c329c4469b1a74ffaf339151
ram <ram>
1993-08-19 14:06:37 +0000

    Changed directory-translation skeleton to be target: search-list. Changed #+hemlock to be (when (member :hemlock *features*) ...)

--- code/generic-site.lisp
+++ code/generic-site.lisp
@@ -7,7 +7,7 @@
 ;;; Scott Fahlman or slisp-group@cs.cmu.edu.
 ;;;
 (ext:file-comment
-  "$Header: /Volumes/share2/src/cmucl/cvs2git/cvsroot/src/code/generic-site.lisp,v 1.5 1992/05/30 12:56:53 ram Exp $")
+  "$Header: /Volumes/share2/src/cmucl/cvs2git/cvsroot/src/code/generic-site.lisp,v 1.6 1993/08/19 14:06:37 ram Exp $")
 ;;;
 ;;; **********************************************************************
 ;;;
@@ -28,30 +28,27 @@
 cmucl-bugs@cs.cmu.edu.
 Loaded subsystems:"))
 
-;;; The following Hemlock initializations will error if run in a core without
-;;; hemlock.
-#+hemlock (progn
 ;;; If you have sources installed on your system, un-comment the following form
 ;;; and change it to point to the source location.  This will allow the Hemlock
-;;; "Edit Definition" command to find sources for functions in the core.  If
-;;; this doesn't work, check that first part of the translation really does
-;;; have the correct prefix.
-;;;
+;;; "Edit Definition" command and the debugger to find sources for functions in
+;;; the core.
 #|
-(ed::add-definition-dir-translation
- "/afs/cs.cmu.edu/project/clisp-1/sun4c_41/15/"
- "<your source location here>")
+(setf (search-list "target:") "<the source tree root>/")
 |#
 
+;;; The following Hemlock initializations will error if run in a core without
+;;; hemlock.
+;;;
 ;;; Use standard X fonts for Hemlock, since the default ones may not work
 ;;; everywhere.  By default, Hemlock used 8x13 and a non-standard underline
 ;;; font, 8x13u (which is part of the distribution.)  Unfortunately, we don't
 ;;; have source for this font, since it was created by hand-editing the
 ;;; bitmaps.
 ;;;
-(hi:setv ed::open-paren-highlighting-font "*-courier-bold-r-normal--*-120-*")
-(hi:setv ed::default-font "*-courier-medium-r-normal--*-120-*")
-(hi:setv ed::active-region-highlighting-font
-	 "*-courier-medium-o-normal--*-120-*")
-
-); #+hemlock
+(when (member :hemlock *features*)
+  (hi::%set-variable-value 'ed::open-paren-highlighting-font :global nil
+			   "*-courier-bold-r-normal--*-120-*")
+  (hi::%set-variable-value 'ed::default-font :global nil
+			   "*-courier-medium-r-normal--*-120-*")
+  (hi::%set-variable-value 'ed::active-region-highlighting-font :global nil
+			   "*-courier-medium-o-normal--*-120-*"))
