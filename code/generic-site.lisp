72f405678d292dd179fc6957ccb367a10646ee5e
ram <ram>
1993-11-16 19:13:10 +0000

    Declare hi::%set-variable-value to be a function so that we don't get undefined warnings on loading into a non-hemlock lisp.

--- code/generic-site.lisp
+++ code/generic-site.lisp
@@ -7,7 +7,7 @@
 ;;; Scott Fahlman or slisp-group@cs.cmu.edu.
 ;;;
 (ext:file-comment
-  "$Header: /Volumes/share2/src/cmucl/cvs2git/cvsroot/src/code/generic-site.lisp,v 1.6 1993/08/19 14:06:37 ram Exp $")
+  "$Header: /Volumes/share2/src/cmucl/cvs2git/cvsroot/src/code/generic-site.lisp,v 1.7 1993/11/16 19:13:10 ram Exp $")
 ;;;
 ;;; **********************************************************************
 ;;;
@@ -45,6 +45,7 @@ Loaded subsystems:"))
 ;;; have source for this font, since it was created by hand-editing the
 ;;; bitmaps.
 ;;;
+(declaim (function hi::%set-variable-value))
 (when (member :hemlock *features*)
   (hi::%set-variable-value 'ed::open-paren-highlighting-font :global nil
 			   "*-courier-bold-r-normal--*-120-*")
