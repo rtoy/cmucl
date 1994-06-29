4283cbdaf205cae8b6d08f6ad5c163901cbdbbf4
hallgren <hallgren>
1994-06-29 21:42:02 +0000

    Initial revision

---
+++ code/irix-os.lisp
@@ -0,0 +1,75 @@
+;;; -*- Mode: Lisp; Package: Lisp; Log: code.log -*-
+;;;
+;;; **********************************************************************
+;;; This code was written as part of the CMU Common Lisp project at
+;;; Carnegie Mellon University, and has been placed in the public domain.
+;;; If you want to use this code or any part of CMU Common Lisp, please contact
+;;; Scott Fahlman or slisp-group@cs.cmu.edu.
+;;;
+(ext:file-comment
+  "$Header: /Volumes/share2/src/cmucl/cvs2git/cvsroot/src/code/irix-os.lisp,v 1.1 1994/06/29 21:41:11 hallgren Exp $")
+;;;
+;;; **********************************************************************
+;;;
+;;; OS interface functions for CMU CL under IRIX.
+;;;
+;;;
+(in-package "SYSTEM")
+(use-package "EXTENSIONS")
+(export '(get-system-info get-page-size os-init))
+
+(pushnew :irix *features*)
+(setq *software-type* "IRIX")
+
+(defvar *software-version* nil "Version string for supporting software")
+
+(defun software-version ()
+  "Returns a string describing version of the supporting software."
+  (unless *software-version*
+    (setf *software-version*
+          (let ((version-line
+                 (with-output-to-string (stream)
+                   (run-program "/usr/bin/uname"
+                                '("-r" )
+                                :output stream
+                                :pty nil
+                                :error nil))))
+            (subseq version-line 0 (1- (length version-line))))))
+  *software-version*)
+
+
+;;; OS-INIT -- interface.
+;;;
+;;; Other OS dependent initializations.
+;;; 
+(defun os-init ()
+  ;; Decache version on save, because it might not be the same when we restart.
+  (setf *software-version* nil))
+
+;;; GET-SYSTEM-INFO  --  Interface
+;;;
+;;;    Return system time, user time and number of page faults.
+;;;
+(defun get-system-info ()
+  (multiple-value-bind
+      (err? utime stime maxrss ixrss idrss isrss minflt majflt)
+      (unix:unix-getrusage unix:rusage_self)
+    (declare (ignore maxrss ixrss idrss isrss minflt))
+    (cond ((null err?)
+	   (error "Unix system call getrusage failed: ~A."
+		  (unix:get-unix-error-msg utime)))
+	  (T
+	   (values utime stime majflt)))))
+
+
+;;; GET-PAGE-SIZE  --  Interface
+;;;
+;;;    Return the system page size.
+;;;
+(defun get-page-size ()
+  (multiple-value-bind (val err)
+		       (unix:unix-getpagesize)
+    (unless val
+      (error "Getpagesize failed: ~A" (unix:get-unix-error-msg err)))
+    val))
+
