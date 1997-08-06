84d32efa47367c758c33d69df3286450b91bb00e
dtc <dtc>
1997-08-06 12:29:15 +0000

    The name argument to compile may be a cons, e.g. '(setf foo).

--- compiler/fndb.lisp
+++ compiler/fndb.lisp
@@ -5,7 +5,7 @@
 ;;; Carnegie Mellon University, and has been placed in the public domain.
 ;;;
 (ext:file-comment
-  "$Header: /Volumes/share2/src/cmucl/cvs2git/cvsroot/src/compiler/fndb.lisp,v 1.68.2.1 1997/06/11 18:50:49 dtc Exp $")
+  "$Header: /Volumes/share2/src/cmucl/cvs2git/cvsroot/src/compiler/fndb.lisp,v 1.68.2.2 1997/08/06 12:29:15 dtc Exp $")
 ;;;
 ;;; **********************************************************************
 ;;;
@@ -1025,7 +1025,7 @@
 ;;;; In the "Miscellaneous" Chapter.
 
 ;;; ### Compiler interface non-standard...
-(defknown compile (symbol &optional (or list function null))
+(defknown compile ((or symbol cons) &optional (or list function null))
   (values (or function symbol cons) boolean boolean))
 
 (deftype optional-filename () '(or filename (member t nil)))
