2c654c8b7b772fe23b0d6841f649ea76189ac2a7
dtc <dtc>
1997-08-16 09:32:44 +0000

    Bring 'documentation closer to ANSI CL: now accepts function, package, and structure-class objects, and calls pcl::documentation if necessary. New function 'function-doc to return a functions documentation if possible.

--- code/misc.lisp
+++ code/misc.lisp
@@ -5,7 +5,7 @@
 ;;; Carnegie Mellon University, and has been placed in the public domain.
 ;;;
 (ext:file-comment
-  "$Header: /Volumes/share2/src/cmucl/cvs2git/cvsroot/src/code/misc.lisp,v 1.23 1997/01/18 14:31:13 ram Exp $")
+  "$Header: /Volumes/share2/src/cmucl/cvs2git/cvsroot/src/code/misc.lisp,v 1.24 1997/08/16 09:32:44 dtc Exp $")
 ;;;
 ;;; **********************************************************************
 ;;;
@@ -28,23 +28,83 @@
 
 (in-package "LISP")
 
+;;; cobbled from stuff in describe.lisp.
+(defun function-doc(x)
+  (let ((name
+	 (case (kernel:get-type x)
+	   (#.vm:closure-header-type
+	    (kernel:%function-name (%closure-function x)))
+	   ((#.vm:function-header-type #.vm:closure-function-header-type)
+	    (kernel:%function-name x))
+	   (#.vm:funcallable-instance-header-type
+	    (typecase x
+	      (kernel:byte-function
+	       (c::byte-function-name x))
+	      (kernel:byte-closure
+	       (c::byte-function-name (byte-closure-function x)))
+	      (eval:interpreted-function
+	       (multiple-value-bind 
+		     (exp closure-p dname)
+		   (eval:interpreted-function-lambda-expression x)
+		 (declare (ignore exp closure-p))
+		 dname))
+	      (t ;; funcallable-instance
+	       (kernel:%function-name
+		(kernel:funcallable-instance-function x))))))))
+    (when (and name (typep name '(or symbol cons)))
+      (values (info function documentation name)))))
 
-(defun documentation (name doc-type)
-  "Returns the documentation string of Doc-Type for Name, or NIL if
+(eval-when (compile load eval)
+  (shadow 'documentation "PCL"))
+
+(defun documentation (x doc-type)
+  "Returns the documentation string of Doc-Type for X, or NIL if
   none exists.  System doc-types are VARIABLE, FUNCTION, STRUCTURE, TYPE,
-  and SETF."
-  (values
-   (case doc-type
-     (variable (info variable documentation name))
-     (function (info function documentation name))
-     (structure
-      (when (eq (info type kind name) :instance)
-	(info type documentation name)))
-     (type
-      (info type documentation name))
-     (setf (info setf documentation name))
-     (t
-      (cdr (assoc doc-type (info random-documentation stuff name)))))))
+  SETF, and T."
+  (flet ((try-generic-doc (x doc-type)
+	   (and (fboundp 'pcl::documentation)
+		(pcl::documentation x doc-type)))
+	 ;; CMUCL random-documentation.
+	 (try-cmucl-random-doc (x doc-type)
+	   (declare (symbol doc-type))
+	   (cdr (assoc doc-type
+		       (values (info random-documentation stuff x))))))
+    (case doc-type
+      (variable 
+       (typecase x
+	 (symbol (values (info variable documentation x)))
+	 (t (try-generic-doc x doc-type))))
+      (function
+       (typecase x
+	 (symbol (values (info function documentation x)))
+	 (function (function-doc x))
+	 (list ;; Must be '(setf symbol)
+	  (values (info function documentation (cadr x))))
+	 (t (try-generic-doc x doc-type))))
+      (structure
+       (typecase x
+	 (symbol (when (eq (info type kind x) :instance)
+		   (values (info type documentation x))))
+	 (t (try-generic-doc x doc-type))))
+      (type
+       (typecase x
+	 (structure-class (values (info type documentation (class-name x))))
+	 (t (or (and (typep x 'symbol) (values (info type documentation x)))
+		(try-generic-doc x doc-type)))))
+      (setf (info setf documentation x))
+      ((t)
+       (typecase x
+	 (function (function-doc x))
+	 (package (package-doc-string x))
+	 (structure-class (values (info type documentation (class-name x))))
+	 (symbol (or (try-generic-doc x doc-type)
+		     (try-cmucl-random-doc x doc-type)))
+	 (t (try-generic-doc x doc-type))))
+      (t
+       (typecase x
+	 (symbol (or (try-generic-doc x doc-type)
+		     (try-cmucl-random-doc x doc-type)))
+	 (t (try-generic-doc x doc-type)))))))
 
 (defun %set-documentation (name doc-type string)
   (case doc-type
