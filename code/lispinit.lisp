91140f7d50c56fa5ffbb93229d3a062fb3f40ede
dtc <dtc>
1997-09-15 16:47:04 +0000

    Defvar for *current-unwind-block* should be *current-unwind-protect-block*.

--- code/lispinit.lisp
+++ code/lispinit.lisp
@@ -5,7 +5,7 @@
 ;;; Carnegie Mellon University, and has been placed in the public domain.
 ;;;
 (ext:file-comment
-  "$Header: /Volumes/share2/src/cmucl/cvs2git/cvsroot/src/code/lispinit.lisp,v 1.49 1997/01/18 14:30:42 ram Exp $")
+  "$Header: /Volumes/share2/src/cmucl/cvs2git/cvsroot/src/code/lispinit.lisp,v 1.50 1997/09/15 16:47:04 dtc Exp $")
 ;;;
 ;;; **********************************************************************
 ;;;
@@ -64,7 +64,7 @@
 (progn
 
 (defvar *current-catch-block*)
-(defvar *current-unwind-block*)
+(defvar *current-unwind-protect-block*)
 (defvar *free-interrupt-context-index*)
 
 ); #-gengc progn
