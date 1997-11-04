cfeea252cdcf32b2d653f3fb695b0f80c1264610
dtc <dtc>
1997-11-04 14:52:27 +0000

    Only need to default to trace encapsulation with CGC.

--- code/ntrace.lisp
+++ code/ntrace.lisp
@@ -5,7 +5,7 @@
 ;;; Carnegie Mellon University, and has been placed in the public domain.
 ;;;
 (ext:file-comment
-  "$Header: /Volumes/share2/src/cmucl/cvs2git/cvsroot/src/code/ntrace.lisp,v 1.13 1997/01/18 14:30:50 ram Exp $")
+  "$Header: /Volumes/share2/src/cmucl/cvs2git/cvsroot/src/code/ntrace.lisp,v 1.14 1997/11/04 14:52:27 dtc Exp $")
 ;;;
 ;;; **********************************************************************
 ;;;
@@ -31,7 +31,7 @@
   "If the trace indentation exceeds this value, then indentation restarts at
    0.")
 
-(defvar *trace-encapsulate-default* #-x86 :default #+x86 t
+(defvar *trace-encapsulate-default* #-cgc :default #+cgc t
   "The default value for the :ENCAPSULATE option to trace.")
 
 
