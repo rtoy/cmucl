0acc75a8fc2dca8cc6a7d4cbe547d3b2c5115c68
dtc <dtc>
1998-12-20 04:22:54 +0000

    Add CMUCL style file-comments; from Peter Van Eynde.

--- motif/lisp/internals.lisp
+++ motif/lisp/internals.lisp
@@ -3,7 +3,11 @@
 ;;; **********************************************************************
 ;;; This code was written as part of the CMU Common Lisp project at
 ;;; Carnegie Mellon University, and has been placed in the public domain.
-;;; If you want to use this code or any part of CMU Common Lisp, please contact;;; Scott Fahlman or slisp-group@cs.cmu.edu.
+;;; If you want to use this code or any part of CMU Common Lisp, please
+;;; contact Scott Fahlman or slisp-group@cs.cmu.edu.
+;;;
+(ext:file-comment
+  "$Header: /Volumes/share2/src/cmucl/cvs2git/cvsroot/src/motif/lisp/internals.lisp,v 1.7 1998/12/20 04:22:54 dtc Exp $")
 ;;;
 ;;; **********************************************************************
 ;;;
--- motif/lisp/prototypes.lisp
+++ motif/lisp/prototypes.lisp
@@ -3,7 +3,11 @@
 ;;; **********************************************************************
 ;;; This code was written as part of the CMU Common Lisp project at
 ;;; Carnegie Mellon University, and has been placed in the public domain.
-;;; If you want to use this code or any part of CMU Common Lisp, please contact;;; Scott Fahlman or slisp-group@cs.cmu.edu.
+;;; If you want to use this code or any part of CMU Common Lisp, please
+;;; contact Scott Fahlman or slisp-group@cs.cmu.edu.
+;;;
+(ext:file-comment
+  "$Header: /Volumes/share2/src/cmucl/cvs2git/cvsroot/src/motif/lisp/prototypes.lisp,v 1.9 1998/12/20 04:22:54 dtc Rel $")
 ;;;
 ;;; **********************************************************************
 ;;;
