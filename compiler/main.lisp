b7a3dd54ffb8392de4de1ca3e52ff7dfaca92d0f
pw <pw>
2000-04-05 17:27:01 +0000

    Return proper ANSI specified values from compile-file depending on error-severity. From Pierre Mai via Ray Toy.

--- compiler/main.lisp
+++ compiler/main.lisp
@@ -5,7 +5,7 @@
 ;;; Carnegie Mellon University, and has been placed in the public domain.
 ;;;
 (ext:file-comment
-  "$Header: /Volumes/share2/src/cmucl/cvs2git/cvsroot/src/compiler/main.lisp,v 1.114 1999/11/25 16:03:43 dtc Exp $")
+  "$Header: /Volumes/share2/src/cmucl/cvs2git/cvsroot/src/compiler/main.lisp,v 1.115 2000/04/05 17:27:01 pw Exp $")
 ;;;
 ;;; **********************************************************************
 ;;;
@@ -1781,7 +1781,7 @@
 		(or (probe-file output-file-name) output-file-name)
 		nil)
 	    (not (null error-severity))
-	    (if (member error-severity '(:warning :error)) t nil))))
+	    (if (eq error-severity :error) t nil))))
 
 ;;;; COMPILE and UNCOMPILE:
 
