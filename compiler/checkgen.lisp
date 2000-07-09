0cf36387ce28349a18d1b62a0558ef0847d295e3
dtc <dtc>
2000-07-09 14:00:18 +0000

    Enhancement to continuation-check-types: even when the proven type represents an unknown number of values, if Cont's destination receives only a single value, generate a :hairy type check for the single-values-type of the asserted type.

--- compiler/checkgen.lisp
+++ compiler/checkgen.lisp
@@ -5,7 +5,7 @@
 ;;; Carnegie Mellon University, and has been placed in the public domain.
 ;;;
 (ext:file-comment
-  "$Header: /Volumes/share2/src/cmucl/cvs2git/cvsroot/src/compiler/checkgen.lisp,v 1.24.2.3 2000/07/06 12:56:58 dtc Exp $")
+  "$Header: /Volumes/share2/src/cmucl/cvs2git/cvsroot/src/compiler/checkgen.lisp,v 1.24.2.4 2000/07/09 14:00:18 dtc Exp $")
 ;;;
 ;;; **********************************************************************
 ;;;
@@ -233,6 +233,10 @@
 ;;; derived type and the asserted type, then we check for the negation of this
 ;;; type instead.
 ;;;
+;;; When the proven type represents an unknown number of values, but Cont's
+;;; destination receives only a single value, a :hairy type check is
+;;; generated for the single-values-type of the asserted type.
+;;;
 (defun continuation-check-types (cont)
   (declare (type continuation cont))
   (let ((atype (continuation-asserted-type cont))
@@ -256,8 +260,10 @@
 	     (assert (values-type-p atype))
 	     (assert (null (args-type-required atype)))
 	     (maybe-negate-check cont (args-type-optional atype) nil))
+	    ((or (exit-p dest) (return-p dest) (mv-combination-p dest))
+	     (values :too-hairy nil))
 	    (t
-	     (values :too-hairy nil))))))
+	     (maybe-negate-check cont (list (single-value-type atype)) t))))))
 
 
 ;;; Probable-Type-Check-P  --  Internal
