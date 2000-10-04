93e6804f84cdc699baa48148bc98953eddc41934
dtc <dtc>
2000-10-04 15:53:19 +0000

    Correct the handling of non-keyword keyword argument names, ensuring they are correctly quoted (from Pierre R. Mai), and correcting the result type of make-keyword which is in general a symbol not a keyword.

--- pcl/boot.lisp
+++ pcl/boot.lisp
@@ -26,7 +26,7 @@
 ;;;
 
 (ext:file-comment
- "$Header: /Volumes/share2/src/cmucl/cvs2git/cvsroot/src/pcl/boot.lisp,v 1.21 2000/08/01 15:04:31 pw Exp $")
+ "$Header: /Volumes/share2/src/cmucl/cvs2git/cvsroot/src/pcl/boot.lisp,v 1.22 2000/10/04 15:53:19 dtc Exp $")
 
 (in-package :pcl)
 
@@ -899,7 +899,7 @@ work during bootstrapping.
 				   (if (consp (car var))
 				       (values (caar var) (cadar var))
 				       (values (make-keyword (car var)) (car var)))
-				 `((,key (get-key-arg1 ,keyword ,args-tail))
+				 `((,key (get-key-arg1 ',keyword ,args-tail))
 				   (,variable (if (consp ,key)
 						  (car ,key)
 						  ,(cadr var))))))
@@ -908,7 +908,7 @@ work during bootstrapping.
 				   (if (consp (car var))
 				       (values (caar var) (cadar var))
 				       (values (make-keyword (car var)) (car var)))
-				 `((,key (get-key-arg1 ,keyword ,args-tail))
+				 `((,key (get-key-arg1 ',keyword ,args-tail))
 				   (,(caddr var) ,key)
 				   (,variable (if (consp ,key)
 						  (car ,key)
