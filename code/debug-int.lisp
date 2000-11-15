0fac6e007ad6254a61c7c93b8ff009c685dda11b
pw <pw>
2000-11-15 11:46:14 +0000

    Tailor the stack trace ambiguity code in x86-call-context a bit for FreeBSD4.

--- code/debug-int.lisp
+++ code/debug-int.lisp
@@ -5,7 +5,7 @@
 ;;; Carnegie Mellon University, and has been placed in the public domain.
 ;;;
 (ext:file-comment
-  "$Header: /Volumes/share2/src/cmucl/cvs2git/cvsroot/src/code/debug-int.lisp,v 1.71.2.4 2000/11/06 18:08:37 dtc Exp $")
+  "$Header: /Volumes/share2/src/cmucl/cvs2git/cvsroot/src/code/debug-int.lisp,v 1.71.2.5 2000/11/15 11:46:14 pw Exp $")
 ;;;
 ;;; **********************************************************************
 ;;;
@@ -947,10 +947,15 @@
 						   :depth (1+ depth)))
 		   (c-path-fp (x86-call-context c-ocfp :depth (1+ depth))))
 	       (cond ((and lisp-path-fp c-path-fp)
-		      ;; Both still seem valid - choose the lisp fram.
+		      ;; Both still seem valid - choose the lisp frame.
 		      (when (zerop depth)
 			(format t "Debug: Both still valid ~s ~s ~s ~s~%"
 				lisp-ocfp lisp-ra c-ocfp c-ra))
+		      #+FreeBSD4
+		      (if (sap> lisp-ocfp c-ocfp)
+			  (values lisp-ra lisp-ocfp)
+			  (values c-ra c-ocfp))
+		      #-FreeBSD4
 		      (values lisp-ra lisp-ocfp))
 		     (lisp-path-fp
 		      ;; The lisp convention is looking good.
