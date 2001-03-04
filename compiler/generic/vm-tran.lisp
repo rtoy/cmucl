304ba552816e49119cc35c1bc694750c54501ffe
pw <pw>
2001-03-04 20:25:04 +0000

    Fix spelling typo in comment line.

--- compiler/generic/vm-tran.lisp
+++ compiler/generic/vm-tran.lisp
@@ -5,7 +5,7 @@
 ;;; Carnegie Mellon University, and has been placed in the public domain.
 ;;;
 (ext:file-comment
-  "$Header: /Volumes/share2/src/cmucl/cvs2git/cvsroot/src/compiler/generic/vm-tran.lisp,v 1.41 2000/10/21 13:08:49 dtc Exp $")
+  "$Header: /Volumes/share2/src/cmucl/cvs2git/cvsroot/src/compiler/generic/vm-tran.lisp,v 1.42 2001/03/04 20:25:04 pw Exp $")
 ;;;
 ;;; **********************************************************************
 ;;;
@@ -41,7 +41,7 @@
   `(%instance-set ,x 0 (the layout ,val)))
 
 
-;;;; Charater support.
+;;;; Character support.
 
 ;;; There are really only base-chars.
 ;;;
