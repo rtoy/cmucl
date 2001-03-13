b290872e5f731073fc8675e69d1500760b5c3b48
pw <pw>
2001-03-13 16:49:33 +0000

    Aid removal of extraneous non-CL exports and moving of get-setf-method to extensions package.

---
+++ bootfiles/18c/boot3.lisp
@@ -0,0 +1,15 @@
+;;; 3/13/01 Got rid of some symbols that didn't belong in CL package.
+;;; Moved get-setf-method to extensions package since it is still used.
+;;; Should be easy enough to flush later as it just calls
+;;; get-setf-expansion checking for a single store value.
+;;;
+;;; Load this into compiler before attempting to worldcom/comcom.
+
+(unexport '(GET-SETF-METHOD-MULTIPLE-VALUE
+	    LOGICAL-PATHNAME-P
+	    DEFINE-SETF-METHOD
+	    HASH-TABLE-WEAK-P
+	    GET-SETF-METHOD)
+	  :lisp)
+
+(load "target:code/exports.lisp")
