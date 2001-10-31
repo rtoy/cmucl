41e757c093e156b3f1b927c0dbfe8f2bc280d7b6
toy <toy>
2001-10-31 19:21:36 +0000

    Bootstrap file for the sparc pseudo-atomic-trap change for internals.h.

---
+++ bootfiles/18c/boot6.lisp
@@ -0,0 +1,10 @@
+;; Bootstrap the pseudo-atomic trap genesis change to put
+;; trap_PseudoAtomic into internals.h.
+#+sparc
+(load "target:code/exports.lisp")
+
+(in-package :sparc)
+
+#+sparc
+(defconstant pseudo-atomic-trap 16)
+
