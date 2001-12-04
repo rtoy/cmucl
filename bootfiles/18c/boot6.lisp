2357df8934e585ff4d0ccf1be4f8e7b1c4e84c76
toy <toy>
2001-12-04 22:25:02 +0000

    This causes problems when building on x86, so arrange it better for sparc only.  Problem and solution from Eric Marsden.

--- bootfiles/18c/boot6.lisp
+++ bootfiles/18c/boot6.lisp
@@ -1,10 +1,8 @@
 ;; Bootstrap the pseudo-atomic trap genesis change to put
 ;; trap_PseudoAtomic into internals.h.
 #+sparc
-(load "target:code/exports.lisp")
-
-(in-package :sparc)
-
-#+sparc
-(defconstant pseudo-atomic-trap 16)
+(progn
+  (load "target:code/exports.lisp")
+  (in-package :sparc)
+  (defconstant pseudo-atomic-trap 16))
 
