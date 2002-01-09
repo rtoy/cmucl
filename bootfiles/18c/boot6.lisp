e47856bc07a6cb6746a03f77b16793d75762b82b
toy <toy>
2002-01-09 20:32:56 +0000

    This should work better now for building on Sparc and non-sparc platforms.

--- bootfiles/18c/boot6.lisp
+++ bootfiles/18c/boot6.lisp
@@ -1,8 +1,9 @@
 ;; Bootstrap the pseudo-atomic trap genesis change to put
 ;; trap_PseudoAtomic into internals.h.
 #+sparc
-(progn
-  (load "target:code/exports.lisp")
-  (in-package :sparc)
-  (defconstant pseudo-atomic-trap 16))
+(load "target:code/exports.lisp")
+#+sparc
+(in-package :sparc)
+#+sparc
+(defconstant pseudo-atomic-trap 16))
 
