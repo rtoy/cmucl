54d63fdef4e0b2d4041375ff474a8af383f59bb1
toy <toy>
2002-11-13 17:14:18 +0000

    Add some #defines and macros to make it easier to create (and understand!) SC_OFFSETs in assembly files.

--- lisp/sparc-lispregs.h
+++ lisp/sparc-lispregs.h
@@ -1,4 +1,4 @@
-/* $Header: /Volumes/share2/src/cmucl/cvs2git/cvsroot/src/lisp/sparc-lispregs.h,v 1.5 2000/10/27 19:42:31 dtc Exp $ */
+/* $Header: /Volumes/share2/src/cmucl/cvs2git/cvsroot/src/lisp/sparc-lispregs.h,v 1.6 2002/11/13 17:14:18 toy Exp $ */
 
 
 
@@ -32,41 +32,87 @@
 
 #endif
 
+#define GREG_NUM(num) (num)
+#define OREG_NUM(num) ((num)+8)
+#define LREG_NUM(num) ((num)+16)
+#define IREG_NUM(num) ((num)+24)
+
 #define NREGS	(32)
 
+/*
+ * Define all the Lisp registers appropriately for assembly and C.
+ * The registers ending with NUM are meant for use in assembly
+ * routines where we need the register number for getting SC_OFFSET
+ */
 #define reg_ZERO	GREG(0)
+#define reg_ZERO_NUM	GREG_NUM(0)
 #define reg_ALLOC	GREG(1)
+#define reg_ALLOC_NUM	GREG_NUM(1)
 #define reg_NIL		GREG(2)
+#define reg_NIL_NUM	GREG_NUM(2)
 #define reg_CSP		GREG(3)
+#define reg_CSP_NUM	GREG_NUM(3)
 #define reg_CFP		GREG(4)
+#define reg_CFP_NUM	GREG_NUM(4)
 #define reg_BSP		GREG(5)
+#define reg_BSP_NUM	GREG_NUM(5)
 /* %g6 and %g7 are supposed to be reserved for the system */
 
 #define reg_NL0		OREG(0)
+#define reg_NL0_NUM	OREG_NUM(0)
 #define reg_NL1		OREG(1)
+#define reg_NL1_NUM	OREG_NUM(1)
 #define reg_NL2		OREG(2)
+#define reg_NL2_NUM	OREG_NUM(2)
 #define reg_NL3		OREG(3)
+#define reg_NL3_NUM	OREG_NUM(3)
 #define reg_NL4		OREG(4)
+#define reg_NL4_NUM	OREG_NUM(4)
 #define reg_NL5		OREG(5)
+#define reg_NL5_NUM	OREG_NUM(5)
 #define reg_NSP		OREG(6)
+#define reg_NSP_NUM	OREG_NUM(6)
 #define reg_NARGS	OREG(7)
+#define reg_NARGS_NUM	OREG_NUM(7)
 
 #define reg_A0		LREG(0)
+#define reg_A0_NUM	LREG_NUM(0)
 #define reg_A1		LREG(1)
+#define reg_A1_NUM	LREG_NUM(1)
 #define reg_A2		LREG(2)
+#define reg_A2_NUM	LREG_NUM(2)
 #define reg_A3		LREG(3)
+#define reg_A3_NUM	LREG_NUM(3)
 #define reg_A4		LREG(4)
+#define reg_A4_NUM	LREG_NUM(4)
 #define reg_A5		LREG(5)
+#define reg_A5_NUM	LREG_NUM(5)
 #define reg_OCFP	LREG(6)
+#define reg_OCFP_NUM	LREG_NUM(6)
 #define reg_LRA		LREG(7)
+#define reg_LRA_NUM	LREG_NUM(7)
 
 #define reg_FDEFN	IREG(0)
+#define reg_FDEFN_NUM	IREG_NUM(0)
 #define reg_LEXENV	IREG(1)
+#define reg_LEXENV_NUM	IREG_NUM(1)
 #define reg_L0		IREG(2)
+#define reg_L0_NUM	IREG_NUM(2)
 #define reg_NFP		IREG(3)
+#define reg_NFP_NUM	IREG_NUM(3)
 #define reg_CFUNC	IREG(4)
+#define reg_CFUNC_NUM	IREG_NUM(4)
 #define reg_CODE	IREG(5)
+#define reg_CODE_NUM	IREG_NUM(5)
 #define reg_LIP		IREG(7)
+#define reg_LIP_NUM	IREG_NUM(7)
+
+/*
+ * Macros to compute the SC_OFFSET value.  See read-var-integer and
+ * sc-offset and friends in debug-info.lisp
+ */
+#define	SC_OFFSET_HI(sc, reg)	((SC_OFFSET(sc,reg)) >> 8)
+#define	SC_OFFSET_LO(sc, reg)	((SC_OFFSET(sc,reg)) & 0xff)
 
 #define REGNAMES \
 	"ZERO",		"ALLOC",	"NULL",		"CSP", \
