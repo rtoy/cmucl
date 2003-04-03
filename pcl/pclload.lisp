fccdc92d4be229ed88c3db366cf9e44a0664abaa
gerd <gerd>
2003-04-03 11:40:06 +0000

    * pcl/pclload.lisp ("PCL"): Remove references to the 	slot-accessor-name package.

--- pcl/pclload.lisp
+++ pcl/pclload.lisp
@@ -1,12 +1,8 @@
 (in-package "PCL")
-(unless (find-package "SLOT-ACCESSOR-NAME")
-  (make-package "SLOT-ACCESSOR-NAME" :use nil))
 (rename-package "PCL" "PCL" '("OLD-PCL"))
-(rename-package "SLOT-ACCESSOR-NAME" "SLOT-ACCESSOR-NAME"
-		'("OLD-SLOT-ACCESSOR-NAME"))
 (import 'kernel:funcallable-instance-p)
 (load "target:pcl/defsys")
 (load-pcl)
 (rename-package "PCL" "PCL" '())
-(rename-package "SLOT-ACCESSOR-NAME" "SLOT-ACCESSOR-NAME" '())
+
 
