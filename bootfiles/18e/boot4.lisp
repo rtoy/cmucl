1d681ff569d7c9e7d10fa79284d8080de1e8ba47
pmai <pmai>
2003-04-21 17:07:13 +0000

    Fixed bug in boot file, and removed erroneous comment.

--- bootfiles/18e/boot4.lisp
+++ bootfiles/18e/boot4.lisp
@@ -3,13 +3,8 @@
 ;;;; which it should be because KERNEL:SEQUENCE-COUNT names a type
 ;;;; used in the compiler.
 ;;;;
-;;;; Note that this doesn't work with Pierre Mai's build scripts
-;;;; because these load code:exports.lisp before they load a
-;;;; target:bootstrap.lisp.  Just choose the
-;;;; UNINTERN-CONFLICTING-SYMBOLS restart when asked in this case.
-;;;;
 
-(unintern 'c::sequence-count)
+(unintern 'c::sequence-count "C")
 (export '(kernel::sequence-count) :kernel)
 
 ;;;; end of file
