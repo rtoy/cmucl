e4b609edd312ae4bf6f2049c72a1741396f5b0b4
gerd <gerd>
2003-08-30 11:30:56 +0000

    * src/ldb/, src/assembler/: Removed; dead code.

--- ldb/print.h
+++
@@ -1,5 +0,0 @@
-/* $Header: /Volumes/share2/src/cmucl/cvs2git/cvsroot/src/ldb/Attic/print.h,v 1.1 1990/02/24 19:37:30 wlott Exp $ */
-
-extern char *lowtag_Names[], *subtype_Names[];
-
-extern void print();
