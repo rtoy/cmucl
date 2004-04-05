3bf16a59b8eab34d5ac4ec468f395e3d45dea84d
rtoy <rtoy>
2004-04-05 18:48:42 +0000

    Boot file for new sxhash function.  Requires a cross-compile.

---
+++ bootfiles/18e/boot23-sxhash.lisp
@@ -0,0 +1,6 @@
+;;; Cross-compile bootstrap file for new sxhash function.
+;;;
+;;; Nothing fancy needed.  Just load up the default cross-compile script.
+
+#+sparc
+(load "target:tools/cross-scripts/cross-sparc-sparc")
