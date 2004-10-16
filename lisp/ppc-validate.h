1a8f9f5b731cd27421010e0750863a8e19847059
rtoy <rtoy>
2004-10-16 23:45:50 +0000

    Increase the size of the read-only, static-space, control-stack, and binding-stack to 128 MB each.  Increase the heap to a maximum of 512 MB, but leave the default heap at 128 MB.  The control-stack and binding-stack were moved down just below the heap.
    No bootstrap or cross-compiled required.

--- lisp/ppc-validate.h
+++ lisp/ppc-validate.h
@@ -1,19 +1,31 @@
 /*
 
- $Header: /Volumes/share2/src/cmucl/cvs2git/cvsroot/src/lisp/ppc-validate.h,v 1.1 2004/07/13 00:26:22 pmai Exp $
+ $Header: /Volumes/share2/src/cmucl/cvs2git/cvsroot/src/lisp/ppc-validate.h,v 1.2 2004/10/16 23:45:50 rtoy Exp $
 
  This code was written as part of the CMU Common Lisp project at
  Carnegie Mellon University, and has been placed in the public domain.
 
 */
 
-
+/* 
+ * The read-only space must be in low memory because the BA
+ * instruction only has 26-bits to specify the address.  When this is
+ * fixed, the read-only space can be moved.  (The BA instruction is
+ * used to jump to assembly routines.)
+ */
 #define READ_ONLY_SPACE_START	(0x01000000)
-#define READ_ONLY_SPACE_SIZE	(0x03ff8000)
+#define READ_ONLY_SPACE_SIZE	(0x07ff8000) /* 128 MB, almost */
   
 #define STATIC_SPACE_START  	(0x08000000)
-#define STATIC_SPACE_SIZE   	(0x017fff00)
+#define STATIC_SPACE_SIZE   	(0x07ff8000) /* 128 MB, almost */
 
+#define CONTROL_STACK_START 	(0x30000000)
+#define CONTROL_STACK_SIZE  	(0x07ff8000) /* 128 MB, almost */
+
+#define BINDING_STACK_START 	(0x38000000)
+#define BINDING_STACK_SIZE  	(0x07ff8000) /* 128 MB, almost */
+
+#if 0
 #define DYNAMIC_0_SPACE_START	(0x40000000)
 #define DYNAMIC_1_SPACE_START	(0x48000000)
 #define DYNAMIC_SPACE_SIZE  	(0x07fff000)
@@ -23,6 +35,12 @@
 
 #define BINDING_STACK_START 	(0x56000000)
 #define BINDING_STACK_SIZE  	(0x00ff0000)
+#endif
+
+#define DEFAULT_DYNAMIC_SPACE_SIZE (0x08000000)	/* 128 MB */
+#define DYNAMIC_0_SPACE_START	(0x40000000)
+#define DYNAMIC_1_SPACE_START	(0x60000000)
+#define DYNAMIC_SPACE_SIZE  	(0x1fff0000) /* 512 MB, almost */
 
 #if 0
 #define HOLES {0x04ff8000, 0x06ff8000, 0x0aff8000, 0x1fff8000}
