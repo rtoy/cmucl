8ae02c414192545b67c8cb40f686ca1d472e5302
rtoy <rtoy>
2006-07-18 18:31:38 +0000

    Don't add bounds for double-double-float component of float.  This causes a crash somewhere.  Don't know why this doesn't work, but I thought it was working a short time ago.

--- code/type.lisp
+++ code/type.lisp
@@ -5,7 +5,7 @@
 ;;; Carnegie Mellon University, and has been placed in the public domain.
 ;;;
 (ext:file-comment
-  "$Header: /Volumes/share2/src/cmucl/cvs2git/cvsroot/src/code/type.lisp,v 1.73 2006/07/07 14:04:01 rtoy Exp $")
+  "$Header: /Volumes/share2/src/cmucl/cvs2git/cvsroot/src/code/type.lisp,v 1.74 2006/07/18 18:31:38 rtoy Exp $")
 ;;;
 ;;; **********************************************************************
 ;;;
@@ -2415,9 +2415,10 @@
 	(double-float ,(coerced-float-bound  low 'double-float)
 		      ,(coerced-float-bound high 'double-float))
 	#+long-float ,(error "stub: no long float support yet")
+	;; Can't add bound for this yet.  Don't know why.
 	#+double-double
-	(double-double-float ,(coerced-float-bound  low 'double-double-float)
-			     ,(coerced-float-bound high 'double-double-float)))))
+	(double-double-float #+nil ,(coerced-float-bound  low 'double-double-float)
+			     #+nil ,(coerced-float-bound high 'double-double-float)))))
 
 (defmacro define-float-format (f)
   `(def-bounded-type ,f float ,f))
