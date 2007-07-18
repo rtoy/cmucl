4060520d543eac81dbda290ef64d7bf7857911fc
fgilham <fgilham>
2007-07-18 15:05:34 +0000

    Add special case for x86_64 to linker.sh.

--- tools/linker.sh
+++ tools/linker.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -x
 
-# $Id: linker.sh,v 1.3 2007/07/17 15:25:14 fgilham Exp $
+# $Id: linker.sh,v 1.4 2007/07/18 15:05:34 fgilham Exp $
 
 # This file was written by Fred Gilham and is placed in the public domain.
 # It comes without warranty of any kind.
@@ -32,9 +32,18 @@ VER=''
 # Set OS-specific variables.
 case "$OPSYS" in
     Linux )
+	# Handle x86_64 version system.  This should be revisited when there's a 64-bit version
+	# of CMUCL on Linux.  Note that -m32 won't work in older versions of GCC which we may
+	# still want to support.
+	ARCH=`uname -m`
+	if [ "$ARCH" = "x86_64" ]
+	then
+	    CRTPATH=`$GCC -m32 -print-libgcc-file-name`
+	    LIBROOT=`dirname $CRTPATH`
+	fi
+	STARTCRT="/usr/lib/crt1.o /usr/lib/crti.o $LIBROOT/crtbegin.o"
+	ENDCRT="$LIBROOT/crtend.o /usr/lib/crtn.o"
 	DLINKER='-dynamic-linker /lib/ld-linux.so.2'
-	STARTCRT="$LIBROOT/../../../crt1.o $LIBROOT/../../../crti.o $LIBROOT/crtbegin.o"
-	ENDCRT="$LIBROOT/crtend.o $LIBROOT/../../../crtn.o"
 	LIBS="-L$LIBROOT -ldl -lm -lgcc -lc -lgcc"
 	;;
     FreeBSD )
