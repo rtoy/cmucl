386fc82184f0472ecf236db9a81feb59680c0f29
rtoy <rtoy>
2007-10-25 14:54:20 +0000

    Update to 19d.

--- tools/cross-scripts/cross-sparc-sparc.lisp
+++ tools/cross-scripts/cross-sparc-sparc.lisp
@@ -20,7 +20,7 @@
      :hash-new
      :random-mt19937			; MT-19937 generator
      :cmu				; Announce this is CMUCL
-     :cmu19 :cmu19c			; Current version identifier
+     :cmu19 :cmu19d			; Current version identifier
      :modular-arith			; Modular arithmetic
      :double-double			; Double-double float support
      )
