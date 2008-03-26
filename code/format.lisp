7b68513e8848abb3de1b0c705787d775f84605d0
rtoy <rtoy>
2008-03-26 16:44:11 +0000

    Missed a place where flonum-to-string wasn't converted to use keyword args.

--- code/format.lisp
+++ code/format.lisp
@@ -5,7 +5,7 @@
 ;;; Carnegie Mellon University, and has been placed in the public domain.
 ;;;
 (ext:file-comment
-  "$Header: /Volumes/share2/src/cmucl/cvs2git/cvsroot/src/code/format.lisp,v 1.81.2.2 2008/03/25 16:20:41 rtoy Exp $")
+  "$Header: /Volumes/share2/src/cmucl/cvs2git/cvsroot/src/code/format.lisp,v 1.81.2.3 2008/03/26 16:44:11 rtoy Exp $")
 ;;;
 ;;; **********************************************************************
 ;;;
@@ -1487,7 +1487,7 @@
       (let* ((signstr (if (minusp (float-sign number)) "-" (if atsign "+" "")))
 	     (signlen (length signstr)))
 	(multiple-value-bind (str strlen ig2 ig3 pointplace)
-			     (lisp::flonum-to-string number nil d nil)
+	    (lisp::flonum-to-string number :width nil :fdigits d :scale nil)
 	  (declare (ignore ig2 ig3))
 	  (when colon (write-string signstr stream))
 	  (dotimes (i (- w signlen (max 0 (- n pointplace)) strlen))
