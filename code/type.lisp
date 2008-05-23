1680d12977a819f9fc55d7817218c077bef4ff6f
rtoy <rtoy>
2008-05-23 03:51:23 +0000

    Double-double float bounds don't work on ppc/darwin for some reason. Disable them.  (This causes the build to either exit silently, or to hang loading fndb.  Did not investigate why.)

--- code/type.lisp
+++ code/type.lisp
@@ -5,7 +5,7 @@
 ;;; Carnegie Mellon University, and has been placed in the public domain.
 ;;;
 (ext:file-comment
-  "$Header: /Volumes/share2/src/cmucl/cvs2git/cvsroot/src/code/type.lisp,v 1.76 2008/04/03 18:10:29 rtoy Exp $")
+  "$Header: /Volumes/share2/src/cmucl/cvs2git/cvsroot/src/code/type.lisp,v 1.77 2008/05/23 03:51:23 rtoy Exp $")
 ;;;
 ;;; **********************************************************************
 ;;;
@@ -2416,8 +2416,11 @@
 		      ,(coerced-float-bound high 'double-float))
 	#+long-float ,(error "stub: no long float support yet")
 	#+double-double
-	(double-double-float ,(coerced-float-bound  low 'double-double-float)
-			     ,(coerced-float-bound high 'double-double-float)))))
+	(double-double-float
+	 ;; For some reason, the ppc/darwin port doesn't like this.  I
+	 ;; (rtoy) don't know why.
+	 #-ppc ,(coerced-float-bound  low 'double-double-float)
+	 #-ppc ,(coerced-float-bound high 'double-double-float)))))
 
 (defmacro define-float-format (f)
   `(def-bounded-type ,f float ,f))
