1633a69e5ed20052adc726ddab791550f2e47f57
agoncharov <agoncharov>
2009-01-07 14:14:37 +0000

    Keep the settings common to all x86 platforms in Config.x86_common. Use a new (much smaller) Config.x86_freebsd, named consistently with (some) other files.

--- tools/create-target.sh
+++ tools/create-target.sh
@@ -33,7 +33,7 @@ if [ $# = 1 ]; then
 	    i386) LISP_VARIANT=x86_darwin ;;
 	    esac
 	    ;;
-    FreeBSD) LISP_VARIANT=FreeBSD_gencgc ;;
+    FreeBSD|freebsd) LISP_VARIANT=x86_freebsd ;;
     # Please fill in some other common systems
     *) echo "Sorry, please specify the desired Lisp variant." 
        exit 1 ;;
@@ -66,7 +66,7 @@ if [ "$MOTIF_VARIANT" = "" ]; then
     case $LISP_VARIANT in
       alpha_linux) MOTIF_VARIANT=alpha_linux ;;
       alpha_osf1) MOTIF_VARIANT=alpha_osf1 ;;
-      FreeBSD*) MOTIF_VARIANT=FreeBSD ;;
+      FreeBSD*|freebsd*) MOTIF_VARIANT=FreeBSD ;;
       NetBSD*) MOTIF_VARIANT=NetBSD ;;
       OpenBSD*) MOTIF_VARIANT=OpenBSD ;;
       *_darwin) MOTIF_VARIANT=Darwin ;;
@@ -91,8 +91,11 @@ find src -name 'CVS' -prune -o -type d -print \
 	| sed "s:^src:$TARGET:g" | xargs mkdir
 
 # Link Makefile and Config files
-( cd $TARGET/lisp ; ln -s ../../src/lisp/GNUmakefile ./Makefile )
-( cd $TARGET/lisp ; ln -s ../../src/lisp/Config.$LISP_VARIANT ./Config )
+ 
+cd $TARGET/lisp
+ln -s ../../src/lisp/GNUmakefile ../../src/lisp/Config.$LISP_VARIANT ../../src/lisp/Config.*_common .
+ln -s Config.$LISP_VARIANT Config
+cd -
 
 # Create empty initial map file
 echo 'Map file for lisp version 0' > $TARGET/lisp/lisp.nm
@@ -121,12 +124,9 @@ case $LISP_VARIANT in
       esac
       sed "s;@@gcname@@;$gcname;" $SETENV/openbsd-features.lisp >> $TARGET/setenv.lisp
       ;;
-  *FreeBSD*)
-      case $LISP_VARIANT in
-        *_gencgc*) gcname=":gencgc" ;;
-	*) gcname=":cgc" ;;
-      esac
-      sed "s;@@gcname@@;$gcname;" $SETENV/freebsd-features.lisp >> $TARGET/setenv.lisp
+  *FreeBSD*|*freebsd*)
+	gcname=":gencgc"
+	sed "s;@@gcname@@;$gcname;" $SETENV/freebsd-features.lisp >> $TARGET/setenv.lisp
       ;;
   *solaris*)
       cat $SETENV/solaris-features.lisp >> $TARGET/setenv.lisp
