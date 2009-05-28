32f6a2ecfee1c68e6a9cad3e904d130c31d357fb
rtoy <rtoy>
2009-05-28 13:09:11 +0000

    From Paul Foley:
    Shouldn't use the final-sigma before an apostrophe.

--- pcl/simple-streams/external-formats/final-sigma.lisp
+++ pcl/simple-streams/external-formats/final-sigma.lisp
@@ -4,18 +4,17 @@
 ;;; This code was written by Paul Foley and has been placed in the public
 ;;; domain.
 ;;;
-(ext:file-comment "$Header: /Volumes/share2/src/cmucl/cvs2git/cvsroot/src/pcl/simple-streams/external-formats/final-sigma.lisp,v 1.1.2.2 2009/05/12 16:31:50 rtoy Exp $")
+(ext:file-comment "$Header: /Volumes/share2/src/cmucl/cvs2git/cvsroot/src/pcl/simple-streams/external-formats/final-sigma.lisp,v 1.1.2.3 2009/05/28 13:09:11 rtoy Exp $")
 
 ;; This is a composing format that attempts to detect sigma in
-;; word-final position and change it from "σ" (U+3C3) to "ς"
-;; (U+3C2).
+;; word-final position and change it from "σ" (U+03C3) to "ς" (U+03C2).
 
 (define-composing-external-format :final-sigma (:size 1)
   (input (state input unput tmp1 tmp2 tmp3 tmp4)
     `(multiple-value-bind (,tmp1 ,tmp2) ,input
        (when (= ,tmp1 #x03C3)
 	 (multiple-value-bind (,tmp3 ,tmp4) ,input
-	   (when (or (not ,tmp3) (< ,tmp3 #x0370))
+	   (when (or (not ,tmp3) (and (< ,tmp3 #x0370) (/= ,tmp3 #x0027)))
 	     (setq ,tmp1 #x03C2))
 	   (when ,tmp3
 	     (,unput ,tmp4))))
