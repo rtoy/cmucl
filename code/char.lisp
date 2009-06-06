080a33d23ce62f14851dc91e42f4bab133108a26
rtoy <rtoy>
2009-06-06 19:57:09 +0000

    Oops.  Revert previous change.  Can't convert #\- because some Unicode names contain a hyphen.

--- code/char.lisp
+++ code/char.lisp
@@ -5,7 +5,7 @@
 ;;; Carnegie Mellon University, and has been placed in the public domain.
 ;;;
 (ext:file-comment
-  "$Header: /Volumes/share2/src/cmucl/cvs2git/cvsroot/src/code/char.lisp,v 1.15.18.3.2.14 2009/06/04 18:47:14 rtoy Exp $")
+  "$Header: /Volumes/share2/src/cmucl/cvs2git/cvsroot/src/code/char.lisp,v 1.15.18.3.2.15 2009/06/06 19:57:09 rtoy Exp $")
 ;;;
 ;;; **********************************************************************
 ;;;
@@ -177,8 +177,7 @@
 	  #-unicode nil
 	  #+unicode
 	  (let ((code (unicode-name-to-codepoint
-		       (nsubstitute #\Space #\-
-				    (nsubstitute #\Space #\_ (string-upcase name))))))
+		       (nsubstitute #\Space #\_ (string-upcase name)))))
 	    (if code
 		(code-char code)
 		nil)))))
