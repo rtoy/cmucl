e62c9818cf195426a56b59283977a5c43623aa56
rtoy <rtoy>
2009-08-18 13:51:06 +0000

    contrib/CATALOG.TXT: o Remove entry for PROFILE, which is no longer a contrib. o Add entry for new embedded-c.
    contrib/contrib.lisp:
    o Add defmodule for ops (contrib-ops).
    o Add defmodule for embedded-c (contrib-embedded-c).
    
    contrib/embedded-c/embedded-c.catalog:
    o New file
    
    contrib/embedded-c/embedded-c.lisp:
    o New file that contains Helmut Eller's embedded-c. Added to CMUCL,
      with permission from Helmut.

--- contrib/contrib.lisp
+++ contrib/contrib.lisp
@@ -5,7 +5,7 @@
 ;;; Carnegie Mellon University, and has been placed in the public domain.
 ;;;
 (ext:file-comment
-  "$Header: /Volumes/share2/src/cmucl/cvs2git/cvsroot/src/contrib/contrib.lisp,v 1.1 2009/08/18 13:12:42 rtoy Exp $")
+  "$Header: /Volumes/share2/src/cmucl/cvs2git/cvsroot/src/contrib/contrib.lisp,v 1.2 2009/08/18 13:51:06 rtoy Exp $")
 
 ;;; Define the various contrib modules.  We use defmodule for now
 ;;; because it's easy for the contribs.
@@ -26,3 +26,12 @@
 
 (defmodule "contrib-psgraph"
     "modules:psgraph/psgraph")
+
+;; This actually consists of several files, but compile-ops will
+;; compile and load them all, so this module appears to have just the
+;; one file.
+(defmodule "contrib-ops"
+    "modules:ops/compile-ops")
+
+(defmodule "contrib-embedded-c"
+    "modules:embedded-c/embedded-c")
