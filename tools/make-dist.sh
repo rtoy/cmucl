688c051fce4f494dc4e113f47cdeaed7230261be
rtoy <rtoy>
2009-08-19 17:58:49 +0000

    Fix issue with installing sources to a directory.  This was affecting Solaris.

--- tools/make-dist.sh
+++ tools/make-dist.sh
@@ -136,5 +136,6 @@ $ROOT/make-extra-dist.sh $OPTIONS $TARGET $VERSION $ARCH $OS || exit 2
 if [ X"$MAKE_SRC_DIST" = "Xyes" ]; then
     # If tar is not GNU tar, set the environment variable GTAR to
     # point to GNU tar.
-    $ROOT/make-src-dist.sh ${INSTALL_DIR:+ -I ${INSTALL_DIR}} $ENABLE_GZIP $ENABLE_BZIP -t ${GTAR:-tar} $VERSION
+    OPTIONS="${INSTALL_DIR:+ -I ${INSTALL_DIR}} $ENABLE_GZIP $ENABLE_BZIP"
+    $ROOT/make-src-dist.sh $OPTIONS -t ${GTAR:-tar} $VERSION
 fi
