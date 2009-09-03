ba26ab97c3b1aac13f6b392a5c4343f7fab7a52e
rtoy <rtoy>
2009-09-03 20:36:27 +0000

    Oops.  Don't need tmp3 and tmp4.

--- pcl/simple-streams/external-formats/cr.lisp
+++ pcl/simple-streams/external-formats/cr.lisp
@@ -4,13 +4,13 @@
 ;;; This code was written by Paul Foley and has been placed in the public
 ;;; domain.
 ;;;
-(ext:file-comment "$Header: /Volumes/share2/src/cmucl/cvs2git/cvsroot/src/pcl/simple-streams/external-formats/cr.lisp,v 1.1.2.1 2009/09/03 16:57:25 rtoy Exp $")
+(ext:file-comment "$Header: /Volumes/share2/src/cmucl/cvs2git/cvsroot/src/pcl/simple-streams/external-formats/cr.lisp,v 1.1.2.2 2009/09/03 20:36:27 rtoy Exp $")
 
 (in-package "STREAM")
 
 ;; Convert CR to/from #\newline.
 (define-composing-external-format :cr (:size 1)
-  (input (state input unput tmp1 tmp2 tmp3 tmp4)
+  (input (state input unput tmp1 tmp2)
     `(multiple-value-bind (,tmp1 ,tmp2)
 	 ,input
        (if (= ,tmp1 (char-code #\return))
