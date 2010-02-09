88619989a7c9bb5b4f90964109be925c88616bd2
rtoy <rtoy>
2010-02-09 02:44:32 +0000

    Unmark a couple of docstrings.  This was causing problems with the pot file.  Needs further investigation.

--- code/gc.lisp
+++ code/gc.lisp
@@ -5,7 +5,7 @@
 ;;; Carnegie Mellon University, and has been placed in the public domain.
 ;;;
 (ext:file-comment
-  "$Header: /Volumes/share2/src/cmucl/cvs2git/cvsroot/src/code/gc.lisp,v 1.42.38.2 2010/02/08 20:21:44 rtoy Exp $")
+  "$Header: /Volumes/share2/src/cmucl/cvs2git/cvsroot/src/code/gc.lisp,v 1.42.38.3 2010/02/09 02:44:32 rtoy Exp $")
 ;;;
 ;;; **********************************************************************
 ;;;
@@ -209,7 +209,7 @@
 
 #-(or cgc gencgc)
 (defun get-bytes-consed-dfixnum ()
-  _N"Returns the number of bytes consed since the first time this function
+  "Returns the number of bytes consed since the first time this function
   was called.  The first time it is called, it returns zero."
   (declare (optimize (speed 3) (safety 0)(inhibit-warnings 3)))
   (cond ((null *last-bytes-in-use*)
@@ -509,7 +509,7 @@
 ;;; 
 #-gencgc
 (defun gc (&optional (verbose-p *gc-verbose*))
-  _N"Initiates a garbage collection.  The optional argument, VERBOSE-P,
+  "Initiates a garbage collection.  The optional argument, VERBOSE-P,
   which defaults to the value of the variable *GC-VERBOSE* controls
   whether or not GC statistics are printed."
   (sub-gc :verbose-p verbose-p :force-p t))
