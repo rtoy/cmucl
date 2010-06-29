71779fbc76494e9e4a3a67488f9e43ced56ae3c3
rtoy <rtoy>
2010-06-29 15:37:11 +0000

    Add comment about what this is for.

--- bootfiles/20a/boot-2010-06-cross-x86.lisp
+++ bootfiles/20a/boot-2010-06-cross-x86.lisp
@@ -1,2 +1,5 @@
+;; Cross-compile script for x86 to add new SC numbers the xmm
+;; (floating-point) registers.  The default cross-compile script is
+;; good enough for this.
 #+x86
 (load "target:tools/cross-scripts/cross-x86-x86")
