a33856c25fb84f6890e72d5b4971ae5185074a38
Raymond Toy <toy.raymond@gmail.com>
2012-02-04 23:29:54 -0800

    Cleanup comments

--- src/lisp/exec-final.c
+++ src/lisp/exec-final.c
@@ -7,9 +7,9 @@
 
 /*
  * See lisp.c for documentation, but basically this is linked in for
- * Solaris executables to indicate that the image is builtin.  We must
- * not define initial_function_addr here because the linker script
- * will set the value appropriately.
+ * executables to indicate that the image is builtin.  We must not
+ * define initial_function_addr here because the linker script will
+ * set the value appropriately.
  */
 int builtin_image_flag = 1;
 
