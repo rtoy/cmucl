b07a82baedbf4ed761c593cbd185a97f3fed4e36
Raymond Toy <toy.raymond@gmail.com>
2013-05-02 21:42:51 -0700

    Add -M option to pass extra flags to make.
    Use getopts to parse the options and also print slightly better error
    messages.

--- bin/rebuild-lisp.sh
+++ bin/rebuild-lisp.sh
@@ -1,14 +1,34 @@
 #!/bin/sh
 
+while getopts "M:h?" arg
+do
+    case $arg in
+      M) MFLAGS="$OPTARG" ;;
+      h) usage ;;
+      \?) usage ;;
+    esac
+done
+
+usage() 
+{
+    echo "rebuild.-lisp.sh [-h?] [-M opts] target-directory"
+    echo "    -h        this help"
+    echo "    -?        this help"
+    echo "    -M opts   Options to pass to make"
+
+}
+
+shift `expr $OPTIND - 1`
+
 if [ "$1" = "" ]
 then
-	echo "Usage: $0 target-directory"
+	usage
 	exit 1
 fi
 
 if [ ! -d "$1" ]
 then
-	echo "$1 isn't a directory"
+	echo "$1" must be a directory.
 	exit 2
 fi
 
@@ -23,4 +43,4 @@ fi
 
 export MAKE
 
-${MAKE} -C $TARGET/lisp clean && ${MAKE} -C $TARGET/lisp
+${MAKE} -C $TARGET/lisp $MFLAGS clean && ${MAKE} -C $TARGET/lisp $MFLAGS
