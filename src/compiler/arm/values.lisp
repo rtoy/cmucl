b0e6efc87c13e05602a792ecf2c0fc2eaa91b7a5
Raymond Toy <toy.raymond@gmail.com>
2013-09-02 12:11:49 -0700

    Author: Raymond Toy <toy.raymond@gmail.com> --- log message follows this line --- Update for ARM, using NOT-IMPLEMENTED.

--- src/compiler/arm/values.lisp
+++ src/compiler/arm/values.lisp
@@ -38,19 +38,7 @@
   (:info nvals)
   (:temporary (:scs (descriptor-reg)) temp)
   (:generator 20
-    (inst move start csp-tn)
-    (inst add csp-tn csp-tn (* nvals vm:word-bytes))
-    (do ((val vals (tn-ref-across val))
-	 (i 0 (1+ i)))
-	((null val))
-      (let ((tn (tn-ref-tn val)))
-	(sc-case tn
-	  (descriptor-reg
-	   (storew tn start i))
-	  (control-stack
-	   (load-stack-tn temp tn)
-	   (storew temp start i)))))
-    (inst li count (fixnumize nvals))))
+    (not-implemented)))
 
 ;;; Push a list of values on the stack, returning Start and Count as used in
 ;;; unknown values continuations.
@@ -67,24 +55,7 @@
   (:vop-var vop)
   (:save-p :compute-only)
   (:generator 0
-    (let ((loop (gen-label))
-	  (done (gen-label)))
-
-      (move list arg)
-      (move start csp-tn)
-
-      (emit-label loop)
-      (inst cmp list null-tn)
-      (inst b :eq done)
-      (loadw temp list vm:cons-car-slot vm:list-pointer-type)
-      (loadw list list vm:cons-cdr-slot vm:list-pointer-type)
-      (inst add csp-tn csp-tn vm:word-bytes)
-      (storew temp csp-tn -1)
-      (test-type list ndescr loop nil vm:list-pointer-type)
-      (error-call vop bogus-argument-to-values-list-error list)
-
-      (emit-label done)
-      (inst sub count csp-tn start))))
+    (not-implemented)))
 
 
 
@@ -93,7 +64,7 @@
 ;;;
 (define-vop (%more-arg-values)
   (:args (context :scs (descriptor-reg any-reg) :target src)
-	 (skip :scs (any-reg zero immediate))
+	 (skip :scs (any-reg immediate))
 	 (num :scs (any-reg) :target count))
   (:arg-types * positive-fixnum positive-fixnum)
   (:temporary (:sc any-reg :from (:argument 0)) src)
@@ -103,22 +74,4 @@
   (:results (start :scs (any-reg))
 	    (count :scs (any-reg)))
   (:generator 20
-    (sc-case skip
-      (zero
-       (move src context))
-      (immediate
-       (inst add src context (* (tn-value skip) word-bytes)))
-      (any-reg
-       (inst add src context skip)))
-    (inst orcc count zero-tn num)
-    (inst b :eq done)
-    (inst move start csp-tn)
-    (inst move dst csp-tn)
-    (inst add csp-tn count)
-    (inst move i count)
-    LOOP
-    (inst subcc i 4)
-    (inst ldn temp src i)
-    (inst b :ne loop)
-    (inst stn temp dst i)
-    DONE))
+    (not-implemented)))
