2b4777a38702cea97dec72231765c433f43852e4
Raymond Toy <toy.raymond@gmail.com>
2013-09-01 21:04:49 -0700

    Update for ARM, using NOT-IMPLEMENTED.

--- src/compiler/arm/subprim.lisp
+++ src/compiler/arm/subprim.lisp
@@ -1,19 +1,17 @@
-;;; -*- Package: SPARC -*-
+;;; -*- Package: ARM -*-
 ;;;
 ;;; **********************************************************************
 ;;; This code was written as part of the CMU Common Lisp project at
 ;;; Carnegie Mellon University, and has been placed in the public domain.
 ;;;
 (ext:file-comment
-  "$Header: src/compiler/sparc/subprim.lisp $")
+  "$Header: src/compiler/arm/subprim.lisp $")
 ;;;
 ;;; **********************************************************************
 ;;;
 ;;;    Linkage information for standard static functions, and random vops.
 ;;;
-;;; Written by William Lott.
-;;; 
-(in-package "SPARC")
+(in-package "ARM")
 
 
 
@@ -32,28 +30,7 @@
   (:vop-var vop)
   (:save-p :compute-only)
   (:generator 50
-    (let ((done (gen-label))
-	  (loop (gen-label))
-	  (not-list (generate-cerror-code vop object-not-list-error object)))
-      (move ptr object)
-      (move count zero-tn)
-
-      (emit-label loop)
-
-      (inst cmp ptr null-tn)
-      (inst b :eq done)
-      (inst nop)
-
-      (test-type ptr temp not-list t vm:list-pointer-type)
-
-      (loadw ptr ptr vm:cons-cdr-slot vm:list-pointer-type)
-      (inst add count count (fixnumize 1))
-      (test-type ptr temp loop nil vm:list-pointer-type)
-
-      (cerror-call vop done object-not-list-error ptr)
-
-      (emit-label done)
-      (move result count))))
+    (not-implemented)))
        
 
 (define-static-function length (object) :translate length)
