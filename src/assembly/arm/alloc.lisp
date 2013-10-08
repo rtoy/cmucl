9ee11ea6c3aef61950720d287cc092379b9c4b47
Raymond Toy <toy.raymond@gmail.com>
2013-10-07 19:48:12 -0700

    Convert to ARM, using NOT-IMPLEMENTED.

--- src/assembly/arm/alloc.lisp
+++ src/assembly/arm/alloc.lisp
@@ -1,19 +1,17 @@
-;;; -*- Package: SPARC -*-
+;;; -*- Package: ARM -*-
 ;;;
 ;;; **********************************************************************
 ;;; This code was written as part of the CMU Common Lisp project at
 ;;; Carnegie Mellon University, and has been placed in the public domain.
 ;;;
 (ext:file-comment
-  "$Header: src/assembly/sparc/alloc.lisp $")
+  "$Header: src/assembly/arm/alloc.lisp $")
 ;;;
 ;;; **********************************************************************
 ;;;
 ;;; Stuff to handle allocating simple objects.
 ;;;
-;;; Written by William Lott.
-;;;
 
-(in-package "SPARC")
+(in-package "ARM")
 
 ;;; But we do everything inline now that we have a better pseudo-atomic.
