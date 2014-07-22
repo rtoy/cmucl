7a11c612892beceb575257c8b2e7fe239964f0e7
Raymond Toy <toy.raymond@gmail.com>
2014-07-22 08:07:31 -0700

    Add implmeentation of sincos in C.
    Darwin/x86 doesn't have sincos in libm so include sincos when building
    on Darwin.

---
+++ src/lisp/sincos.c
@@ -0,0 +1,43 @@
+#include "fdlibm.h"
+
+void sincos (double x, double *s, double *c)
+{
+    int ix;
+    union { int i[2]; double d; } ux;
+
+    ux.d = x;
+    ix = ux.i[HIWORD] & 0x7fffffff;
+
+    /* |x| ~< pi/4 */
+    if (ix < 0x3fe921fb) {
+        *s = __kernel_sin(x, 0.0, 0);
+        *c = __kernel_cos(x, 0.0);
+    } else {
+        /* Argument reduction needed */
+        double y[2];
+        int n;
+        
+        n = __ieee754_rem_pio2(x, y);
+
+        switch (n & 3) {
+          case 0:
+              *s = __kernel_sin(y[0], y[1], 1);
+              *c = __kernel_cos(y[0], y[1]);
+              break;
+          case 1:
+              *s = __kernel_cos(y[0], y[1]);
+              *c = - __kernel_sin(y[0], y[1], 1);
+              break;
+          case 2:
+              *s = - __kernel_sin(y[0], y[1], 1);
+              *c = - __kernel_cos(y[0], y[1]);
+              break;
+          case 3:
+              *s = - __kernel_cos(y[0], y[1]);
+              *c = __kernel_sin(y[0], y[1], 1);
+              break;
+        }
+    }
+
+    return;
+}
