33a91f6afe5d68ea41a649db6e40d93d5896fb45
Raymond Toy <toy.raymond@gmail.com>
2014-08-01 00:01:38 -0700

    Add tests for CLEAR-OUTPUT.

---
+++ tests/gray-streams.lisp
@@ -0,0 +1,39 @@
+;;;; -*- Lisp -*-
+
+(require :gray-streams)
+
+(defpackage gray-streams-tests
+  (:use #:common-lisp #:lisp-unit))
+
+(in-package #:gray-streams-tests)
+
+(defparameter *test-path*
+  (merge-pathnames (make-pathname :name :unspecific :type :unspecific
+                                  :version :unspecific)
+                   *load-truename*)
+  "Directory for temporary test files.")
+
+(defparameter *test-file*
+  (merge-pathnames #p"test-data.tmp" *test-path*))
+
+(eval-when (:load-toplevel)
+  (ensure-directories-exist *test-path* :verbose t))
+
+(define-test clear-output-1
+  (:tag :trac)
+  ;; Create a Gray stream and make sure that clear-output works.
+  (assert-eql
+   0
+   (unwind-protect
+	(let ((s (open *test-file*
+		       :direction :output
+		       :if-exists :supersede
+		       :class 'lisp::character-output-stream)))
+	  (write-char #\a s)
+	  (clear-output s)
+	  (close s)
+	  (setf s (open *test-file*))
+	  (file-length s))
+     (delete-file *test-file*))))
+		      
+    
