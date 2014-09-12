c042852462fc92df2da2259c191a909400199fb4
Raymond Toy <toy.raymond@gmail.com>
2014-09-11 22:29:38 -0700

    Support FP exceptions on NetBSD without feraiseexcept.
    Not quite fully working on NetBSD, but using this approach on Darwin
    does the right thing and all the tests pass. (Could these failures be
    due to NetBSD not compiling with just sse2 and thus uses x87 for the
    operations?)
    
     * setexception.c:
       * Add support for NetBSD. Instead of using feraiseexcept, try to
         generate the appropriate operations to generate the desired
         exceptions.
     * double-values.c:
       * Helper functions for setexception to return appropriate float
         values. These are in a different file so that the compiler can't
         optimize the values away when used in fdlibm_setexceptions.
     * GNUmakefile:
       * Compue double-values.c everywhere. Should be harmless since only
         NetBSD uses these functions.

--- src/lisp/setexception.c
+++ src/lisp/setexception.c
@@ -1,4 +1,25 @@
+#if defined(__NetBSD__)
+/*
+ * NetBSD doesn't have fenv.h. At least the version currently being
+ * used to build cmucl doesn't.  Assume this also means we don't have
+ * feraiseexcept.  So, to generate the desired exceptions, we have to
+ * create the appropriate operations to generate the desired
+ * exceptions.
+ */
+#undef HAVE_FERAISEEXCEPT
+
+extern double double_zero();
+extern double double_huge();
+extern double double_tiny();
+
+#else
+#define HAVE_FERAISEEXCEPT
+#endif
+
+#ifdef HAVE_FERAISEEXCEPT
 #include <fenv.h>
+#endif
+
 #include <math.h>
 #include <stdio.h>
 
@@ -50,32 +71,48 @@ fdlibm_setexception(double x, enum FDLIBM_EXCEPTION type)
           /* Division by zero. Use the sign of x to get the correct
            *  signed infinity
            */
+#ifdef HAVE_FERAISEEXCEPT
           feraiseexcept(FE_DIVBYZERO);
-          
           ret = copysign(INFINITY, x);
+#else
+          ret = copysign(1, x) / double_zero();
+#endif
+          
           break;
       case 1:
           /* Underflow. Use the sign of x to get a signed zero. */
+#ifdef HAVE_FERAISEEXCEPT
           feraiseexcept(FE_UNDERFLOW);
           ret = copysign(0.0, x);
+#else
+          ret = double_tiny() * double_tiny();;
+#endif
           break;
       case 2:
           /* overflow */
+#ifdef HAVE_FERAISEEXCEPT
           feraiseexcept(FE_OVERFLOW);
           ret = copysign(INFINITY, x);
+#else
+          ret = double_huge() * copysign(double_huge(), x);
+#endif
           break;
       case 3:
       {
-          /* invalid */
+          /* if */
 
           if (!isQNaN(x)) {
               /*
                * If it's not a quiet NaN, we want to signal an invalid
                * operation. Otherwise, we silently return a NaN.
                */
+#ifdef HAVE_FERAISEEXCEPT
               feraiseexcept(FE_INVALID);
+#else
+              ret = double_zero() / double_zero();
+              return ret;
+#endif
           }
-          
           /*
            * FIXME: Of the many NaN values that we have, what NaN
            * should we return?
