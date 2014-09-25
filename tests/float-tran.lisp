bcc8cd7dd797fe3998efdca0cf2adfe49150bb3f
Raymond Toy <toy.raymond@gmail.com>
2014-09-24 20:27:54 -0700

    Fix type derivation of the exponent for DECODE-FLOAT.
    The minimum exponent is from 0, it's from least-positive float.
    
     * src/compiler/float-tran.lisp:
       * Derive the correct minimum exponent by using the exponent from
         the least-positive float value of the appropriate type.
     * tests/float-tran.lisp:
       * Add tests for the derived exponent type for DECODE-FLOAT.
    
    Conflicts:
    	tests/float-tran.lisp

--- tests/float-tran.lisp
+++ tests/float-tran.lisp
@@ -1,3 +1,4 @@
+
 ;; Tests for various float transformations.
 
 (defpackage :float-tran-tests
@@ -14,4 +15,18 @@
   (assert-equalp (c::make-member-type :members (list 1f0))
 		 (c::decode-float-sign-derive-type-aux (c::specifier-type '(single-float (0f0))))))
 
-  
+(define-test decode-float-exp
+  "Test type derivation of the exponent from decode-float"
+  (assert-equalp (c::specifier-type '(integer -148 128))
+		 (c::decode-float-exp-derive-type-aux
+		  (c::specifier-type 'single-float)))
+  (assert-equalp (c::specifier-type '(integer -1073 1024))
+		 (c::decode-float-exp-derive-type-aux
+		  (c::specifier-type 'double-float)))
+  #+double-double
+  (assert-equalp (c::specifier-type '(integer -1073 1024))
+		 (c::decode-float-exp-derive-type-aux
+		  (c::specifier-type 'double-double-float)))
+  (assert-equalp (c::specifier-type '(integer 2 8))
+		 (c::decode-float-exp-derive-type-aux
+		  (c::specifier-type '(double-float 2d0 128d0)))))
