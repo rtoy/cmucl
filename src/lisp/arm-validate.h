84fa139f3999c92d8dffc1caa29b727a829583be
Raymond Toy <toy.raymond@gmail.com>
2014-12-31 16:59:00 -0800

    Update according to review, and correct some sizes
    Remove prefix and suffix from  _ARM_VALIDATE_H_ 
    Use correct values for address map (from design doc).
    Replace MB_128 with MB and adjust other usages appropriately.
    Remove support for gencgc

--- src/lisp/arm-validate.h
+++ src/lisp/arm-validate.h
@@ -3,26 +3,23 @@
  * placed in the public domain.
  */
 
-#ifndef _ARM_VALIDATE_H_
-#define _ARM_VALIDATE_H_
+#ifndef ARM_VALIDATE_H
+#define ARM_VALIDATE_H
 
 /*
  * Address map:
  *
- *	0x00000000->0x0f800000  248M C code and stuff(?)
- *      0x0f800000->0x10000000    8M for linkage table area
- *	0x10000000->0x20000000  256M Read-Only Space.
- *	0x20000000->0x28000000  128M Binding stack growing up.
- *	0x28000000->0x38000000  256M Static Space.
- *	0x38000000->0x40000000  128M Control stack growing up.
- *	0x40000000->0x80000000 1024M Dynamic space 1
- *	0x80000000->0xc0000000 1024M Dynamic space 2
- *      0xc0000000->0xffffffff 1024M C stack, dynamic libs, etc.      
+ *      0x0f000000->0x10000000   16M for linkage table area
+ *      0x10000000->0x14000000   64M Read-Only Space.
+ *      0x20000000->0x22000000   32M Binding stack growing up.
+ *      0x28000000->0x2a000000   32 Control stack growing up.
+ *      0x30000000->0x34000000   64M Static Space.
+ *      0x40000000->0x60000000  512M Dynamic space 1
+ *      0x60000000->0x80000000  512M Dynamic space 2
  *
  */
 
-/* 128 MB */
-#define MB_128	(0x08000000)
+#define MB	(1024*1024)
 
 #ifdef LINKAGE_TABLE
 /*
@@ -39,38 +36,33 @@
  * long).  Hope that's enough!  Make sure this doesn't overlap the
  * READ_ONLY_SPACE_START!
  */
-#define FOREIGN_LINKAGE_SPACE_SIZE  (0xF000000U)	/* 8 MB */
+#define FOREIGN_LINKAGE_SPACE_SIZE  (8*MB)
 #endif
 
 #define READ_ONLY_SPACE_START	(SpaceStart_TargetReadOnly)
-#define READ_ONLY_SPACE_SIZE	(2*MB_128)	       /* 256 MB max */
+#define READ_ONLY_SPACE_SIZE	(64*MB)
 
 #define BINDING_STACK_START 	(0x20000000)
-#define BINDING_STACK_SIZE  	(MB_128)               /* 128 MB max */
+#define BINDING_STACK_SIZE  	(32*MB)
 
 #define STATIC_SPACE_START  	(SpaceStart_TargetStatic)
-#define STATIC_SPACE_SIZE   	(2*MB_128)             /* 256 MB max */
+#define STATIC_SPACE_SIZE   	(64*MB)
 
-#define CONTROL_STACK_START 	(0x38000000)
-#define CONTROL_STACK_SIZE  	(MB_128)               /* 128 MB max */
+#define CONTROL_STACK_START 	(0x28000000)
+#define CONTROL_STACK_SIZE  	(32*MB)
 #define CONTROL_STACK_END       (CONTROL_STACK_START + control_stack_size)
 
 /* The default dynamic space to allocate */
 #define DEFAULT_DYNAMIC_SPACE_SIZE  	(0x10000000)	/* 256 MB */
 
 /* The maximum dynamic space that we can allocate */
-#ifdef GENCGC
-/*
- * For GENCGC, we can use both dynamic spaces, so we get at least
- * double the heap size.
- */
-#define DYNAMIC_SPACE_SIZE      (0xB0000000)	/* 2816 MB max */
-#else
-#define DYNAMIC_SPACE_SIZE      (0x40000000)	/* 1GB max */
-#endif
+#define DYNAMIC_SPACE_SIZE      (512*MB)
 
+#ifdef GENCGC
+#error gencgc not supported
+#else
 #define DYNAMIC_0_SPACE_START	(SpaceStart_TargetDynamic)
-/* This isn't used with GENCGC */
 #define DYNAMIC_1_SPACE_START	(DYNAMIC_1_SPACE_START + DYNAMIC_SPACE_SIZE)
+#endif
 
 #endif
