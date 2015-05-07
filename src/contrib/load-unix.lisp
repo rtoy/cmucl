3191f538dd21d8656e4d79fbf31802ddacc099ff
Raymond Toy <toy.raymond@gmail.com>
2015-05-06 21:07:00 -0700

    For linux, Load unix-glibc2.lisp instead of unix.lisp.

--- src/contrib/load-unix.lisp
+++ src/contrib/load-unix.lisp
@@ -1,6 +1,7 @@
 ;; Load extra functionality in the UNIX package.
 
 (ext:without-package-locks
-  (load "modules:unix/unix"))
+  (load #-linux "modules:unix/unix"
+	#+linux "modules:unix/unix-glibc2"))
 
 (provide 'unix)
