eb4a83b4d038719203f991126dba4bfcfbd284cd
Raymond Toy <toy.raymond@gmail.com>
2015-05-10 14:16:20 -0700

    Load up the unix fasl file using compile-file-pathname.

--- src/contrib/load-unix.lisp
+++ src/contrib/load-unix.lisp
@@ -1,7 +1,7 @@
 ;; Load extra functionality in the UNIX package.
 
 (ext:without-package-locks
-  (load #-linux "modules:unix/unix"
-	#+linux "modules:unix/unix-glibc2"))
+  (load (compile-file-pathname #-linux "modules:unix/unix"
+			       #+linux "modules:unix/unix-glibc2")))
 
 (provide 'unix)
