1ca0a5571183b014a50fad56bea89472a47c1e8b
Raymond Toy <toy.raymond@gmail.com>
2015-06-14 08:31:14 -0700

    Fix #4: ELT signals error on invalid index on lists
    code/seq.lisp:
    o Define internal LIST-ELT* function that executes ELT on lists,
      signaling an error if the index is invalid.
    
    compiler/seqtran.lisp:
    o Change the deftransform for ELT to use LIST-ELT* instead of NTH.
    
    tests/issues.lisp:
    o Add test for this issue.

--- tests/issues.lisp
+++ tests/issues.lisp
@@ -23,3 +23,18 @@
   (assert-equal
    '(square x)
    (funcall (compiler-macro-function 'square) '(funcall #'square x) nil)))
+
+(define-test issue.5
+    (:tag :issues)
+  (assert-true
+   (handler-case
+       (let ((f (compile nil '(lambda (list)
+			       (declare (type list list)
+				(optimize (speed 1) (safety 1) (compilation-speed 1) (space 1) (debug 1)))
+			       (elt list 3)))))
+	 (funcall f (list 0 1 2)))
+     ;; ELT should signal an error in this case.
+     (lisp::index-too-large-error ()
+       t)
+     (t ()
+       nil))))
