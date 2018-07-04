3af22f92b9c842ba7e88414ea8fa528e30c2260a
Raymond Toy <toy.raymond@gmail.com>
2018-07-04 12:40:30 -0700

    Add some timing code, but not for tests.

--- tests/bignum.lisp
+++ tests/bignum.lisp
@@ -24,3 +24,33 @@
 	       (prod-knuth (bignum::classical-multiply-bignums r1 r2))
 	       (prod-hd (bignum::classical-multiply-bignum-hd r1 r2)))
 	  (assert-equal prod-knuth prod-hd r1 r2))))))
+
+
+;; Just for simple timing tests so we can redo the timing tests if needed.
+#+nil
+(define-test hd-timing
+  "Test execution time"
+  (:tag :bignum-tests)
+  (let ((rng (kernel::make-random-object :state
+					 (kernel:init-random-state)
+					 :rand 0 :cached-p nil))
+	(range (ash 1 128))
+	(reps 10000))
+    (flet ((gen-bignum (x sign)
+	     (do ((r (random x rng) (random x rng)))
+		 ((typep r 'bignum)
+		  (if (zerop sign)
+		      r (- r))))))
+      (let* ((r1 (gen-bignum range 1))
+	     (r2 (gen-bignum range 1)) res)
+	(time
+	 (dotimes (k reps)
+	   (declare (fixnum k)) (setf res
+				      (bignum::classical-multiply-bignums r1 r2))))
+	(print res)
+	(time
+	 (dotimes (k reps)
+	   (declare (fixnum k)) (setf res
+				      (bignum::classical-multiply-bignum-hd r1 r2))))
+	(print res)))))
+
