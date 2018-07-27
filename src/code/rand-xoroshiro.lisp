0c427fc137491327a31e997b7178efbaf7690ff4
Raymond Toy <toy.raymond@gmail.com>
2018-07-27 10:50:23 -0700

    Remove extra closing paren

--- src/code/rand-xoroshiro.lisp
+++ src/code/rand-xoroshiro.lisp
@@ -492,7 +492,7 @@
      (error 'simple-type-error
 	    :expected-type '(or (integer 1) (float (0.0))) :datum arg
 	    :format-control _"Argument is not a positive integer or a positive float: ~S")
-	    :format-arguments (list arg)))))
+	    :format-arguments (list arg))))
 
 ;; Jump function for the generator.  See the jump function in
 ;; http://xoroshiro.di.unimi.it/xoroshiro128plus.c
