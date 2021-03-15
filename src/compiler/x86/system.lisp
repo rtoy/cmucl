f7acaf94a93603c28665de18fe1527ca0824e164
Raymond Toy <toy.raymond@gmail.com>
2021-03-15 16:36:21 -0700

    Remove stray 4 at start of the file.

--- src/compiler/x86/system.lisp
+++ src/compiler/x86/system.lisp
@@ -1,4 +1,4 @@
-4;;; -*- Mode: LISP; Syntax: Common-Lisp; Base: 10; Package: x86 -*-
+;;; -*- Mode: LISP; Syntax: Common-Lisp; Base: 10; Package: x86 -*-
 ;;;
 ;;; **********************************************************************
 ;;; This code was written as part of the CMU Common Lisp project at
