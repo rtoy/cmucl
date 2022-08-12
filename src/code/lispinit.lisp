c416dc2ec3de15fc85118a86bb2dcacd50ca101f
Raymond Toy <toy.raymond@gmail.com>
2022-08-12 09:47:51 -0700

    Fix #128: quit accepts an exit code
    Change `quit` to have an additional optional arg that specifies the
    exit code to use when quitting lisp.
    
    Tested by doing
    
    ```
    (quit nil 42)
    ```
    
    and
    
    ```
    (quit t 99)
    ```
    
    In each case the shell says the exit code was 42 and 99, respectively.

--- src/code/lispinit.lisp
+++ src/code/lispinit.lisp
@@ -492,14 +492,15 @@
 
 ;;; Quit gets us out, one way or another.
 
-(defun quit (&optional recklessly-p)
-  "Terminates the current Lisp.  Things are cleaned up unless Recklessly-P is
-  non-Nil."
+(defun quit (&optional recklessly-p (exit-code 0))
+  "Terminates the current Lisp.  Things are cleaned up unless
+  Recklessly-P is non-Nil.  On quitting, Lisp sets the return code to
+  exit-code, defaulting to 0."
   (if recklessly-p
-      (unix:unix-exit 0)
+      (unix:unix-exit exit-code)
       (progn
         (mapc (lambda (fn) (ignore-errors (funcall fn))) *cleanup-functions*)
-        (throw '%end-of-the-world 0))))
+        (throw '%end-of-the-world exit-code))))
 
 
 #-mp ; Multi-processing version defined in multi-proc.lisp.
