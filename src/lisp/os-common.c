ec14c5b16fc37e56023f06934f98f90f25a6fffd
Raymond Toy <toy.raymond@gmail.com>
2022-08-29 14:07:02 -0700

    Include stdlib.h and unistd.h
    stdlib.h is needed for malloc and friends.  unistd.h is needed for
    sysconf.

--- src/lisp/os-common.c
+++ src/lisp/os-common.c
@@ -11,8 +11,10 @@
 #include <pwd.h>
 #include <stdbool.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <sys/stat.h>
+#include <unistd.h>
 #include <time.h>
 
 #include "os.h"
