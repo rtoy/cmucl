fba3f3a8be47647e528683670254ea7ccef35b31
Raymond Toy <toy.raymond@gmail.com>
2022-11-14 05:09:37 +0000

    Fix #141: Use setlocale to handle localization settings

--- src/code/unix.lisp
+++ src/code/unix.lisp
@@ -2902,3 +2902,19 @@
   (alien:alien-funcall
    (alien:extern-alien "os_setlocale"
 		       (function c-call:int))))
+
+(defun unix-get-lc-messages ()
+  _N"Get LC_MESSAGES from the current locale.  If we can't, return
+  NIL.  A call to UNIX-SETLOCALE must have been done previously before
+  calling this so that the correct locale is returned."
+  (with-alien ((buf (array c-call:char 256)))
+    (let ((result
+	    (alien-funcall
+	     (extern-alien "os_get_lc_messages"
+			   (function c-call:int
+				     (* c-call:char)
+				     c-call:int))
+	     (cast buf (* c-call:char))
+	     256)))
+      (when (zerop result)
+	(cast buf c-call:c-string)))))
