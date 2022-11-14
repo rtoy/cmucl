10f6311f91ae56ce58b57e4bd412a5351f78737a
Raymond Toy <toy.raymond@gmail.com>
2022-11-14 14:49:31 -0800

    Fix merge mistake
    Accidentally deleted the test
    issue.139-default-external-format-write-file

--- tests/issues.lisp
+++ tests/issues.lisp
@@ -720,6 +720,30 @@
       (assert-equal (map 'list #'char-name string)
 		    (map 'list #'char-name (read-line s))))))
 
+(define-test issue.139-default-external-format-write-file
+    (:tag :issues)
+  ;; Test that opening a file for writing uses the default :utf8.
+  ;; First write something out to the file.  Then read it back in
+  ;; using an explicit format of utf8 and verifying that we got the
+  ;; right contents.
+  (let ((string (concatenate 'string
+                             ;; This is "hello" in Korean
+                             '(#\Hangul_syllable_an
+                               #\Hangul_Syllable_Nyeong
+                               #\Hangul_Syllable_Ha
+                               #\Hangul_Syllable_Se
+                               #\Hangul_Syllable_Yo))))
+    (with-open-file (s (merge-pathnames "out-utf8.txt"
+                                        *test-path*)
+                       :direction :output
+                       :if-exists :supersede)
+      (write-line string s))
+    (with-open-file (s (merge-pathnames "out-utf8.txt"
+                                        *test-path*)
+                       :direction :input
+                       :external-format :utf-8)
+      (assert-equal (map 'list #'char-name string)
+                   (map 'list #'char-name (read-line s))))))
 
 (define-test issue.139-locale-external-format
     (:tag :issues)
