8367cd338c98952f644c8563368f82a2e151d86f
Raymond Toy <toy.raymond@gmail.com>
2022-12-08 07:26:45 -0800

    Fix merge mistake.  LANG should not be set.
    When merging master, we added LANG again when it should have been
    deleted.

--- bin/run-tests.sh
+++ bin/run-tests.sh
@@ -47,13 +47,9 @@ function cleanup {
 
 trap cleanup EXIT
 
-# Set a known value for LANG here so that we can tests consistently.
-# The most important part is that the codeset is UTF-8.
-LANG=en_US.UTF-8
-
 if [ $# -eq 0 ]; then
     # No args so run all the tests
-    $LISP -noinit -load tests/run-tests.lisp -eval '(cmucl-test-runner:run-all-tests)'
+    $LISP -noinit -nositeinit -load tests/run-tests.lisp -eval '(cmucl-test-runner:run-all-tests)'
 else
     # Run selected files.  Convert each file name to uppercase and append "-TESTS"
     result=""
@@ -62,6 +58,6 @@ else
 	new=`echo $f | tr '[a-z]' '[A-Z]'`
         result="$result "\"$new-TESTS\"
     done
-    $LISP -noinit -load tests/run-tests.lisp -eval "(progn (cmucl-test-runner:load-test-files) (cmucl-test-runner:run-test $result))"
+    $LISP -noinit -nositeinit -load tests/run-tests.lisp -eval "(progn (cmucl-test-runner:load-test-files) (cmucl-test-runner:run-test $result))"
 fi
 
