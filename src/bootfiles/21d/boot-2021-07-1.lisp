0dac88e6a51c9912d2fed92c814835f2ab04f771
Raymond Toy <toy.raymond@gmail.com>
2023-02-25 07:24:24 -0800

    Add comment on what the bootfile is doing.

--- src/bootfiles/21d/boot-2021-07-1.lisp
+++ src/bootfiles/21d/boot-2021-07-1.lisp
@@ -1,6 +1,10 @@
 ;; Bootstrap file
 ;;
 ;; Use "bin/build.sh -B boot-2021-07-1" to build this.
+;;
+;; We want to export the symbols from the KERNEL package which also
+;; exists in the C package, so we unintern the conflicting symbols from
+;; the C package.
 
 (in-package "KERNEL")
 (ext:without-package-locks
