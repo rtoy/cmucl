f1393af8f1c50e2fade81a6cdecced7c4a47233a
Raymond Toy <toy.raymond@gmail.com>
2023-02-27 07:14:42 -0800

    Ignore the condition arg; we don't use it.
    Also remove the call to `describe` that was used for debugging.

--- src/bootfiles/21d/boot-2021-07-1.lisp
+++ src/bootfiles/21d/boot-2021-07-1.lisp
@@ -10,8 +10,7 @@
 (ext:without-package-locks
   (handler-bind
       ((error (lambda (c)
-		;;(declare (ignore c))
-		(describe c)
+		(declare (ignore c))
 		(invoke-restart 'lisp::unintern-conflicting-symbols))))
     (export '(DOUBLE-FLOAT-INT-EXPONENT
 	      SINGLE-FLOAT-INT-EXPONENT))))
