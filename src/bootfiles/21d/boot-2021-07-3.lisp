0b4ec82d2de83ee986b48218fac1b0ff0c2a6b7a
Raymond Toy <toy.raymond@gmail.com>
2023-03-02 10:50:47 -0800

    Add x86 reader conditional
    Don't set package to "X86" if we're not building on x86.
    
    One less thing to worry about when bootstrapping.

--- src/bootfiles/21d/boot-2021-07-3.lisp
+++ src/bootfiles/21d/boot-2021-07-3.lisp
@@ -3,7 +3,9 @@
 ;;
 ;; Use bin/build.sh -B boot-2021-07-3 to build this (along with any
 ;; other bootfiles that are still needed).
+#+x86
 (in-package :x86)
+
 #+x86
 (ext:without-package-locks
   (defparameter static-functions
