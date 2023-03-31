0a00f00dcebdee4940dd1637d2eadafd34291451
Raymond Toy <toy.raymond@gmail.com>
2023-03-31 09:21:35 -0700

    Remove get-page-size from all the foo-os.lisp files.
    get-page-size is now defined in unix.lisp so we don't need it defined
    in foo-os.lisp anymore.  Well, except we didn't remove the definition
    from mach-os.lisp which is very different.

--- src/code/sunos-os.lisp
+++ src/code/sunos-os.lisp
@@ -68,14 +68,3 @@
 		  (unix:get-unix-error-msg utime)))
 	  (T
 	   (values utime stime majflt)))))
-
-;;; GET-PAGE-SIZE  --  Interface
-;;;
-;;;    Return the system page size.
-;;;
-(defun get-page-size ()
-  (multiple-value-bind (val err)
-		       (unix:unix-getpagesize)
-    (unless val
-      (error (intl:gettext "Getpagesize failed: ~A") (unix:get-unix-error-msg err)))
-    val))
