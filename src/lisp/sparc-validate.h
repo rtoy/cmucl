d2921059728fbc45b568602d5be0515aba0b9f66
Carl S. Shapiro <cshapiro@panix.com>
2023-05-15 17:35:44 +0000

    Remove unused CONTROL_STACK_END that's #if'd out anyway.

--- src/lisp/sparc-validate.h
+++ src/lisp/sparc-validate.h
@@ -98,9 +98,6 @@
 #define STATIC_SPACE_SIZE   	((2*MB_128) - SPARSE_BLOCK_SIZE)	/* 256 MB - 32 KB, 256 MB max */
 
 #define CONTROL_STACK_SIZE  	(MB_128 - SPARSE_BLOCK_SIZE)	/* 128 MB - 32 KB, 128 MB max */
-#if 0
-#define CONTROL_STACK_END       (CONTROL_STACK_START + control_stack_size)
-#endif
 
 #define SIGNAL_STACK_SIZE	SIGSTKSZ
 
