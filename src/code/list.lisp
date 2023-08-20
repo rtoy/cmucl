46f5352ac1d9edc8f8c099816f7f61a8c686a9f9
Raymond Toy <toy.raymond@gmail.com>
2023-08-20 15:15:27 -0700

    Always hash list2 and add tests
    Made a logic error; we can't hash the shorter of the two lists, at
    least not without a big change to the implementation.  We always hash
    the second list now.  Remove shorter-list-to-hashtable.
    
    Add tests for subsetp.

--- src/code/list.lisp
+++ src/code/list.lisp
@@ -991,35 +991,6 @@
 
 (defvar *allow-hashtable-for-set-functions* t)
 
-(declaim (start-block shorter-list-to-hashtable subsetp))
-
-(defun shorter-list-to-hashtable (list1 list2 key test test-not)
-  ;; Find the shorter list and return the length and the shorter list
-  (when test-not
-    (return-from shorter-list-to-hashtable nil))
-  (let ((hash-test (let ((test-fn (if (and (symbolp test)
-                                           (fboundp test))
-                                      (fdefinition test)
-                                      test)))
-                     (cond ((eql test-fn #'eq) 'eq)
-                           ((eql test-fn #'eql) 'eql)
-                           ((eql test-fn #'equal) 'equal)
-                           ((eql test-fn #'equalp) 'equalp)))))
-    (unless hash-test
-      (return-from shorter-list-to-hashtable nil))
-    (multiple-value-bind (min-length shorter-list)
-        (do ((len 0 (1+ len))
-             (lst1 list1 (cdr lst1))
-             (lst2 list2 (cdr lst2)))
-            ((or (null lst1) (null lst2))
-             (values len (if (null lst1) list1 list2))))
-      (when (< min-length *min-list-length-for-hashtable*)
-        (return-from shorter-list-to-hashtable nil))
-      (let ((hashtable (make-hash-table :test hash-test :size min-length)))
-        (dolist (item shorter-list)
-          (setf (gethash (apply-key key item) hashtable) item))
-        (values hashtable shorter-list)))))
-
 (defun subsetp (list1 list2 &key key (test #'eql testp) (test-not nil notp))
   "Returns T if every element in list1 is also in list2."
   (declare (inline member))
@@ -1029,27 +1000,20 @@
   ;; SUBSETP is used early in TYPE-INIT where hash tables aren't
   ;; available yet, so we can't use hashtables then.  LISPINIT will
   ;; take care to disable this for the kernel.core.  SAVE will set
-  ;; this to true it's safe to use hash tables for SUBSETP.
-  (multiple-value-bind (hashtable shorter-list)
-      (when *allow-hashtable-for-set-functions*
-        (shorter-list-to-hashtable list1 list2 key test test-not))
+  ;; this to true when it's safe to use hash tables for SUBSETP.
+  (let ((hashtable
+          (when *allow-hashtable-for-set-functions*
+            (list-to-hashtable list2 key test test-not))))
     (cond (hashtable
-           (cond ((eq shorter-list list1)
-                  (dolist (item list2)
-	            (unless (nth-value 1 (gethash (apply-key key item) hashtable))
-	              (return-from subsetp nil))))
-                 ((eq shorter-list list2)
-	          (dolist (item list1)
-	            (unless (nth-value 1 (gethash (apply-key key item) hashtable))
-	              (return-from subsetp nil)))))
+	   (dolist (item list1)
+	     (unless (nth-value 1 (gethash (apply-key key item) hashtable))
+	       (return-from subsetp nil)))
            t)
-	  ((null hashtable)
+	  (t
 	   (dolist (item list1)
 	     (unless (with-set-keys (member (apply-key key item) list2))
 	       (return-from subsetp nil)))
 	   T))))
-
-(declaim (end-block))
 
 
 ;;; Functions that operate on association lists
