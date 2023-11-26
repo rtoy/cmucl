089574efdf80812bdef550501afba9ce439d4223
Raymond Toy <toy.raymond@gmail.com>
2023-11-26 14:51:49 -0800

    Add test for non-existent user

--- tests/os.lisp
+++ tests/os.lisp
@@ -3,7 +3,7 @@
 
 (in-package "OS-TESTS")
 
-(define-test user-homedir
+(define-test user-homedir.1
   "Test user-homedir"
   (:tag :issues)
   ;; Simple test to see if unix-get-user-homedir returns the expected
@@ -16,9 +16,22 @@
     (let* ((info-dir (unix:user-info-dir user-info))
            (info-name (unix:user-info-name user-info))
            (expected-home-pathname (pathname
-                                    (concatenate 'string info-dir "/")))
-           (home-pathname (unix:unix-get-user-homedir info-name)))
-      (assert-true info-dir)
-      (assert-true info-name)
+                                    (concatenate 'string info-dir "/"))))
+      (multiple-value-bind (home-pathname status)
+          (unix:unix-get-user-homedir info-name)
+        (assert-true info-dir)
+        (assert-true info-name)
 
-      (assert-equal home-pathname expected-home-pathname))))
+        (assert-equal home-pathname expected-home-pathname)
+        (assert-eql status 0)))))
+
+(define-test user-homedir.2
+  "Test user-homedir"
+  (:tag :issues)
+  ;; Simple test to see if unix-get-user-homedir returns the expected
+  ;; value for a user that does not exist.  Well, we assume such a
+  ;; user doesn't exist.
+      (multiple-value-bind (home-pathname status)
+          (unix:unix-get-user-homedir "zotuserunknown")
+        (assert-eql home-pathname nil)
+        (assert-eql status 0)))
