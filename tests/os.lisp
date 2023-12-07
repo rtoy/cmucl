4f64fc56aa361a8adc1534658dcf370a9ac6e00e
Raymond Toy <toy.raymond@gmail.com>
2023-12-07 14:50:04 -0800

    Update tests for changes in get-user-homedir-namestring
    The function name has changed and we return a namestring instead of a
    pathname.

--- tests/os.lisp
+++ tests/os.lisp
@@ -7,7 +7,7 @@
 (define-test user-homedir.1
   "Test user-homedir"
   (:tag :issues)
-  ;; Simple test to see if get-user-homedir-pathname returns the
+  ;; Simple test to see if get-user-homedir-namestring returns the
   ;; expected value.  Use getuid and getpwuid to figure out what the
   ;; name and home directory should be.
   (let* ((uid (unix:unix-getuid))
@@ -15,15 +15,13 @@
     (assert-true uid)
     (assert-true user-info)
     (let* ((info-dir (unix:user-info-dir user-info))
-           (info-name (unix:user-info-name user-info))
-           (expected-home-pathname (pathname
-                                    (concatenate 'string info-dir "/"))))
-      (multiple-value-bind (home-pathname status)
-          (system:get-user-homedir-pathname info-name)
+           (info-name (unix:user-info-name user-info)))
+      (multiple-value-bind (home-namestring status)
+          (system:get-user-homedir-namestring info-name)
         (assert-true info-dir)
         (assert-true info-name)
 
-        (assert-equal home-pathname expected-home-pathname)
+        (assert-equal home-namestring info-dir)
         (assert-eql status 0)))))
 
 (define-test user-homedir.2
@@ -33,6 +31,6 @@
   ;; value for a user that does not exist.  Well, we assume such a
   ;; user doesn't exist.
       (multiple-value-bind (home-pathname status)
-          (system:get-user-homedir-pathname "zotuserunknown")
+          (system:get-user-homedir-namestring "zotuserunknown")
         (assert-eql home-pathname nil)
         (assert-eql status 0)))
