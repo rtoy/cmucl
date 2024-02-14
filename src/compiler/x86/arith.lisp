31b7ffd97a687be21d617c44685366d12b51ff21
Raymond Toy <toy.raymond@gmail.com>
2024-02-13 19:57:48 -0800

    Remove old implementation of xoroshiro-next
    This was for xoroshiro128+, and we're using xoroshiro128** now.

--- src/compiler/x86/arith.lisp
+++ src/compiler/x86/arith.lisp
@@ -1678,79 +1678,6 @@
 
 (in-package "VM")
 
-#||
-#+random-xoroshiro
-(progn
-(defknown xoroshiro-next ((simple-array double-float (2)))
-  (values (unsigned-byte 32) (unsigned-byte 32))
-  (movable))
-
-(define-vop (xoroshiro-next)
-  (:policy :fast-safe)
-  (:translate xoroshiro-next)
-  (:args (state :scs (descriptor-reg) :to (:result 3)))
-  (:arg-types simple-array-double-float)
-  (:results (r1 :scs (unsigned-reg))
-	    (r0 :scs (unsigned-reg)))
-  (:result-types unsigned-num unsigned-num)
-  (:temporary (:sc double-reg) s0)
-  (:temporary (:sc double-reg) s1)
-  (:temporary (:sc double-reg) t0)
-  (:generator 10
-    ;; s0 = state[0]
-    (inst movsd s0 (make-ea :dword :base state
-			 :disp (- (+ (* vm:vector-data-offset
-					vm:word-bytes)
-				     (* 8 0))
-				  vm:other-pointer-type)))
-    ;; s1 = state[1]
-    (inst movsd s1 (make-ea :dword :base state
-			 :disp (- (+ (* vm:vector-data-offset
-					vm:word-bytes)
-				     (* 8 1))
-				  vm:other-pointer-type)))
-    ;; Compute result = s0 + s1
-    (inst movapd t0 s0)
-    (inst paddq t0 s1)
-    ;; Save the 64-bit result as two 32-bit results
-    (inst movd r0 t0)
-    (inst psrlq t0 32)
-    (inst movd r1 t0)
-
-    ;; s1 = s1 ^ s0
-    (inst xorpd s1 s0)
-
-    ;; s0 = rotl(s0,55) = s0 << 55 | s0 >> 9
-    (inst movapd t0 s0)
-    (inst psllq s0 55)			; s0 = s0 << 55
-    (inst psrlq t0 9)			; t0 = s0 >> 9
-    (inst orpd s0 t0)			; s0 = rotl(s0, 55)
-
-    (inst movapd t0 s1)
-    (inst xorpd s0 s1)			; s0 = s0 ^ s1
-    (inst psllq t0 14)			; t0 = s1 << 14
-    (inst xorpd s0 t0)			; s0 = s0 ^ t0
-    (inst movsd (make-ea :dword :base state
-			 :disp (- (+ (* vm:vector-data-offset
-					vm:word-bytes)
-				     (* 8 0))
-				  vm:other-pointer-type))
-	  s0)
-
-    ;; s1 = rotl(s1, 36) = s1 << 36 | s1 >> 28, using t0 as temp
-    (inst movapd t0 s1)
-    (inst psllq s1 36)
-    (inst psrlq t0 28)
-    (inst orpd s1 t0)
-
-    (inst movsd (make-ea :dword :base state
-			 :disp (- (+ (* vm:vector-data-offset
-					vm:word-bytes)
-				     (* 8 1))
-				  vm:other-pointer-type))
-	  s1)))
-)
-||#
 #+random-xoroshiro
 (progn
 (defknown xoroshiro-next ((simple-array double-float (2)))
@@ -1790,6 +1717,8 @@
     ;; 
     ;; 	 return result;
     ;; }
+
+    ;; s0 = state[0]
     (inst movsd s0 (make-ea :dword :base state
                                    :disp (- (+ (* vm:vector-data-offset
 					          vm:word-bytes)
@@ -1819,6 +1748,7 @@
     (inst psrlq t0 32)
     (inst movd r1 t0)
 
+    ;; s1 = state[1]
     (inst movsd s1 (make-ea :dword :base state
 			           :disp (- (+ (* vm:vector-data-offset
 					          vm:word-bytes)
