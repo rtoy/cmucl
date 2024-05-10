2c4e223e3a53f79e3c147a4663803ff1620ef609
Raymond Toy <toy.raymond@gmail.com>
2024-05-10 15:00:29 -0700

    Declare type_writer to have a void* arg instead of caddr_t.
    Update datatrans.h so that message_write_float is declared to have a
    second arg of type void*.  Then update implementation of
    message_write_float to have a matching function signature and cast the
    void* arg to float* so we can get the float value.

--- src/motif/server/datatrans.c
+++ src/motif/server/datatrans.c
@@ -265,10 +265,12 @@ void message_write_color(message_t m,XColor *color,int tag)
   message_put_word(m,color->blue);
 }
 
-void message_write_float(message_t m,float *f,int tag)
+void message_write_float(message_t m,void *f,int tag)
 {
+  float *fl = (float *) f;
+  
   message_put_dblword(m,combine_type_and_data(tag,0));
-  message_put_dblword(m,*f);
+  message_put_dblword(m,*fl);
 }
 
 
