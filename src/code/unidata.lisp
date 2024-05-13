3a0f81e8ee355605ae2bf622615b38fa4eb14956
Raymond Toy <toy.raymond@gmail.com>
2024-05-13 14:23:47 -0700

    Ignore some categories when finding the upper or lower case character
    For `unicode-upper`, we ignore characters with the category Lt, Mn,
    Nl, and So and just return the character unchanged.  Likewise, for
    `unicode-lower`, we ignore Lt, Nl, and So.

--- src/code/unidata.lisp
+++ src/code/unidata.lisp
@@ -883,7 +883,9 @@
   (unless (unidata-scase *unicode-data*) (load-scase))
   (let* ((scase (unidata-scase *unicode-data*))
 	 (n (logand (qref32 scase code) #xFF)))
-    (if (zerop n)
+    (if (or (zerop n)
+            ;; Ignore category Lt, Mn, Nl, So
+            (member (unicode-category code) '(92 32 75 109)))
 	code
 	(let* ((m (aref (scase-svec scase) (logand n #x7F))))
 	  (if (logbitp 7 n) (+ code m) (- code m))))))
@@ -894,7 +896,9 @@
   (unless (unidata-scase *unicode-data*) (load-scase))
   (let* ((scase (unidata-scase *unicode-data*))
 	 (n (logand (ash (qref32 scase code) -8) #xFF)))
-    (if (zerop n)
+    (if (or (zerop n)
+            ;; Ignore category Lt, Nl, So
+            (member (unicode-category code) '(92 75 109)))
 	code
 	(let ((m (aref (scase-svec scase) (logand n #x7F))))
 	  (if (logbitp 7 n) (+ code m) (- code m))))))
