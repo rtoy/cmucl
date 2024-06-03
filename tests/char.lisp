7c711f4fba8ba0cfea848010354cf0ee9a3c231c
Raymond Toy <toy.raymond@gmail.com>
2024-06-03 16:12:48 -0700

    Fix a typo and add a comment.

--- tests/char.lisp
+++ tests/char.lisp
@@ -9,12 +9,15 @@
   (:tag :issues)
   (let ((test-codes
           ;; Find all the codes where the CL lower case character
-          ;; doesn't match the Unicode lowe case character.
+          ;; doesn't match the Unicode lower case character.
           (loop for code from 128 below char-code-limit
                for ch = (code-char code)
                when (/= (char-code (char-downcase ch)) (or (lisp::unicode-lower code) code))
                  collect code)))
     (dolist (code test-codes)
+      ;; Verify that we convert to the CL lower case character instead
+      ;; of the Unicode lower case character for the cases where these
+      ;; are different.
       (assert-false (char-equal (code-char (lisp::unicode-lower code))
                                 (code-char code))
                     code
