e1b306cbd0966f0dec160d1f734fbb0304feab6f
Raymond Toy <toy.raymond@gmail.com>
2024-08-21 18:53:18 -0700

    Fix #262:  arch_skip_inst invalid code
    `arch_skip_instruction` was not correctly updated to handle the new
    UD1 instruction for trapping.  It was just using the 3rd byte of the
    instruction to get the trap code.  However, that's incorrect.  The low
    6 bits of the byte represent the trap code.
    
    Add a simple macro to extract this value and use it also in
    `sigill_handler` (which was extracting the code correctly).

--- src/lisp/x86-arch.c
+++ src/lisp/x86-arch.c
@@ -29,7 +29,13 @@
  * included here.
  */
 static const unsigned char ud1[] = {0x0f, 0xb9};
-      
+
+/*
+ * Extract the error trap code from the UD1 instruction.  BYTE must be
+ * the 3rd byte of the UD1 instruction that represents the mod r/m
+ * byte.
+ */
+#define UD1_CODE(byte) ((byte) & 0x3f)
 
 /*
  * Set to positive value to enabled debug prints related to the sigill
@@ -161,7 +167,8 @@ arch_skip_instruction(os_context_t * context)
      */
     pc += sizeof(ud1);
 
-    code = *pc++;
+    code = UD1_CODE(*pc++);
+
     SC_PC(context) = (unsigned long) pc;
 
     switch (code) {
@@ -387,7 +394,7 @@ sigill_handler(HANDLER_ARGS)
        * number is placed in the low 6-bits of the 3rd byte of the
        * instruction.
        */
-      trap = *(((char *)SC_PC(context)) + 2) & 0x3f;
+      trap = UD1_CODE(*(((char *)SC_PC(context)) + 2));
 
       DPRINTF(debug_handlers, (stderr, "code = %x\n", trap));
 
