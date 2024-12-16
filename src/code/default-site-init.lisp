28f24fa8575ed1d0948c8813b59cf97c2dae2fa0
Raymond Toy <toy.raymond@gmail.com>
2024-12-16 16:11:03 +0000

    Fix #360: Add default-site-init file

--- src/code/default-site-init.lisp
+++ src/code/default-site-init.lisp
@@ -5,28 +5,20 @@
 ;;; Carnegie Mellon University, and has been placed in the public domain.
 ;;;
 (ext:file-comment
-  "$Header: src/code/generic-site.lisp $")
+  "$Header: src/code/default-site-init.lisp $")
 ;;;
 ;;; **********************************************************************
 ;;;
-;;; This file is installed as "library:site-init" in binary
-;;; distributions of CMUCL.
+;;; This is the default site init file and should be installed as
+;;; "library:default-site-init".  This file is used only if
+;;; "library:site-init" does not exist.
+
 ;;;
 (in-package "SYSTEM")
 
 ;;; Put your site name here...
-(setq *short-site-name* "Unknown")
-(setq *long-site-name* "Site name not initialized")
-
-;;; We would appreciate it if each site establishes a local maintainer who can
-;;; filter bug reports from novice users to make sure that they really have
-;;; found a bug.  Fill in the maintainer's address here..
-(when *herald-items*
-  (rplaca
-   (cdr (member :bugs *herald-items*))
-   '("Send questions and bug reports to your local CMUCL maintainer, " terpri
-     "or see <http://www.cmucl.org/support.html>." terpri
-     "Loaded subsystems:" terpri)))
+;; (setq *short-site-name* "Unknown")
+;; (setq *long-site-name* "Site name not initialized")
 
 ;;; If you have sources installed on your system, un-comment the following form
 ;;; and change it to point to the source location.  This will allow the Hemlock
@@ -47,14 +39,6 @@
 ;;;
 ;;; If your sources are located somewhere else, change this
 ;;; accordingly.
-#|
 (setf (search-list "target:")
-      (list
-       (namestring
-	(make-pathname
-	 :directory (pathname-directory
-		     (merge-pathnames (make-pathname
-				       :directory '(:relative :up :up
-						    :up "src"))
-				      (pathname lisp::*cmucl-core-path*)))))))
-|#
+      '("library:../src/"))
+
