c8b9082bebb2f275bd27981e3eccf2cd9e7cee3c
Raymond Toy <toy.raymond@gmail.com>
2025-02-21 06:21:49 -0800

    Convert test to use with-temporary-file

--- tests/fd-streams.lisp
+++ tests/fd-streams.lisp
@@ -17,6 +17,7 @@
 (eval-when (:load-toplevel)
   (ensure-directories-exist *test-path* :verbose t))
 
+#+nil
 (define-test clear-output-1
   (:tag :trac)
   (assert-eql
@@ -34,3 +35,20 @@
 	  (setf s (open *test-file*))
 	  (file-length s))
      (delete-file *test-file*))))
+
+(define-test clear-output-1
+  (:tag :trac)
+  (assert-eql
+   0
+   (ext:with-temporary-file (test-file)
+     (let ((s (open test-file
+		    :direction :output
+		    :if-exists :supersede)))
+       ;; Write a character to the (fully buffered) output
+       ;; stream. Clear the output and close the file. Nothing
+       ;; should have been written to the file.
+       (write-char #\a s)
+       (clear-output s)
+       (close s)
+       (setf s (open test-file))
+       (file-length s)))))
