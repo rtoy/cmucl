fc5bd588f5d2a781ce1b86e284f3cb14485b8345
Raymond Toy <toy.raymond@gmail.com>
2025-05-18 16:48:54 -0700

    Fix warnings/notes produced by shellcheck for make-main-dist.sh

--- bin/make-main-dist.sh
+++ bin/make-main-dist.sh
@@ -2,7 +2,7 @@
 
 usage() {
     cat <<EOF
-`basename $0`  -C option -E ext [-h?] [-t tar][-I destdir] [-G group] [-O owner] [-M mandir]
+$(basename "$0")  -C option -E ext [-h?] [-t tar][-I destdir] [-G group] [-O owner] [-M mandir]
         target-directory version arch os
   -h           This help
   -?           This help
@@ -36,11 +36,11 @@ do
         I) INSTALL_DIR=$OPTARG ;;
         M) MANDIR=$OPTARG ;;
 	t) GTAR=$OPTARG ;;
-	h | \?) usage; exit 1 ;;
+	h | \?) usage ;;
     esac
 done
 
-shift `expr $OPTIND - 1`
+shift $((OPTIND - 1))
 
 # -C and -E options are required
 if [ -z "$COMPRESS" ]; then
@@ -53,7 +53,7 @@ if [ -z "$COMPRESS_EXT" ]; then
     exit 2
 fi
 
-if [ "$1" = "" -o "$2" = "" -o "$3" = "" -o "$4" = "" ]
+if [ "$1" = "" ] || [ "$2" = "" ] || [ "$3" = "" ] || [ "$4" = "" ]
 then
     usage
 fi
@@ -80,7 +80,7 @@ DOCDIR=${DOCDIR:-share/cmucl/$VERSION/doc}
 # Where to install man pages
 MANDIR=${MANDIR:-share/man/man1}
 
-TARGET="`echo $1 | sed 's:/*$::'`"
+TARGET="$(echo "$1" | sed 's:/*$::')"
 
 # Core file to look for.
 CORE=lisp.core
@@ -119,121 +119,126 @@ case $OS in
 esac
 
 # Frob PATH to use /usr/ucb/install for Solaris
-if [ `uname -s` = "SunOS" ]
+if [ "$(uname -s)" = "SunOS" ]
 then
 	PATH=/usr/ucb:$PATH
 fi
 
 if [ -z "$INSTALL_DIR" ]; then
     # Clean out DESTDIR, if we're not installing there.
-    echo Cleaning $DESTDIR
-    [ -d $DESTDIR ] && rm -rf $DESTDIR
+
+    echo Cleaning "$DESTDIR"
+    [ -d "$DESTDIR" ] && rm -rf "$DESTDIR"
 fi
 
 # set -x
 echo Installing main components
-install -d ${GROUP} ${OWNER} -m 0755 $DESTDIR/bin
-install -d ${GROUP} ${OWNER} -m 0755 $DESTDIR/$CMUCLLIBVER
-install -d ${GROUP} ${OWNER} -m 0755 $DESTDIR/$CMUCLLIBVER/lib
-install -d ${GROUP} ${OWNER} -m 0755 $DESTDIR/$CMUCLLIBVER/lib/subsystems
-install -d ${GROUP} ${OWNER} -m 0755 $DESTDIR/$CMUCLLIBVER/lib/ext-formats
-install -d ${GROUP} ${OWNER} -m 0755 $DESTDIR/${DOCDIR}
-install -d ${GROUP} ${OWNER} -m 0755 $DESTDIR/${MANDIR}
-install ${GROUP} ${OWNER} -m 0755 $TARGET/lisp/lisp $DESTDIR/bin/lisp-$VERSION
+installit() {
+    install ${GROUP:+ -g "$GROUP"} ${OWNER:+ -o "$OWNER"} "$@"
+}
+
+installit -d -m 0755 "$DESTDIR"/bin
+installit -d -m 0755 "$DESTDIR"/"$CMUCLLIBVER"
+installit -d -m 0755 "$DESTDIR"/"$CMUCLLIBVER"/lib
+installit -d -m 0755 "$DESTDIR"/"$CMUCLLIBVER"/lib/subsystems
+installit -d -m 0755 "$DESTDIR"/"$CMUCLLIBVER"/lib/ext-formats
+installit -d -m 0755 "$DESTDIR"/"${DOCDIR}"
+installit -d -m 0755 "$DESTDIR"/"${MANDIR}"
+installit -m 0755 "$TARGET"/lisp/lisp "$DESTDIR"/bin/lisp-"$VERSION"
 # Install symlink for lisp
-(cd $DESTDIR/bin; ln -fs lisp-$VERSION lisp)
+(cd "$DESTDIR"/bin || exit 1; ln -fs lisp-"$VERSION" lisp)
 # Install symlink for man pages
-(cd $DESTDIR/${MANDIR}
- ln -fs lisp-$VERSION.1 lisp.1
- ln -fs cmucl-$VERSION.1 cmucl.1)
+(cd "$DESTDIR"/"${MANDIR}" || exit 1
+ ln -fs lisp-"$VERSION".1 lisp.1
+ ln -fs cmucl-"$VERSION".1 cmucl.1)
 
 if [ "$EXECUTABLE" = "true" ]
 then
-    install ${GROUP} ${OWNER} -m 0644 $TARGET/lisp/lisp.a $DESTDIR/$CMUCLLIBVER/lib/
-    install ${GROUP} ${OWNER} -m 0644 $TARGET/lisp/exec-init.o $DESTDIR/$CMUCLLIBVER/lib/
-    install ${GROUP} ${OWNER} -m 0644 $TARGET/lisp/exec-final.o $DESTDIR/$CMUCLLIBVER/lib/
-    install ${GROUP} ${OWNER} -m 0755 src/tools/linker.sh $DESTDIR/$CMUCLLIBVER/lib/
-    if [ -f src/tools/$SCRIPT-cmucl-linker-script ]; then
-	install ${GROUP} ${OWNER} -m 0755 src/tools/$SCRIPT-cmucl-linker-script $DESTDIR/$CMUCLLIBVER/lib/
+    installit -m 0644 "$TARGET"/lisp/lisp.a "$DESTDIR"/"$CMUCLLIBVER"/lib/
+    installit -m 0644 "$TARGET"/lisp/exec-init.o "$DESTDIR"/"$CMUCLLIBVER"/lib/
+    installit -m 0644 "$TARGET"/lisp/exec-final.o "$DESTDIR"/"$CMUCLLIBVER"/lib/
+    installit -m 0755 src/tools/linker.sh "$DESTDIR"/"$CMUCLLIBVER"/lib/
+    if [ -f src/tools/"$SCRIPT"-cmucl-linker-script ]; then
+	installit -m 0755 src/tools/"$SCRIPT"-cmucl-linker-script "$DESTDIR"/"$CMUCLLIBVER"/lib/
     fi
 fi
-for corefile in $TARGET/lisp/$CORE
+for corefile in "$TARGET"/lisp/$CORE
 do
-  install ${GROUP} ${OWNER} -m 0644 $corefile $DESTDIR/$CMUCLLIBVER/lib/
+  installit -m 0644 "$corefile" "$DESTDIR"/"$CMUCLLIBVER"/lib/
 done
-install ${GROUP} ${OWNER} -m 0755 src/tools/load-foreign.csh src/tools/config \
-	$DESTDIR/$CMUCLLIBVER/lib/
-install ${GROUP} ${OWNER} -m 0644 src/tools/config.lisp \
-	$DESTDIR/$CMUCLLIBVER/lib/
-install ${GROUP} ${OWNER} -m 0644 src/code/default-site-init.lisp \
-	$DESTDIR/$CMUCLLIBVER/lib/
-install ${GROUP} ${OWNER} -m 0644 $TARGET/lisp/lisp.nm $TARGET/lisp/lisp.map \
-	$TARGET/lisp/internals.h $TARGET/lisp/internals.inc $DESTDIR/$CMUCLLIBVER/
-install ${GROUP} ${OWNER} -m 0755 src/tools/sample-wrapper $DESTDIR/$CMUCLLIBVER/
+installit -m 0755 src/tools/load-foreign.csh src/tools/config \
+	"$DESTDIR"/"$CMUCLLIBVER"/lib/
+installit -m 0644 src/tools/config.lisp \
+	"$DESTDIR"/"$CMUCLLIBVER"/lib/
+installit -m 0644 src/code/default-site-init.lisp \
+	"$DESTDIR"/"$CMUCLLIBVER"/lib/
+installit -m 0644 "$TARGET"/lisp/lisp.nm "$TARGET"/lisp/lisp.map \
+	"$TARGET"/lisp/internals.h "$TARGET"/lisp/internals.inc "$DESTDIR"/"$CMUCLLIBVER"/
+installit -m 0755 src/tools/sample-wrapper "$DESTDIR"/"$CMUCLLIBVER"/
 
 for f in gray-streams gray-compat simple-streams iodefs
 do
-    install ${GROUP} ${OWNER} -m 0644 $TARGET/pcl/$f-library.$FASL $DESTDIR/$CMUCLLIBVER/lib/subsystems/
+    installit -m 0644 "$TARGET"/pcl/$f-library.$FASL "$DESTDIR"/"$CMUCLLIBVER"/lib/subsystems/
 done
 
 for f in src/pcl/simple-streams/external-formats/*.lisp src/pcl/simple-streams/external-formats/aliases src/i18n/unidata.bin
 do
-    install ${GROUP} ${OWNER} -m 0644 $f $DESTDIR/$CMUCLLIBVER/lib/ext-formats/
+    installit -m 0644 "$f" "$DESTDIR"/"$CMUCLLIBVER"/lib/ext-formats/
 done
 
 # set -x
 # Create the directories for asdf and defsystem
 for f in asdf defsystem asdf/doc
 do
-    install -d ${GROUP} ${OWNER} -m 0755 $DESTDIR/$CMUCLLIBVER/lib/contrib/$f
+    installit -d -m 0755 "$DESTDIR"/"$CMUCLLIBVER"/lib/contrib/$f
 done
 
-case `uname -s` in
+case $(uname -s) in
   Linux*) UCONTRIB="unix-glibc2" ;;
   *) UCONTRIB="unix" ;;
 esac
 
-install -d ${GROUP} ${OWNER} -m 0755 $DESTDIR/$CMUCLLIBVER/lib/contrib/unix
-install ${GROUP} ${OWNER} -m 0644 $TARGET/contrib/unix/$UCONTRIB.$FASL $DESTDIR/$CMUCLLIBVER/lib/contrib/unix
-install ${GROUP} ${OWNER} -m 0644 src/contrib/load-unix.lisp $DESTDIR/$CMUCLLIBVER/lib/contrib
-install ${GROUP} ${OWNER} -m 0644 src/contrib/unix/${UCONTRIB}.lisp $DESTDIR/$CMUCLLIBVER/lib/contrib/unix
+installit -d -m 0755 "$DESTDIR"/"$CMUCLLIBVER"/lib/contrib/unix
+installit -m 0644 "$TARGET"/contrib/unix/$UCONTRIB.$FASL "$DESTDIR"/"$CMUCLLIBVER"/lib/contrib/unix
+installit -m 0644 src/contrib/load-unix.lisp "$DESTDIR"/"$CMUCLLIBVER"/lib/contrib
+installit -m 0644 src/contrib/unix/${UCONTRIB}.lisp "$DESTDIR"/"$CMUCLLIBVER"/lib/contrib/unix
 
 # Copy the source files for asdf and defsystem
-for f in `(cd src; find contrib/asdf contrib/defsystem -type f -print | grep -v CVS)`
+for f in $( (cd src || exit 1; find contrib/asdf contrib/defsystem -type f -print | grep -v CVS) )
 do
-    install ${GROUP} ${OWNER} -m 0644 src/$f $DESTDIR/$CMUCLLIBVER/lib/$f
+    installit -m 0644 src/"$f" "$DESTDIR"/"$CMUCLLIBVER"/lib/"$f"
 done
 
 # Install the fasl files for asdf and defsystem
 for f in asdf defsystem
 do
-    install ${GROUP} ${OWNER} -m 0644 $TARGET/contrib/$f/$f.$FASL $DESTDIR/$CMUCLLIBVER/lib/contrib/$f
+    installit -m 0644 "$TARGET"/contrib/$f/$f.$FASL "$DESTDIR"/"$CMUCLLIBVER"/lib/contrib/$f
 done
 
 # Install the docs for asdf
 for f in src/contrib/asdf/doc/*
 do
-    base=`basename $f`
-    install ${GROUP} ${OWNER} -m 0644 $f $DESTDIR/$CMUCLLIBVER/lib/contrib/asdf/doc/$base
+    base=$(basename "$f")
+    installit -m 0644 "$f" "$DESTDIR"/"$CMUCLLIBVER"/lib/contrib/asdf/doc/"$base"
 done
 
-install ${GROUP} ${OWNER} -m 0644 src/general-info/cmucl.1 \
-	$DESTDIR/${MANDIR}/cmucl-$VERSION.1
-install ${GROUP} ${OWNER} -m 0644 src/general-info/lisp.1 \
-	$DESTDIR/${MANDIR}/lisp-$VERSION.1
-install ${GROUP} ${OWNER} -m 0644 src/general-info/README $DESTDIR/${DOCDIR}
-if [ -f src/general-info/release-$VERSION.txt ] 
+installit -m 0644 src/general-info/cmucl.1 \
+	"$DESTDIR"/"${MANDIR}"/cmucl-"$VERSION".1
+installit -m 0644 src/general-info/lisp.1 \
+	"$DESTDIR"/"${MANDIR}"/lisp-"$VERSION".1
+installit -m 0644 src/general-info/README "$DESTDIR"/"${DOCDIR}"
+if [ -f src/general-info/release-"$VERSION".txt ] 
 then
-	install ${GROUP} ${OWNER} -m 0644 src/general-info/release-$VERSION.txt \
-		$DESTDIR/${DOCDIR}
+	installit -m 0644 src/general-info/release-"$VERSION".txt \
+		"$DESTDIR"/"${DOCDIR}"
 fi
 
 if [ -z "$INSTALL_DIR" ]; then
     sync ; sleep 1 ; sync ; sleep 1 ; sync
     echo Tarring main components
-    $GTAR -C $DESTDIR $COMPRESS -cf cmucl-$VERSION-$ARCH-$OS.tar.$COMPRESS_EXT .
+    $GTAR -C "$DESTDIR" "$COMPRESS" -cf cmucl-"$VERSION"-"$ARCH"-"$OS".tar."$COMPRESS_EXT" .
 
-    echo Cleaning $DESTDIR
-    [ -d $DESTDIR ] && rm -rf $DESTDIR
+    echo Cleaning "$DESTDIR"
+    [ -d "$DESTDIR" ] && rm -rf "$DESTDIR"
     echo Done
 fi
