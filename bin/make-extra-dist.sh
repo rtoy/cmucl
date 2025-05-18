be8645a7cc7fac7240ae53260a4b0e2fdb903529
Raymond Toy <toy.raymond@gmail.com>
2025-05-18 16:38:44 -0700

    Fix warnings/notes produced by shellcheck.

--- bin/make-extra-dist.sh
+++ bin/make-extra-dist.sh
@@ -2,7 +2,7 @@
 
 usage() {
     cat <<EOF
-`basename $0` -C option -E ext [-t tar] [-I destdir] [-G group] [-O owner]
+$(basename "$0") -C option -E ext [-t tar] [-I destdir] [-G group] [-O owner]
   -h           This help
   -?           This help
   -t tar       Tar program to use
@@ -32,11 +32,11 @@ do
 	O) OWNER="-o $OPTARG" ;;
         I) INSTALL_DIR=$OPTARG ;;
 	t) GTAR=$OPTARG ;;
-	h | \?) usage; exit 1 ;;
+	h | \?) usage ;;
     esac
 done
 
-shift `expr $OPTIND - 1`
+shift $((OPTIND - 1))
 
 # -C and -E options are required
 if [ -z "$COMPRESS" ]; then
@@ -49,7 +49,7 @@ if [ -z "$COMPRESS_EXT" ]; then
     exit 2
 fi
 
-if [ "$1" = "" -o "$2" = "" -o "$3" = "" -o "$4" = "" ]
+if [ "$1" = "" ] || [ "$2" = "" ] || [ "$3" = "" ] || [ "$4" = "" ]
 then
     usage
 fi
@@ -61,7 +61,7 @@ then
 fi
 
 DESTDIR=${INSTALL_DIR:-release-$$}
-TARGET="`echo $1 | sed 's:/*$::'`"
+TARGET="$(echo "$1" | sed 's:/*$::')"
 VERSION=$2
 ARCH=$3
 OS=$4
@@ -81,76 +81,80 @@ case $ARCH in
 esac
 
 # Frob PATH to use /usr/ucb/install for Solaris
-if [ `uname -s` = "SunOS" ]
+if [ "$(uname -s)" = "SunOS" ]
 then
 	PATH=/usr/ucb:$PATH
 fi
 
 if [ -z "$INSTALL_DIR" ]; then
-    echo Cleaning $DESTDIR
-    [ -d $DESTDIR ] && rm -rf $DESTDIR
+    echo Cleaning "$DESTDIR"
+    [ -d "$DESTDIR" ] && rm -rf "$DESTDIR"
 fi
 
+installit() {
+    install ${GROUP:+ -g "$GROUP"} ${OWNER:+ -o "$OWNER"} "$@"
+}
+
 echo Installing extra components
-install -d ${GROUP} ${OWNER} -m 0755 $DESTDIR/$CMUCLLIBVER/lib
+installit -d -m 0755 "$DESTDIR"/"$CMUCLLIBVER"/lib
 
-install -d ${GROUP} ${OWNER} -m 0755 $DESTDIR/$CMUCLLIBVER/lib/subsystems
+installit -d -m 0755 "$DESTDIR"/"$CMUCLLIBVER"/lib/subsystems
 
-install -d ${GROUP} ${OWNER} -m 0755 $DESTDIR/$CMUCLLIBVER/lib/contrib
+installit -d -m 0755 "$DESTDIR"/"$CMUCLLIBVER"/lib/contrib
 
 for ext in $FASL
 do
-  install ${GROUP} ${OWNER} -m 0644 $TARGET/clx/clx-library.$ext \
-      $DESTDIR/$CMUCLLIBVER/lib/subsystems/
-  install ${GROUP} ${OWNER} -m 0644 $TARGET/hemlock/hemlock-library.$ext \
-      $DESTDIR/$CMUCLLIBVER/lib/subsystems/
-  install ${GROUP} ${OWNER} -m 0644 $TARGET/interface/clm-library.$ext  \
-      $DESTDIR/$CMUCLLIBVER/lib/subsystems/
+  installit -m 0644 "$TARGET"/clx/clx-library."$ext" \
+      "$DESTDIR"/"$CMUCLLIBVER"/lib/subsystems/
+  installit -m 0644 "$TARGET"/hemlock/hemlock-library."$ext" \
+      "$DESTDIR"/"$CMUCLLIBVER"/lib/subsystems/
+  installit -m 0644 "$TARGET"/interface/clm-library."$ext"  \
+      "$DESTDIR"/"$CMUCLLIBVER"/lib/subsystems/
 done
 
-install ${GROUP} ${OWNER} -m 0644 src/hemlock/XKeysymDB \
+installit -m 0644 src/hemlock/XKeysymDB \
 	src/hemlock/hemlock11.cursor src/hemlock/hemlock11.mask \
-	$TARGET/hemlock/spell-dictionary.bin \
-	$DESTDIR/$CMUCLLIBVER/lib/
-install ${GROUP} ${OWNER} -m 0755 src/hemlock/mh-scan $DESTDIR/$CMUCLLIBVER/lib/
-install ${GROUP} ${OWNER} -m 0755 $TARGET/motif/server/motifd \
-	$DESTDIR/$CMUCLLIBVER/lib/
+	"$TARGET"/hemlock/spell-dictionary.bin \
+	"$DESTDIR"/"$CMUCLLIBVER"/lib/
+installit -m 0755 src/hemlock/mh-scan "$DESTDIR"/"$CMUCLLIBVER"/lib/
+installit -m 0755 "$TARGET"/motif/server/motifd \
+	"$DESTDIR"/"$CMUCLLIBVER"/lib/
 
 # Install the contrib stuff.  Create the directories and then copy the files.
 #
 # asdf, defsystem and unix are part of the main distribution, so skip
 # these directories.
-for d in `(cd src; find contrib -type d -print | grep -v "asdf\|defsystem")`
+for d in $( (cd src || exit 1; find contrib -type d -print | grep -v "asdf\|defsystem") )
 do
-    install -d ${GROUP} ${OWNER} -m 0755 $DESTDIR/$CMUCLLIBVER/lib/$d
+    installit -d -m 0755 "$DESTDIR"/"$CMUCLLIBVER"/lib/"$d"
 done
 
-for f in `(cd src/contrib; find . -type f -print | grep -v "asdf\|defsystem\|unix")`
+for f in $( (cd src/contrib || exit 1; find . -type f -print | grep -v "asdf\|defsystem\|unix") )
 do
-    DIR=`dirname $f`
-    install ${GROUP} ${OWNER} -m 0644 src/contrib/$f $DESTDIR/$CMUCLLIBVER/lib/contrib/$DIR
+    DIR=$(dirname "$f")
+    installit -m 0644 src/contrib/"$f" "$DESTDIR"/"$CMUCLLIBVER"/lib/contrib/"$DIR"
 done
 
 # Install all the locale data.
 
-for d in `(cd src/i18n/; find locale -type d -print)`
+for d in $( (cd src/i18n/ || exit 1; find locale -type d -print) )
 do
-    install -d ${GROUP} ${OWNER} -m 0755 $DESTDIR/$CMUCLLIBVER/lib/$d
+    installit -d -m 0755 "$DESTDIR"/"$CMUCLLIBVER"/lib/"$d"
 done
 
 # Install mo files.  Ignore any emacs-style backup files.
-for f in `(cd $TARGET/i18n; find locale -type f -print | grep -v '~.*~\|.*~')`
+for f in $( (cd "$TARGET"/i18n || exit 1; find locale -type f -print | grep -v '~.*~\|.*~') )
 do
-    DIR=`dirname $f`
-    install ${GROUP} ${OWNER} -m 0644 $TARGET/i18n/$f $DESTDIR/$CMUCLLIBVER/lib/$DIR
+    DIR=$(dirname "$f")
+    installit -m 0644 "$TARGET"/i18n/"$f" "$DESTDIR"/"$CMUCLLIBVER"/lib/"$DIR"
 done
 
 if [ -z "$INSTALL_DIR" ]; then
     sync ; sleep 1 ; sync ; sleep 1 ; sync
     echo Tarring extra components
-    $GTAR -C $DESTDIR $COMPRESS -cf cmucl-$VERSION-$ARCH-$OS.extra.tar.$COMPRESS_EXT lib
+    $GTAR -C "$DESTDIR" "$COMPRESS" -cf cmucl-"$VERSION"-"$ARCH"-"$OS".extra.tar."$COMPRESS_EXT" lib
 
-    echo Cleaning $DESTDIR
-    [ -d $DESTDIR ] && rm -rf $DESTDIR
+    echo Cleaning "$DESTDIR"
+    [ -d "$DESTDIR" ] && rm -rf "$DESTDIR"
     echo Done
 fi
