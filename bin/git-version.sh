b07068fcabf6fc747a37bdfae97566ed0943ac46
Raymond Toy <toy.raymond@gmail.com>
2025-05-20 14:02:28 -0700

    Address #380:  Clean up more shell scripts
    Run shellcheck on git-version.sh and fix the warnings produced by
    shellcheck.

--- bin/git-version.sh
+++ bin/git-version.sh
@@ -8,7 +8,7 @@ git-version.sh [-hfv]
     -f    The version is printed as a C file #define expression.
           Otherwise, the version is just printed to stdout
 
-    -v    Use this as the version instead of using `git describe`, which
+    -v    Use this as the version instead of using 'git describe', which
           is the default
 
 Determine the version of cmucl.  By
@@ -28,6 +28,8 @@ while getopts "hfv:" arg; do
 	   ;;
 	v) VERSION="$OPTARG"
 	   ;;
+	*) usage
+	   ;;
     esac
 done
 
@@ -36,20 +38,20 @@ if [ -n "$VERSION" ]; then
     GIT_HASH="$VERSION"
     DEFAULT_VERSION="$VERSION"
 else
-    GIT_HASH="`(git describe --dirty 2>/dev/null || git describe 2>/dev/null)`"
+    GIT_HASH="$( (git describe --dirty 2>/dev/null || git describe 2>/dev/null) )"
 
-    if [ `expr "X$GIT_HASH" : 'Xsnapshot-[0-9][0-9][0-9][0-9]-[01][0-9]'` != 0 ]; then
+    if expr "X$GIT_HASH" : 'Xsnapshot-[0-9][0-9][0-9][0-9]-[01][0-9]'; then
 	# The git hash looks like snapshot-yyyy-mm-<stuff>.  Remove the
 	# "snapshot-" part.
-	DEFAULT_VERSION=`expr "$GIT_HASH" : "snapshot-\(.*\)"`
-    elif [ `expr "X$GIT_HASH" : 'X[0-9][0-9][a-f]'` != 0 ]; then
+	DEFAULT_VERSION=$(expr "$GIT_HASH" : "snapshot-\(.*\)")
+    elif expr "X$GIT_HASH" : 'X[0-9][0-9][a-f]'; then
 	# The git hash looks like a release which is 3 hex digits.  Use it as is.
 	DEFAULT_VERSION="${GIT_HASH}"
     fi
 fi
 
 if [ -z "$FILE" ]; then
-    echo $DEFAULT_VERSION
+    echo "$DEFAULT_VERSION"
 else
     cat <<EOF
 /*
