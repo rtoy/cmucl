468f6d76de042df10dc7cd32a769edb05c1d0859
Raymond Toy <toy.raymond@gmail.com>
2025-05-20 14:44:38 -0700

    Discard output from expr
    Forgot to discard the output from `expr`, which goes to stdout, which
    eventually goes to the cmucl-version.h file.  That's not wanted.

--- bin/git-version.sh
+++ bin/git-version.sh
@@ -40,11 +40,11 @@ if [ -n "$VERSION" ]; then
 else
     GIT_HASH="$( (git describe --dirty 2>/dev/null || git describe 2>/dev/null) )"
 
-    if expr "X$GIT_HASH" : 'Xsnapshot-[0-9][0-9][0-9][0-9]-[01][0-9]'; then
+    if expr "X$GIT_HASH" : 'Xsnapshot-[0-9][0-9][0-9][0-9]-[01][0-9]' > /dev/null; then
 	# The git hash looks like snapshot-yyyy-mm-<stuff>.  Remove the
 	# "snapshot-" part.
 	DEFAULT_VERSION=$(expr "$GIT_HASH" : "snapshot-\(.*\)")
-    elif expr "X$GIT_HASH" : 'X[0-9][0-9][a-f]'; then
+    elif expr "X$GIT_HASH" : 'X[0-9][0-9][a-f]'> /dev/null; then
 	# The git hash looks like a release which is 3 hex digits.  Use it as is.
 	DEFAULT_VERSION="${GIT_HASH}"
     fi
