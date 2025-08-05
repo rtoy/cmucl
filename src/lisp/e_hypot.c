1756320905491dbd61e1482e4d9177f822b197fb
Raymond Toy <toy.raymond@gmail.com>
2025-08-04 21:01:57 -0700

    Replace __HI and __LO macros that no longer exist
    The __HI and __LO macros to access the high and parts of a double no
    longer exist.  We need to use a union for this.
    
    To make the code look like the original, define some new inline
    functions get_hiword, get_loword, and set_hiword.  The first two get,
    respectively, the high and low words of a double float.  The second
    sets the high word of a double to the given value.  These are all
    that's needed for the hypot function.

--- src/lisp/e_hypot.c
+++ src/lisp/e_hypot.c
@@ -45,6 +45,34 @@
 
 #include "fdlibm.h"
 
+static inline void
+set_hiword(double* d, int hi)
+{
+    union { int i[2]; double d; } ux;
+
+    ux.d = *d;
+    ux.i[HIWORD] = hi;
+    *d = ux.d;
+}
+
+static inline int
+get_loword(double d) 
+{
+    union { int i[2]; double d; } ux;
+    ux.d = d;
+    return ux.i[LOWORD];
+}
+
+static inline int
+get_hiword(double d)
+{
+    union { int i[2]; double d; } ux;
+    ux.d = d;
+
+    return ux.i[HIWORD];
+}
+
+    
 #ifdef __STDC__
 	double __ieee754_hypot(double x, double y)
 #else
@@ -55,30 +83,31 @@
 	double a=x,b=y,t1,t2,y1,y2,w;
 	int j,k,ha,hb;
 
-	ha = __HI(x)&0x7fffffff;	/* high word of  x */
-	hb = __HI(y)&0x7fffffff;	/* high word of  y */
+	ha = get_hiword(x)&0x7fffffff;	/* high word of  x */
+	hb = get_hiword(y)&0x7fffffff;	/* high word of  y */
 	if(hb > ha) {a=y;b=x;j=ha; ha=hb;hb=j;} else {a=x;b=y;}
-	__HI(a) = ha;	/* a <- |a| */
-	__HI(b) = hb;	/* b <- |b| */
+	set_hiword(&a,ha);	/* a <- |a| */
+	set_hiword(&b, hb);	/* b <- |b| */
+	
 	if((ha-hb)>0x3c00000) {return a+b;} /* x/y > 2**60 */
 	k=0;
 	if(ha > 0x5f300000) {	/* a>2**500 */
 	   if(ha >= 0x7ff00000) {	/* Inf or NaN */
 	       w = a+b;			/* for sNaN */
-	       if(((ha&0xfffff)|__LO(a))==0) w = a;
-	       if(((hb^0x7ff00000)|__LO(b))==0) w = b;
+	       if(((ha&0xfffff)|get_loword(a))==0) w = a;
+	       if(((hb^0x7ff00000)|get_loword(b))==0) w = b;
 	       return w;
 	   }
 	   /* scale a and b by 2**-600 */
 	   ha -= 0x25800000; hb -= 0x25800000;	k += 600;
-	   __HI(a) = ha;
-	   __HI(b) = hb;
+	   set_hiword(&a, ha);
+	   set_hiword(&b, hb);
 	}
 	if(hb < 0x20b00000) {	/* b < 2**-500 */
 	    if(hb <= 0x000fffff) {	/* subnormal b or 0 */	
-		if((hb|(__LO(b)))==0) return a;
+		if((hb|(get_loword(b)))==0) return a;
 		t1=0;
-		__HI(t1) = 0x7fd00000;	/* t1=2^1022 */
+		set_hiword(&t1, 0x7fd00000); /* t1=2^1022 */
 		b *= t1;
 		a *= t1;
 		k -= 1022;
@@ -86,30 +115,30 @@
 	        ha += 0x25800000; 	/* a *= 2^600 */
 		hb += 0x25800000;	/* b *= 2^600 */
 		k -= 600;
-	   	__HI(a) = ha;
-	   	__HI(b) = hb;
+		set_hiword(&a, ha);
+		set_hiword(&b, hb);
 	    }
 	}
     /* medium size a and b */
 	w = a-b;
 	if (w>b) {
 	    t1 = 0;
-	    __HI(t1) = ha;
+	    set_hiword(&t1, ha);
 	    t2 = a-t1;
 	    w  = sqrt(t1*t1-(b*(-b)-t2*(a+t1)));
 	} else {
 	    a  = a+a;
 	    y1 = 0;
-	    __HI(y1) = hb;
+	    set_hiword(&y1, hb);
 	    y2 = b - y1;
 	    t1 = 0;
-	    __HI(t1) = ha+0x00100000;
+	    set_hiword(&t1, ha+0x00100000);
 	    t2 = a - t1;
 	    w  = sqrt(t1*y1-(w*(-w)-(t1*y2+t2*b)));
 	}
 	if(k!=0) {
 	    t1 = 1.0;
-	    __HI(t1) += (k<<20);
+	    set_hiword(&t1, get_hiword(t1) + (k<<20));
 	    return t1*w;
 	} else return w;
 }
