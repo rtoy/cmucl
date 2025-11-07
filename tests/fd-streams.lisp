ef79c4ece6fdaae07791ced213dab53d80362858
Raymond Toy <toy.raymond@gmail.com>
2025-11-07 10:38:26 -0800

    Convert clear-output-1 test to use with-temporary-file

--- tests/fd-streams.lisp
+++ tests/fd-streams.lisp
@@ -36,6 +36,25 @@
 	  (file-length s))
      (delete-file *test-file*))))
 
+(define-test clear-output-1
+    (:tag :trac)
+  (assert-eql
+   0
+   (ext:with-temporary-file (test-file)
+     (let ((s (open test-file
+		    :direction :output
+		    :if-exists :supersede)))
+       ;; Write a character to the (fully buffered) output
+       ;; stream. Clear the output and close the file. Nothing
+       ;; should have been written to the file.
+       (write-char #\a s)
+       (clear-output s)
+       (close s)
+       (setf s (open test-file))
+       (prog1
+	   (file-length s)
+	 (close s))))))
+
 (define-test file-position.1
     (:tag :issues)
   ;; Create a short test file
