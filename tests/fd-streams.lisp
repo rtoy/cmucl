fef970d55aac584f24ed2ccb7745593be6ba7214
Raymond Toy <toy.raymond@gmail.com>
2025-11-10 10:14:36 -0800

    Remove nil'ed out test code.
    The original clear-output-1 test has been replaced with a version that
    uses with-temporary-file.

--- tests/fd-streams.lisp
+++ tests/fd-streams.lisp
@@ -17,25 +17,6 @@
 (eval-when (:load-toplevel)
   (ensure-directories-exist *test-path* :verbose t))
 
-#+nil
-(define-test clear-output-1
-  (:tag :trac)
-  (assert-eql
-   0
-   (unwind-protect
-	(let ((s (open *test-file*
-		       :direction :output
-		       :if-exists :supersede)))
-	  ;; Write a character to the (fully buffered) output
-	  ;; stream. Clear the output and close the file. Nothing
-	  ;; should have been written to the file.
-	  (write-char #\a s)
-	  (clear-output s)
-	  (close s)
-	  (setf s (open *test-file*))
-	  (file-length s))
-     (delete-file *test-file*))))
-
 (define-test clear-output-1
     (:tag :trac)
   (assert-eql
