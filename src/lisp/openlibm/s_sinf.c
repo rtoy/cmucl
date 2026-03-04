0cf6892505327e8878327d2fe10efc03f0823f16
Raymond Toy <toy.raymond@gmail.com>
2026-03-04 12:12:12 -0800

    Fix typo for sinf

--- src/lisp/openlibm/s_sinf.c
+++ src/lisp/openlibm/s_sinf.c
@@ -36,7 +36,7 @@ s3pio2 = 3*M_PI_2,			/* 0x4012D97C, 0x7F3321D2 */
 s4pio2 = 4*M_PI_2;			/* 0x401921FB, 0x54442D18 */
 
 OLM_DLLEXPORT float
-openibm_sinf(float x)
+openlibm_sinf(float x)
 {
 	double y;
 	int32_t n, hx, ix;
