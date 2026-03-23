f4345a37a4b9b4b2c1a80fe05a1a732aea641fd9
Raymond Toy <toy.raymond@gmail.com>
2026-03-23 10:17:34 -0700

    First cut at arm64 subprim.lisp
    Basically the sparc version but updated for arm64.
    
    Compiles, but no other testing, obviously.
    
    [skip-ci]

---
+++ src/compiler/arm64/subprim.lisp
@@ -0,0 +1,62 @@
+;;; -*- Package: ARM64 -*-
+;;;
+;;; **********************************************************************
+;;; This code was written as part of the CMU Common Lisp project at
+;;; Carnegie Mellon University, and has been placed in the public domain.
+;;;
+(ext:file-comment
+  "$Header: src/compiler/arm64/subprim.lisp $")
+;;;
+;;; **********************************************************************
+;;;
+;;;    Linkage information for standard static functions, and random vops.
+;;;
+;;; Written by William Lott.
+;;; ARM64 port derived from the SPARC implementation.
+;;;
+(in-package "ARM64")
+
+
+    
+
+;;;; Length
+
+(define-vop (length/list)
+  (:translate length)
+  (:args (object :scs (descriptor-reg) :target ptr))
+  (:arg-types list)
+  (:temporary (:scs (descriptor-reg) :from (:argument 0)) ptr)
+  (:temporary (:scs (non-descriptor-reg)) temp)
+  (:temporary (:scs (any-reg) :type fixnum :to (:result 0) :target result)
+              count)
+  (:results (result :scs (any-reg descriptor-reg)))
+  (:policy :fast-safe)
+  (:vop-var vop)
+  (:save-p :compute-only)
+  (:generator 50
+    (emit-not-implemented)
+    (let ((done (gen-label))
+          (loop (gen-label))
+          (not-list (generate-cerror-code vop object-not-list-error object)))
+      (move ptr object)
+      (move count zero-tn)
+
+      (emit-label loop)
+
+      ;; Unlike SPARC, ARM64 has no branch delay slots; no NOP needed.
+      (inst cmp ptr null-tn)
+      (inst b :eq done)
+
+      (test-type ptr temp not-list t vm:list-pointer-type)
+
+      (loadw ptr ptr vm:cons-cdr-slot vm:list-pointer-type)
+      (inst add count count (fixnumize 1))
+      (test-type ptr temp loop nil vm:list-pointer-type)
+
+      (cerror-call vop done object-not-list-error ptr)
+
+      (emit-label done)
+      (move result count))))
+
+
+(define-static-function length (object) :translate length)
