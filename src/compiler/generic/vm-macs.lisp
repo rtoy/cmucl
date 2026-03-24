81a7dd3d43e8ba5b2bfaee449841329e3657861c
Raymond Toy <toy.raymond@gmail.com>
2026-03-24 15:36:11 -0700

    Define arm64 fasl file version number and name.
    [skip-ci]

--- src/compiler/generic/vm-macs.lisp
+++ src/compiler/generic/vm-macs.lisp
@@ -239,7 +239,8 @@
 	  sgi-fasl-file-implementation
 	  ppc-fasl-file-implementation
 	  amd64-fasl-file-implementation
-	  arm-fasl-file-implementation))
+	  arm-fasl-file-implementation
+	  arm64-fasl-file-implementation))
 
 ;;; Constants for the different implementations.  These are all defined in
 ;;; one place to make sure they are all unique.
@@ -247,7 +248,8 @@
 (defparameter fasl-file-implementations
   '(nil "Pmax" "Sparc" "RT" "RT/AFPA" "x86" "HPPA"
     "Big-endian byte-code" "Little-endian byte-code" "Alpha" "SGI" "PPC" "AMD64"
-    "ARM"))
+    "ARM"
+    "ARM64/aarch64"))
 (defconstant pmax-fasl-file-implementation 1)
 (defconstant sparc-fasl-file-implementation 2)
 (defconstant rt-fasl-file-implementation 3)
@@ -261,6 +263,7 @@
 (defconstant ppc-fasl-file-implementation 11)
 (defconstant amd64-fasl-file-implementation 12)
 (defconstant arm-fasl-file-implementation 13)
+(defconstant arm64-fasl-file-implementation 14)
 
 ;;; The maximum number of SCs in any implementation.
 (defconstant sc-number-limit 32)
