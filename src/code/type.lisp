3dfca652b4707cbb430fdf8a8e98a422dea89fa3
Raymond Toy <toy.raymond@gmail.com>
2026-06-23 15:38:50 -0700

    Remove defypte of standard-char from code/type.lisp
    It's not needed now that we have a separate concrete standard-char
    type.

--- src/code/type.lisp
+++ src/code/type.lisp
@@ -3640,16 +3640,6 @@
   "Type of characters that aren't base-char's.  None in CMU CL."
   '(and character (not base-char)))
 
-#+nil
-(deftype standard-char ()
-  "Type corresponding to the charaters required by the standard."
-  '(member #\NEWLINE #\SPACE #\! #\" #\# #\$ #\% #\& #\' #\( #\) #\* #\+ #\,
-	   #\- #\. #\/ #\0 #\1 #\2 #\3 #\4 #\5 #\6 #\7 #\8 #\9 #\: #\; #\< #\=
-	   #\> #\?  #\@ #\A #\B #\C #\D #\E #\F #\G #\H #\I #\J #\K #\L #\M
-	   #\N #\O #\P #\Q #\R #\S #\T #\U #\V #\W #\X #\Y #\Z #\[ #\\ #\]
-	   #\^ #\_ #\` #\a #\b #\c #\d #\e #\f #\g #\h #\i #\j #\k #\l #\m
-	   #\n #\o #\p #\q #\r #\s #\t #\u #\v #\w #\x #\y #\z #\{
-    #\| #\} #\~))
 (deftype keyword ()
   "Type for any keyword symbol."
   '(and symbol (satisfies keywordp)))
